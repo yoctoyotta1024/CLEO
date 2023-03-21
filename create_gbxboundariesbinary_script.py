308a66ce32b32478c1d77b3d39565a6c3f97017a
clara bayley <yoctoyotta1024@yahoo.com>
2023-03-21 10:30:33 +0100

    abspath given to pyscripts as argument

--- create_gbxboundariesbinary_script.py
+++ create_gbxboundariesbinary_script.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from pathlib import Path
 
@@ -5,7 +6,8 @@ from pySD.gbxboundariesbinary_src.create_gbxboundaries import *
 from pySD.gbxboundariesbinary_src.read_gbxboundaries import *
 
 ### path and filenames
-abspath = "/Users/yoctoyotta1024/Documents/b1_springsummer2023/CLEO/"
+#abspath = "/Users/yoctoyotta1024/Documents/b1_springsummer2023/CLEO/"
+abspath = sys.argv[1]
 constsfile = abspath+"libs/claras_SDconstants.hpp"
 configfile = abspath+"src/config/config.txt"
 
