ef01d1f76f1f87b6d2ffa9c8d4185f0361e92088
clara bayley <yoctoyotta1024@yahoo.com>
2023-03-22 10:45:18 +0100

    use g++ not gcc compiler for linking as well as comilation

--- runfromclone.sh
+++ runfromclone.sh
@@ -3,7 +3,7 @@
 module load gcc/11.2.0-gcc-11.2.0
 conda activate /work/mh1126/m300950/superdropsV2
 
-CXX=gcc cmake -S ./ -B ./build
+CXX=g++ cmake -S ./ -B ./build
 
 python ./create_gbxboundariesbinary_script.py ./
 python ./create_initsuperdropsbinary_script.py ./ 5e9
