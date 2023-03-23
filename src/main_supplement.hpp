73f222e3f1ada6a97342b263209557b4d4ee876a
Clara Levante <yoctoyotta1024@yahoo.com>
2023-03-23 09:52:15 +0100

    validations working on Levante

--- src/main_supplement.hpp
+++ src/main_supplement.hpp
@@ -71,8 +71,8 @@ combined process of those two individual processes */
   /* choose an amalgamation of sdm processes to make the returned sdmprocess */
   // const auto sdmprocess =  condensation_process >> collision_process >> sedimentation_process;
   // const auto sdmprocess = condensation_process >> collision_process;
-  // const auto sdmprocess = collision_process >> sedimentation_process;
-  const auto sdmprocess = collision_process;
+  const auto sdmprocess = collision_process >> sedimentation_process;
+  // const auto sdmprocess = collision_process;
   // const auto sdmprocess = condensation_process;
   // const auto sdmprocess = NullProcess{};
 
