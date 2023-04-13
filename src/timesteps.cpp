8417f98616f1a85082fd9b49004d09da26ceaf1d
clara bayley <yoctoyotta1024@yahoo.com>
2023-04-13 16:43:27 +0900

    inline timestep funcs

--- src/timesteps.cpp
+++ src/timesteps.cpp
@@ -5,8 +5,6 @@ timstep variables for SDM */
 
 #include "timesteps.hpp"
 
-using model_step = std::chrono::duration<int, std::ratio<1, 100000>>;
-using secd = std::chrono::duration<double>;
 
 Timesteps::Timesteps(Config config)
 /* (dimensionless) double's that are timesteps in config struct
@@ -46,26 +44,4 @@ timestep = 0 */
                                 "coll or sedi tstep. undefined sdm timstepping");
   }
 
-}
-
-int realtime2timestep(const double TSTEP)
-/* convert TSTEP [seconds] (a double e.g. from config file)
-into a dimensionless time and then into an integer no. of
-model timesteps using model_step chrono function */
-{
-  return std::chrono::round<model_step>(secd{TSTEP / dlc::TIME0}).count();
-}
-
-double timestep2realtime(const int mdlstep)
-/* convert model timestep (integer) into a dimensionless time
-given secd and model_step chrono functions */
-{
-  return secd{model_step{mdlstep}}.count() * dlc::TIME0;
-}
-
-double timestep2dimlesstime(const int mdlstep)
-/* convert model timestep (integer) into a dimensionless time
-given secd and model_step chrono functions */
-{
-  return secd{model_step{mdlstep}}.count();
 }
--- src/timesteps.hpp
+++ src/timesteps.hpp
@@ -16,18 +16,30 @@ timestep variables for SDM */
 
 namespace dlc = dimless_constants;
 
-int realtime2timestep(const double TSTEP);
+using model_step = std::chrono::duration<int, std::ratio<1, 100000>>;
+using secd = std::chrono::duration<double>;
+
+inline int realtime2timestep(const double TSTEP)
 /* convert TSTEP [seconds] (a double e.g. from config file)
 into a dimensionless time and then into an integer no. of
 model timesteps using model_step chrono function */
+{
+  return std::chrono::round<model_step>(secd{TSTEP / dlc::TIME0}).count();
+}
 
-double timestep2realtime(const int mdlstep);
+inline double timestep2realtime(const int mdlstep)
 /* convert model timestep (integer) into a dimensionless time
 given secd and model_step chrono functions */
+{
+  return secd{model_step{mdlstep}}.count() * dlc::TIME0;
+}
 
-double timestep2dimlesstime(const int mdlstep);
+inline double timestep2dimlesstime(const int mdlstep)
 /* convert model timestep (integer) into a dimensionless time
 given secd and model_step chrono functions */
+{
+  return secd{model_step{mdlstep}}.count();
+}
 
 struct Timesteps
 {
