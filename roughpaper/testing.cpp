4faae3b37c6e74a6253a2d04df3dd4c4a7732e05
clara bayley <yoctoyotta1024@yahoo.com>
2023-04-13 20:33:52 +0900

    testing delted unused files

--- roughpaper/testing.cpp
+++ roughpaper/testing.cpp
@@ -32,23 +32,6 @@ void print_nbourmaps(const Maps4GridBoxes &gbxmaps, const double COORD0);
 void print_gridboxmaps(const Maps4GridBoxes &gbxmaps, const double COORD0);
 void print_superdropcoords(const std::vector<GridBox> &gridboxes,
                            const Maps4GridBoxes &gbxmaps);
-int visualise_tsteps();
-
-inline int nextt_coupl_or_motion(const int t_sdm, const int couplstep,
-                               const int motionstep)
-/* given current time, t_sdm, work out which event (motion or coupling)
-is next to occur and return the time of the sooner event */
-{
-  const int next_motion = ((t_sdm / motionstep) + 1) * motionstep; // t of next xchange
-  const int next_coupl = ((t_sdm / couplstep) + 1) * couplstep;             // t of next output
-
-  return std::min(next_motion, next_coupl);
-}
-
-inline int next_step(int t, int step)
-{
-  return ((t / step) + 1) * step;
-}
 
 int main()
 {
@@ -82,8 +65,6 @@ int main()
   move_superdrops_in_domain(gbxmaps, sdmmotion,
                             SDsInGBxs, gridboxes);
   print_superdropcoords(gridboxes, gbxmaps);
-
-  visualise_tsteps();
 
   return 0;
 }
@@ -200,50 +181,4 @@ void print_superdropcoords(const std::vector<GridBox> &gridboxes,
   }
 
   std::cout << "\n-----------------------\n";
-}
-
-void run_sdmstep(const int t_mdl, const int couplstep,
-                 const int motionstep, const int sdmstep)
-{
-  int t_sdm = t_mdl;
-  while (t_sdm < t_mdl + couplstep)
-  {
-    const int nextt = nextt_coupl_or_motion(t_sdm, couplstep, motionstep);
-
-    for (int subt = t_sdm; subt < nextt; subt=next_step(subt, sdmstep))
-    {
-      if (subt % sdmstep == 0)
-      {
-        std::cout << subt << " ---> process step\n";
-      }
-    }
-
-    if (t_sdm % motionstep == 0)
-    {
-      std::cout << t_sdm <<  " --> motion step\n";
-    }
-
-    t_sdm += nextt;
-  }
-}
-
-int visualise_tsteps()
-{
-  const int t_end = 10;
-  const int couplstep = 8; //outstep
-  const int motionstep = 6;
-  const int sdmstep = 5;
-  int t_mdl = 0; // time that is incremented by length 'outstep' between coupling communication
- 
-  while (t_mdl <= t_end)
-  {
-    run_sdmstep(t_mdl, couplstep, motionstep, sdmstep);
-    std::cout << t_mdl << " -> run driver step\n";
-    std::cout << t_mdl << " -> couple step\n";
-
-    t_mdl += couplstep;
-  }
-
-
-  return 0;
 }
