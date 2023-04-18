ef318c64016851d81e57bc04f79628152ab1712b
clara bayley <yoctoyotta1024@yahoo.com>
2023-04-18 18:37:41 +0900

    executable and cmake needed for thermofromfile lib

---
+++ libs/thermofromfile/thermodynamicsfromfile.cpp
@@ -0,0 +1,12 @@
+// Author: Clara Bayley
+// File: thermodynamicsfromfile.cpp
+/* implementation of things specifically
+to run uncoupled SDM where thermodynamics
+are read from file */
+
+#include "thermodynamicsfromfile.hpp"
+
+void ThermodynamicsFromFile::run_thermostep(const int couplstep) const
+{
+  std::cout << "thermostep\n";
+}
