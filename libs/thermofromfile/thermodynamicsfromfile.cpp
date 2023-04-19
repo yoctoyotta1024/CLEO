7702b695ed05827a165db77675a0bdaafa56dae0
clara bayley <yoctoyotta1024@yahoo.com>
2023-04-19 10:45:35 +0900

    thermvar data read into vector given file name

--- libs/thermofromfile/thermodynamicsfromfile.cpp
+++ libs/thermofromfile/thermodynamicsfromfile.cpp
@@ -6,10 +6,26 @@ are read from file */
 
 #include "thermodynamicsfromfile.hpp"
 
+std::vector<double> thermovar_frombinary(std::string_view filename)
+{
+  std::ifstream file(open_binary(filename));
+
+  std::vector<VarMetadata> meta(metadata_from_binary(file));
+  
+  VarMetadata varmeta(meta.at(0));
+  file.seekg(varmeta.b0, std::ios::beg);
+  std::vector<double> thermovar(varmeta.nvar, 0);
+  binary_into_buffer<double>(file, thermovar);
+
+  return thermovar;
+}
+
 ThermodynamicsFromFile::ThermodynamicsFromFile(const Config &config,
                                                const int ngridboxes)
 {
-  std::cout << "initit\n";
+  thermovar_frombinary(config.press_filename);
+  std::cout << "Pressure data read from binary\n";
+
 }
 
 void ThermodynamicsFromFile::run_thermostep(const int couplstep) const
