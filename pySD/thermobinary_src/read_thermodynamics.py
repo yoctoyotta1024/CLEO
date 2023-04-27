ae9910102b9b94a0796380f967f54b9134fd0fe4
Clara Levante <yoctoyotta1024@yahoo.com>
2023-04-27 08:55:43 +0200

    cleaned up plotting of thermodata

--- pySD/thermobinary_src/read_thermodynamics.py
+++ pySD/thermobinary_src/read_thermodynamics.py
@@ -17,6 +17,58 @@ def relative_humidity(press, temp, qvap, Mr_ratio):
 
     return relh, supersat
 
+class Thermo4D:
+
+    def __init__(self, thermodata, ndims, ntime):
+        
+        reshape = [ntime] + list(np.flip(ndims))
+        
+        self.vars = ["press", "temp", "qvap", "qcond"]
+        self.press = np.reshape(thermodata["press"], reshape)
+        self.temp = np.reshape(thermodata["temp"], reshape)
+        self.qvap= np.reshape(thermodata["qvap"], reshape)
+        self.qcond = np.reshape(thermodata["qcond"], reshape)
+        # self.wvel = np.array([])
+        # self.uvel = np.array([])
+        # self.vvel = np.array([])
+
+        if "wvel" in thermodata.keys():
+            self.wvel = np.reshape(thermodata["wvel"], reshape) 
+            self.vars += ["wvel"]
+            if "uvel" in thermodata.keys():
+                self.uvel = np.reshape(thermodata["uvel"], reshape) 
+                self.vars += ["uvel"]
+                if "vvel" in thermodata.keys():
+                    self.vvel = np.reshape(thermodata["vvel"], reshape)  
+                    self.vars += ["vvel"]
+    
+    def __getitem__(self, key):
+      if key not in self.vars:
+        err = "no variable "+key+" in Thermo4D"
+        raise ValueError(err)         
+      elif key == "press":
+        return self.press
+      elif key == "temp":
+        return self.temp
+      elif key == "qvap":
+        return self.qvap
+      elif key == "qcond":
+        return self.qcond
+      elif key == "wvel":
+        return self.wvel
+      elif key == "uvel":
+        return self.uvel
+      elif key == "vvel":
+        return self.vvel
+
+    def meanytime(self, var):
+
+        return np.mean(var, axis=(0,1))
+
+    def meanxytime(self, var):
+
+        return np.mean(var, axis=(0,1,2))
+
 def read_dimless_thermodynamics_binary(thermofile, ngridboxes,
                                        ntime, SDnspace):
 
@@ -44,21 +96,25 @@ def read_dimless_thermodynamics_binary(thermofile, ngridboxes,
   
   return thermodata
 
-def get_thermodynamics_from_thermofile(thermofile, ngridboxes, inputs=False,
+def get_thermodynamics_from_thermofile(thermofile, ndims, inputs=False,
                                        constsfile="", configfile=""):
 
   if not inputs:  
     inputs = thermoinputsdict(configfile, constsfile)
 
+  ngridboxes = int(np.prod(ndims))
   thermodata = read_dimless_thermodynamics_binary(thermofile, ngridboxes, 
                                                   inputs["ntime"],
                                                   inputs["SDnspace"]) 
   dth = DimlessThermodynamics(inputs=inputs)
-  
-  return dth.redimensionalise(thermodata)
+  thermodata = dth.redimensionalise(thermodata)
+
+  thermodata = Thermo4D(thermodata, ndims, inputs["ntime"]) 
+
+  return thermodata # shaped into 4D array dims [time, y, x, z]
 
 def plot_thermodynamics_timeslice(constsfile, configfile, gridfile,
-                                  thermofile, t2plt, binpath, savefig):
+                                  thermofile, binpath, savefig):
 
     plt.rcParams.update({'font.size': 14})
 
@@ -69,42 +125,117 @@ def plot_thermodynamics_timeslice(constsfile, configfile, gridfile,
     zhalf, xhalf, yhalf = rgrid.halfcoords_from_gbxbounds(gbxbounds)
     zfull, xfull, yfull = rgrid.fullcell_fromhalfcoords(zhalf, xhalf, yhalf)
 
-    ngridboxes = int(np.prod(ndims))
-    thermodata = get_thermodynamics_from_thermofile(thermofile, ngridboxes,
+    thermodata = get_thermodynamics_from_thermofile(thermofile, ndims,
                                                     inputs=inputs)
     
-    vars = [v for v in thermodata.keys()]
+    plot_1dprofiles(zfull, thermodata, binpath, savefig)
+    
+    plot_2dcolormaps(zhalf, xhalf, thermodata, inputs, binpath, savefig)
+
+    plot_2dwindfield(zfull, xfull, zhalf, xhalf, thermodata["wvel"],
+                    thermodata["uvel"], binpath, savefig)
+
+def plot_1dprofiles(zfull, thermodata, binpath, savefig):
+    
+    vars = thermodata.vars
     units = [" /Pa", " /K", "", ""]+[" /ms$^{-1}$"]*3
     fig, axs = plt.subplots(nrows=2, ncols=4, figsize=(12, 6))
     axs = axs.flatten()
 
-    gbxs = range(ngridboxes)
-    if t2plt == "all":
-      for v, var in enumerate(vars):
-        for t in range(inputs['ntime']):
-          axs[v].plot(thermodata[var][t,:], gbxs, marker="x")
-      
-    else:
-      for v, var in enumerate(vars):
-        end, stp = inputs["T_END"]+inputs["COUPLTSTEP"], inputs["COUPLTSTEP"]
-        times = np.arange(0, end, stp)
-        t = np.argmin(abs(t2plt-times))
-        l = axs[v].plot(thermodata[var][t,:], gbxs,
-                        color="k", marker="x", linestyle="--")
-      
-        axs[0].legend(["t={:.0f}s".format(times[t])], loc="upper right")
-    
-    for v in range(len(vars)):
-      axs[v].set_xlabel(vars[v]+units[v])
-      axs[v].set_ylabel("GBx")
+    handles, labels = [], []
+    for v, var in enumerate(vars):
+      var1d_alltime = np.mean(thermodata[var], axis=(1,2)) # z profile at each time
+      line = axs[v].plot(var1d_alltime.T, zfull[None,:].T, marker="x")
+      axs[v].set_xlabel(vars[v]+units[v])  
+      handles.append(line)
+
+    axs[0].set_ylabel("z /km")
+    axs[4].set_ylabel("z /km")
     for a in range(len(vars), len(axs), 1):
       axs[a].remove()
 
     fig.tight_layout()
     if savefig:
-        tstr = str(t2plt).replace('.', "p")
-        savename = "/gridboxboundaries.png"
+        savename = "/thermo1dalltimeprofiles.png"
         fig.savefig(binpath+savename, dpi=400,
                     bbox_inches="tight", facecolor='w', format="png")
         print("Figure .png saved as: "+binpath+savename)
     plt.show()
+
+def plot_2dcolormaps(zhalf, xhalf, thermodata, inputs, binpath, savefig):
+
+  vars = ["press", "temp", "qvap", "qcond"]
+  units = [" /Pa", " /K", "", ""]
+  cmaps = ["PRGn", "RdBu", "BrBG", "BrBG", "Blues", "Blues"]
+  xxh, zzh = np.meshgrid(xhalf, zhalf, indexing="ij") # dims [xdims, zdims]
+  
+  fig, axs = plt.subplots(nrows=2, ncols=3,  figsize=(8,6))
+  axs = axs.flatten()
+  
+  for v, var in enumerate(vars):
+    mean2d = thermodata.meanytime(thermodata[var]) #avg over time and y axes
+    pcm = axs[v].pcolormesh(xxh[:,:], zzh[:,:], mean2d, cmap=cmaps[v])
+    plt.colorbar(pcm, ax=axs[v], location="top", label=var+units[v])
+
+  relh, supersat = relative_humidity(thermodata.press, thermodata.temp,
+                                      thermodata.qvap, inputs["Mr_ratio"])
+  
+  pcm =axs[4].pcolormesh(xxh[:,:], zzh[:,:],
+                    thermodata.meanytime(relh), cmap=cmaps[4])
+  plt.colorbar(pcm, ax=axs[4], location="top", label="relative humidity")
+  pcm = axs[5].pcolormesh(xxh[:,:], zzh[:,:],
+                    thermodata.meanytime(supersat)*100, cmap=cmaps[5])
+  plt.colorbar(pcm, ax=axs[5], location="top", label="% supersaturation")
+
+  for ax in axs:
+    ax.set_aspect("equal")
+
+  axs[0].set_ylabel("z /km")
+  axs[3].set_ylabel("z /km")
+  for ax in axs[3:]:
+    ax.set_xlabel("x /km")
+
+  fig.tight_layout()
+  if savefig:
+      savename = "/thermo2dmeanprofiles.png"
+      fig.savefig(binpath+savename, dpi=400,
+                  bbox_inches="tight", facecolor='w', format="png")
+      print("Figure .png saved as: "+binpath+savename)
+  plt.show()
+
+def plot_2dwindfield(zfull, xfull, zhalf, xhalf, wvel4d, uvel4d, 
+                     binpath, savefig):
+  
+  cmaps = ["coolwarm"]*3
+  xxh, zzh = np.meshgrid(xhalf, zhalf, indexing="ij") # dims [xdims, zdims]
+  xxf, zzf = np.meshgrid(xfull, zfull, indexing="ij") # dims [xdims, zdims]
+  meanwvel = np.mean(wvel4d, axis=(0,1)) #avg over y and time axes
+  meanuvel = np.mean(uvel4d, axis=(0,1))
+  norm = np.sqrt(meanwvel**2 + meanuvel**2)
+
+  fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(9,6))
+  axs = axs.flatten()
+
+  pcm = axs[0].pcolormesh(xxh[:,:], zzh[:,:], meanwvel, cmap=cmaps[0])
+  plt.colorbar(pcm, ax=axs[0], location="top", label="w / m s$^{-1}$") 
+
+  pcm = axs[1].pcolormesh(xxh[:,:], zzh[:,:], meanuvel, cmap=cmaps[0])
+  plt.colorbar(pcm, ax=axs[1], location="top", label="u / m s$^{-1}$")
+
+  pcm = axs[2].pcolormesh(xxh[:,:], zzh[:,:], norm, cmap=cmaps[0])
+  plt.colorbar(pcm, ax=axs[2], location="top",
+               label="|wind velocity| / m s$^{-1}$")
+  axs[2].quiver(xxf, zzf, meanuvel, meanwvel)
+  
+  axs[0].set_ylabel("z /km")
+  for ax in axs:
+    ax.set_xlabel("x /km")
+    ax.set_aspect("equal")
+
+  fig.tight_layout()
+  if savefig:
+      savename = "/thermowindprofiles.png"
+      fig.savefig(binpath+savename, dpi=400,
+                  bbox_inches="tight", facecolor='w', format="png")
+      print("Figure .png saved as: "+binpath+savename)
+  plt.show()
