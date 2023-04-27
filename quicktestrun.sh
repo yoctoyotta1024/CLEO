1b28bb4846a0e8bfd51799fc88b854f29c542a2f
Clara Levante <yoctoyotta1024@yahoo.com>
2023-04-27 04:10:50 +0200

    sbatch script updated

--- quicktestrun.sh
+++ quicktestrun.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+#SBATCH --job-name=quickrun
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=30G
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=clara.bayley@mpimet.mpg.de
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh1126
+#SBATCH --output=./build/bin/quickrun_out.%j.out
+#SBATCH --error=./build/bin/quickrun_err.%j.out
 
 module load python3/2022.01-gcc-11.2.0
 source activate /work/mh1126/m300950/pySDenv
+module load gcc/11.2.0-gcc-11.2.0
 
-# cd build
+cd build
 # make clean && make
 ./src/runCLEO "../src/config/config.txt" "../libs/claras_SDconstants.hpp"
