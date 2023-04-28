1e3063571da758f66fbbf8d30c4fd78151a39cdb
Clara Levante <yoctoyotta1024@yahoo.com>
2023-04-28 08:35:36 +0200

    all validations working

--- create_initsuperdropsbinary_script.py
+++ create_initsuperdropsbinary_script.py
@@ -23,8 +23,8 @@ isfigures = [True, True]
 
 ### ------------ Number of Superdroplets per Gridbox ------------ ###
 # nsupers = 64 # int or dict of ints for number of superdroplets in a gridbox
-zlim = 500
-npergbx = 256
+zlim = 1500
+npergbx = 64
 nsupers = initattributes.nsupers_at_domain_base(gridfile, constsfile, npergbx, zlim)
 ### ---------------------------------------------------------------- ###
 
