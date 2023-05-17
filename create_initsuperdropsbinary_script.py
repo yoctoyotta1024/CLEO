0b1cd360bb6ad7aa52ac34f04d197ae08844ea11
clara bayley <yoctoyotta1024@yahoo.com>
2023-05-17 14:37:40 +0900

    testing SDs run

--- create_initsuperdropsbinary_script.py
+++ create_initsuperdropsbinary_script.py
@@ -24,7 +24,7 @@ isfigures = [True, True]
 ### ------------ Number of Superdroplets per Gridbox ------------ ###
 # nsupers = 64 # int or dict of ints for number of superdroplets in a gridbox
 zlim = 750
-npergbx = 8
+npergbx = 16
 nsupers = initattributes.nsupers_at_domain_base(gridfile, constsfile, npergbx, zlim)
 ### ---------------------------------------------------------------- ###
 
