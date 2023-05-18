b1cfc77e08f87dc41fb1802475f361ca4738ea6a
Clara Levante <yoctoyotta1024@yahoo.com>
2023-05-18 09:56:38 +0200

    better incorporation of concepts to runSDMstep template

--- libs/thermofromfile/run_thermofromfile.hpp
+++ libs/thermofromfile/run_thermofromfile.hpp
@@ -39,10 +39,10 @@ are read from file */
 
 namespace dlc = dimless_constants;
 
-template <typename A, typename B, typename C>
+template <class MSDs, SdmProcess P, Observer O>
 void timestep_thermofromfile(const int t_end,
                             const int couplstep,
-                            const RunSDMStep<A, B, C> &sdm,
+                            const RunSDMStep<MSDs, P, O> &sdm,
                             ThermodynamicsFromFile &thermodyn,
                             Kokkos::Random_XorShift64_Pool<> &genpool,
                             Kokkos::vector<GridBox> &gridboxes,
@@ -83,9 +83,9 @@ about thermodynamic state (changes) is possible. */
   return t_mdl + couplstep;
 }
 
-template <typename A, typename B, typename C>
+template <class MSDs, SdmProcess P, Observer O>
 void run_thermofromfile(const Config &config,
-                    const RunSDMStep<A, B, C> &sdm,
+                    const RunSDMStep<MSDs, P, O> &sdm,
                     const int t_end, const int couplstep)
 /* create superdroplets and gridboxes and then run uncoupled
 superdroplet model (SDM) using thermodynamics read from files */
@@ -126,10 +126,10 @@ superdroplet model (SDM) using thermodynamics read from files */
             << "\n ------------------------------------ \n";
 }
 
-template <typename A, typename B, typename C>
+template <class MSDs, SdmProcess P, Observer O>
 void timestep_thermofromfile(const int t_end,
                             const int couplstep,
-                            const RunSDMStep<A, B, C> &sdm,
+                            const RunSDMStep<MSDs, P, O> &sdm,
                             ThermodynamicsFromFile &thermodyn,
                             Kokkos::Random_XorShift64_Pool<> &genpool,
                             Kokkos::vector<GridBox> &gridboxes,
