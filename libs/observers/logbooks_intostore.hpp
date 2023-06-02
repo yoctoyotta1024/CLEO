e4c236c7e477f81241a6d329c3fef456e9c0fc71
clara bayley <yoctoyotta1024@yahoo.com>
2023-06-02 14:29:19 +0900

    new file for observign logbooks

---
+++ libs/observers/logbooks_intostore.hpp
@@ -0,0 +1,6 @@
+// Author: Clara Bayley
+// File: "logbooks_intostore.hpp"
+/* structures obeying the ObserveLbks
+concept for various ways of observing
+logbooks which ends up writing data
+into a (zarr) store on disk */
