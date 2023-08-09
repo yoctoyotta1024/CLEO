6d07d8040a1c7ccf99b3ddb6e6f1a5075d1c9008
Clara Levante <yoctoyotta1024@yahoo.com>
2023-08-09 11:22:18 +0200

    collision kinetics calc working as expected

--- roughpaper/testing.cpp
+++ roughpaper/testing.cpp
@@ -34,28 +34,101 @@ could compile with e.g.
 
 namespace dlc = dimless_constants;
 
+void cke_check(const double d1cm,
+               const double d2cm,
+               const Superdrop &drop1,
+               const Superdrop &drop2)
+{
+  const auto terminalv(SimmelTerminalVelocity{});
+  CollisionKinetics ck(terminalv);
+
+  std::cout << "cke check: " << ck.collision_kinetic_energy(drop1, drop2) << "\n";
+  
+  const double d1(d1cm/100); // [m]
+  const double d2(d2cm/100);
+  const double vdiff(terminalv(drop1) - terminalv(drop2));
+  const double ratio(std::pow(d1, 3.0) * std::pow(d2, 3.0) / (std::pow(d1, 3.0) + std::pow(d2, 3.0)));
+  const double ckeconst(dlc::Rho_l * dlc::RHO0 * std::numbers::pi / 12.0);
+  const double cke(vdiff * vdiff * ratio * ckeconst);
+  std::cout << "breakdown: " << dlc::Rho_l * dlc::RHO0 << ", " << vdiff << ", " << ratio << " -> " << cke << "\n";
+}
+
+void surfe_check(const double d1cm,
+               const double d2cm,
+               const Superdrop &drop1,
+               const Superdrop &drop2)
+{
+  const auto terminalv(SimmelTerminalVelocity{});
+  CollisionKinetics ck(terminalv);
+
+  std::cout << "surfe1: " << ck.surfenergy(drop1) << "\n";
+  std::cout << "surfe2: " << ck.surfenergy(drop2) << "\n";
+
+  const double d1(d1cm/100); // [m]
+  const double d2(d2cm/100);
+  const double sigma(7.28e-2);
+
+  std::cout << "1: " << sigma * std::numbers::pi * d1 * d1 << "\n"; 
+  std::cout << "2: " << sigma * std::numbers::pi * d2 * d2 << "\n"; 
+}
+
+void totsurfe_check(const double d1cm,
+               const double d2cm,
+               const Superdrop &drop1,
+               const Superdrop &drop2)
+{
+  const auto terminalv(SimmelTerminalVelocity{});
+  CollisionKinetics ck(terminalv);
+
+  std::cout << "totsurfe: " << ck.total_surfenergy(drop1, drop2) << "\n";
+  
+  const double d1(d1cm/100); // [m]
+  const double d2(d2cm/100);
+  const double sigma(7.28e-2);
+
+  std::cout << "tot: " << sigma * std::numbers::pi * (d1 * d1 + d2 * d2)<< "\n"; 
+}
+
+void coalsurfe_check(const double d1cm,
+               const double d2cm,
+               const Superdrop &drop1,
+               const Superdrop &drop2)
+{
+  const auto terminalv(SimmelTerminalVelocity{});
+  CollisionKinetics ck(terminalv);
+
+  std::cout << "totsurfe: " << ck.coal_surfenergy(drop1, drop2) << "\n";
+  
+  const double d1(d1cm/100); // [m]
+  const double d2(d2cm/100);
+  const double sigma(7.28e-2);
+
+  const double d3sum = std::pow((d1 * d1 * d1 + d2 * d2 * d2), 2.0/3.0);
+  std::cout << "tot: " << sigma * std::numbers::pi * d3sum << "\n"; 
+}
+
 int main()
 {
+  const double d1cm(0.1); // [cm]
+  const double d2cm(0.36); // [cm]
+
   auto sdIdGen = Superdrop::IDType::Gen{};
   
   const auto isolute(std::make_shared<const SoluteProperties>());
   const double im_sol = 3.0;
-  
+
   const unsigned long long ieps1 = 3;
-  const double iradius1 = 0.18 / 200.0 / dlc::R0;
+  const double iradius1 = d1cm / 200.0 / dlc::R0;
   Superdrop drop1(isolute, ieps1, iradius1, im_sol, 0.0, 0.0, 0.0, sdIdGen.next());
 
   const unsigned long long ieps2 = 6;
-  const double iradius2 = 0.0715 / 200.0 / dlc::R0;;
+  const double iradius2 = d2cm / 200.0 / dlc::R0;;
   Superdrop drop2(isolute, ieps2, iradius2, im_sol, 0.0, 0.0, 0.0, sdIdGen.next());
 
-  const auto terminalv(SimmelTerminalVelocity{});
-  CollisionKinetics ck(terminalv);
+  cke_check(d1cm, d2cm, drop1, drop2);
+  surfe_check(d1cm, d2cm, drop1, drop2);
+  totsurfe_check(d1cm, d2cm, drop1, drop2);
+  coalsurfe_check(d1cm, d2cm, drop1, drop2);
 
-  std::cout << ck.collision_kinetic_energy(drop1, drop2) << "\n";
-  std::cout << ck.surfenergy(drop1) << "\n";
-  std::cout << ck.surfenergy(drop2) << "\n";
-  std::cout << ck.total_surfenergy(drop1, drop2) << "\n";
-  std::cout << ck.coal_surfenergy(drop1, drop2) << "\n";
   return 0;
 }
