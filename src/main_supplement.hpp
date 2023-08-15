72761b3ed105c89f95812dbdf8ae0c32f6bb0501
Clara Levante <yoctoyotta1024@yahoo.com>
2023-08-15 09:06:50 +0200

    commment out unsued motion

--- src/main_supplement.hpp
+++ src/main_supplement.hpp
@@ -92,10 +92,10 @@ SdMotion auto create_sdmotion(const int motionstep)
 {
   // const auto terminalv = RogersYauTerminalVelocity{};
   // const auto terminalv = NullTerminalVelocity{};
-  const auto terminalv = SimmelTerminalVelocity{};
-  const SdMotion auto movewithsedi = MoveWithSedimentation(motionstep,
-                                                          &step2dimlesstime,
-                                                          terminalv);
+  // const auto terminalv = SimmelTerminalVelocity{};
+  // const SdMotion auto movewithsedi = MoveWithSedimentation(motionstep,
+  //                                                         &step2dimlesstime,
+  //                                                         terminalv);
 
   // auto rhotilda = [](const ThermoState &state)
   // { return state.press / (state.temp * (dlc::Rgas_dry + state.qvap * dlc::Rgas_v)); };
