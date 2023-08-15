1aaae8b29449e08264b1da5d74704173da8389ee
Clara Levante <yoctoyotta1024@yahoo.com>
2023-08-15 18:45:41 +0200

    using djvalid setup

--- src/main_supplement.hpp
+++ src/main_supplement.hpp
@@ -128,13 +128,13 @@ combined process of those two individual processes */
                                       config.cond_atol));
 
   /* create process for collision-coalescene in SDM */
-  // const auto terminalv = SimmelTerminalVelocity{};
-  // const auto probs_coal = CollCoalProb_LowList(terminalv);
-  const auto probs_coal = CollCoalProb_Golovin();
-  // const auto probs_coal = CollCoalProb_Long();
-  const auto coal(CollisionCoalescenceProcess(mdlsteps.collsubstep,
-                                              &step2realtime,
-                                              probs_coal));
+  // // const auto terminalv = SimmelTerminalVelocity{};
+  // // const auto probs_coal = CollCoalProb_LowList(terminalv);
+  // const auto probs_coal = CollCoalProb_Golovin();
+  // // const auto probs_coal = CollCoalProb_Long();
+  // const auto coal(CollisionCoalescenceProcess(mdlsteps.collsubstep,
+  //                                             &step2realtime,
+  //                                             probs_coal));
 
   // /* create process for collision-breakup in SDM */
   // const auto terminalv = SimmelTerminalVelocity{};
@@ -152,19 +152,19 @@ combined process of those two individual processes */
   //                                        terminalv,
   //                                        config.nfrags));
 
-  // const double coalrate(5e-7); // coalescence rate [s^-1]
-  // const double burate(0.0);  // breakup rate [s^-1]
-  // const auto djvalid(CollisionCoalBuConst(mdlsteps.collsubstep,
-  //                                         &step2realtime,
-  //                                         config.nfrags,
-  //                                         coalrate, burate));
+  const double coalrate(5e-7); // coalescence rate [s^-1]
+  const double burate(0.0);  // breakup rate [s^-1]
+  const auto djvalid(CollisionCoalBuConst(mdlsteps.collsubstep,
+                                          &step2realtime,
+                                          config.nfrags,
+                                          coalrate, burate));
 
   /* choose an amalgamation of sdm processes to make the returned sdmprocess */
   // const auto sdmprocess = cond >> coal;
-  const auto sdmprocess = coal;
+  // const auto sdmprocess = coal;
   // const auto sdmprocess = cond;
   // const auto sdmprocess = coalall;
-  // const auto sdmprocess = djvalid;
+  const auto sdmprocess = djvalid;
 
   return sdmprocess;
   // return NullProcess{};
