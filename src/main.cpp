0eef160e94bf15df4086db399174759ce82bf451
clara bayley <yoctoyotta1024@yahoo.com>
2023-10-13 01:17:48 +0200

    basic outline of main complete

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Thursday 12th October 2023
+ * Last Modified: Friday 13th October 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -34,17 +34,15 @@ struct FSStore
 {
 };
 
-struct ThermoSolver
+struct TimeSteps
 {
-  int run() const
-  {
-    std::cout << "Hi World\n";
-
-    return 0;
-  }
+private:
+  unsigned int t_end = 5;
+public:
+  unsigned int get_t_end() const { return t_end; }
 };
 
-struct GridboxMaps 
+struct GridBoxMaps 
 {
 };
 
@@ -60,40 +58,138 @@ struct Observer
 {
 };
 
-class CLEOSDM
+struct GridBoxes
 {
-private:
-  ThermoSolver solver;
-  GridboxMaps gbxmaps; // maps from gridbox indexes to domain coordinates
+};
+
+struct SuperDrops
+{
+};
+
+struct CLEOSDM
+{
+  TimeSteps tsteps; // timesteps for model (e.g. coupling and end time)
+  GridBoxMaps gbxmaps; // maps from gridbox indexes to domain coordinates
   Microphys microphys; // microphysical process
   Motion motion; // super-droplets' motion
   Observer obs; // observer
 
-public:
-  int run() const
+  GridBoxes generate_gridboxes() const
   {
-    std::cout << "Hello World\n";
+    return GridBoxes{};
+  }
 
+  SuperDrops generate_superdrops() const
+  {
+    return SuperDrops{};
+  }
+
+  int prepare_to_timestep(const GridBoxes &GBxs,
+                          const SuperDrops &SDs) const
+  {
     return 0;
+  }
+
+  void run_step(const unsigned int t_mdl) const
+  {
+    std::cout << "SDM Call @ t=" << t_mdl << "\n" ;
   }
 };
 
-int run_cleo(const CLEOSDM &sdm, const ThermoSolver &solver)
+struct CoupledDynamics
 {
-  solver.run();
-  sdm.run();
+  int prepare_to_timestep(const GridBoxes &GBxs,
+                          const SuperDrops &SDs) const
+  {
+    return 0;
+  }
+
+  void run_step(const unsigned int t_mdl) const
+  {
+    std::cout << "Dyn Call @ t=" << t_mdl << "\n" ;
+  }
+};
+
+struct RunStats
+{
+private:
+  Kokkos::Timer kokkostimer;
+  double time1;
+  double time2;
+
+public:
+  void pre_timestepping()
+  {
+    time1 = kokkostimer.seconds();
+  }
+
+  void post_timestepping()
+  {
+    time2 = kokkostimer.seconds();
+  }
+
+  void summary()
+  {
+    std::cout << "\n ----- CLEO run complete ----- \n"
+              << "       Duration: " << time2 << "s \n"
+              << "       Initialisation: " << time1 << "s \n"
+              << "       Timestepping: " << time2 - time1 << "s \n"
+              << "------------------------------- \n";
+  }
+};
+
+void start_step(const unsigned int t_mdl)
+{}
+
+unsigned int next_step(unsigned int t_mdl)
+{
+  return ++t_mdl;
+}
+
+int run_cleo(const CLEOSDM &sdm, const CoupledDynamics &coupldyn)
+{
+  // generate runtime objects
+  RunStats stats;
+  GridBoxes GBxs(sdm.generate_gridboxes());
+  SuperDrops SDs(sdm.generate_superdrops());
+
+  // prepare CLEO for timestepping
+  coupldyn.prepare_to_timestep(GBxs, SDs);
+  sdm.prepare_to_timestep(GBxs, SDs);
+  stats.pre_timestepping();
+  
+  // timestep CLEO from t=0 to t=t_end
+  int t_mdl = 0;
+  while (t_mdl <= sdm.tsteps.get_t_end())
+  {
+    /* start step (in general involves coupling) */
+    start_step(t_mdl);
+
+    /* advance SDM (optionally concurrent to dynamics solver) */
+    sdm.run_step(t_mdl);
+
+    /* advance dynamics solver (optionally concurrent to SDM) */
+    coupldyn.run_step(t_mdl);
+
+    /* proceed to next step (in general involves coupling) */
+    t_mdl = next_step(t_mdl);
+  }
+  stats.post_timestepping();
+  
+  // summary of runtime statistics
+  stats.summary();
 
   return 0;
 }
 
 int main(int argc, char *argv[])
 {
-  Kokkos::Timer kokkostimer;
-
   if (argc < 2)
   {
     throw std::invalid_argument("configuration file(s) not specified");
   }
+ 
+  Kokkos::Timer kokkostimer;
 
   /* read input parameters from configuration file(s) */
   const std::string_view configfile = argv[1];    // path to configuration file
@@ -102,20 +198,22 @@ int main(int argc, char *argv[])
   /* create zarr store for writing output to storage */
   FSStore fsstore;
   
-  /* Thermodynamics Solver to couple to CLEO */
-  const ThermoSolver solver;
-
-  /* CLEO SDM coupled to a Thermodyanmics Solver */
+  /* CLEO Super-Droplet Model (excluding coupled dynamics solver) */
   const CLEOSDM sdm;
+
+  /* Solver of Dynamics coupled to CLEO SDM */
+  const CoupledDynamics coupldyn;
 
   /* run coupled CLEO SDM with Kokkos */
   Kokkos::initialize(argc, argv);
   {
-    run_cleo(sdm, solver);
+    run_cleo(sdm, coupldyn);
   }
 
   Kokkos::finalize();
-  std::cout << "  ------ Total Duration: " << kokkostimer.seconds() << "s ----- \n";
+  const double ttot(kokkostimer.seconds());
+  std::cout << "-----\n Total Program Duration: "
+            << ttot << "s \n-----\n";
 
   return 0;
 }
