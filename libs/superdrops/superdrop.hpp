06e2f87974110541a59df42ec9eaba43d397076a
clara bayley <yoctoyotta1024@yahoo.com>
2023-10-13 16:58:50 +0200

    superdrop attributes seperated from superdrop

--- libs/superdrops/superdrop.hpp
+++ libs/superdrops/superdrop.hpp
@@ -40,7 +40,6 @@ namespace dlc = dimless_constants;
 
 struct SoluteProperties
 {
-  const double rho_l;   // (dimensionless) density of liquid in droplets
   const double rho_sol; // (dimensionless) density of solute in droplets
   const double mrsol;   // (dimensionless) Mr of solute
   const double ionic;   // degree ionic dissociation (van't Hoff factor)
@@ -54,37 +53,66 @@ struct SoluteProperties
   KOKKOS_INLINE_FUNCTION ~SoluteProperties() = default;
 };
 
+struct SuperdropAttrs
+/* attributes of a superdroplet*/
+{
+  double radius;                                  // radius of superdroplet
+  double msol;                                    // mass of solute dissovled
+  unsigned long long xi;                          // multiplicity of superdroplet
+  std::shared_ptr<const SoluteProperties> solute; // reference to solute properties
+}
+
 class Superdrop
 {
 private:
-  std::shared_ptr<const SoluteProperties> solute; // reference to solute properties
+  unsigned int sd_gbxindex; // matches index of gridbox the superdrop occupies
+  double coord3;            // a 3rd spatial coordinate of superdroplet (z)
+  double coord1;            // a 1st spatial coordinate of superdroplet (x)
+  double coord2;            // a 2nd spatial coordinate of superdroplet (y)
+  SuperdropAttrs attrs;     // attributes of a superdroplet
 
 public:
   using IDType = IntID; // type of ID (from superdrop_ids.hpp) to identify superdrop via integer
   // using IDType = EmptyID; // empty type of ID (for non-existent superdrop identity)
-
-  unsigned int sd_gbxindex;        // matches index of gridbox the superdrop occupies
-  unsigned long long eps;          // multiplicity of superdroplet
-  double radius;                   // radius of superdroplet
-  double m_sol;                    // mass of solute dissovled
-  double coord3;                   // a 3rd spatial coordinate of superdroplet (z)
-  double coord1;                   // a 1st spatial coordinate of superdroplet (x)
-  double coord2;                   // a 2nd spatial coordinate of superdroplet (y)
   [[no_unique_address]] IDType id; // superdroplet (unique) identity
 
   KOKKOS_INLINE_FUNCTION
-  Superdrop(const std::shared_ptr<const SoluteProperties> isolute,
-            const unsigned int isd_gbxindex,
-            const unsigned long long ieps, const double iradius,
-            const double im_sol, const double icoord3,
-            const double icoord1, const double icoord2,
+  Superdrop(const unsigned int isd_gbxindex,
+            const double icoord3,
+            const double icoord1,
+            const double icoord2,
+            const double iradius,
+            const double imsol,
+            const unsigned long long ixi,
+            const std::shared_ptr<const SoluteProperties> isolute,
             const IDType iid)
-      : solute(isolute), sd_gbxindex(isd_gbxindex), eps(ieps), radius(iradius),
-        m_sol(im_sol), coord3(icoord3), coord1(icoord1),
-        coord2(icoord2), id(iid) {}
+      : sd_gbxindex(isd_gbxindex),
+        coord3(icoord3), coord1(icoord1), coord2(icoord2),
+        attrs({iradius, imsol, ixi, isolute}),
+        id(iid) {}
+
+  KOKKOS_INLINE_FUNCTION
+  Superdrop(const unsigned int isd_gbxindex,
+            const double icoord3,
+            const double icoord1,
+            const double icoord2,
+            const SuperdropAttrs iattrs,
+            const IDType iid)
+      : sd_gbxindex(isd_gbxindex),
+        coord3(icoord3), coord1(icoord1), coord2(icoord2),
+        attrs(iattrs), id(iid) {} 
 
   KOKKOS_INLINE_FUNCTION Superdrop() = default;  // Kokkos requirement for a (dual)View
   KOKKOS_INLINE_FUNCTION ~Superdrop() = default; // Kokkos requirement for a (dual)View
+
+  unsigned int get_sd_gbxindex() { return sd_gbxindex; }
+  double get_coord3() { return attrs.coord3; }
+  double get_coord1() { return attrs.coord1; }
+  double get_coord2() { return attrs.coord2; }
+  double get_radius() { return attrs.radius; }
+  double get_msol() { return attrs.msol; }
+  double get_xi() { return attrs.xi; }
+  SoluteProperties get_solute() { return attrs.solute; }
 };
 
 struct Superdrops
