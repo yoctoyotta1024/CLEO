1814a85ab270b430770a36a88c0ae1e1ed878136
clara bayley <yoctoyotta1024@yahoo.com>
2023-10-14 11:17:00 +0200

    using kokkos aliases for view names

--- libs/cleoconstants.hpp
+++ libs/cleoconstants.hpp
@@ -6,7 +6,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Friday 13th October 2023
+ * Last Modified: Saturday 14th October 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
---
+++ libs/kokkosaliases.hpp
@@ -0,0 +1,35 @@
+/*
+ * ----- CLEO -----
+ * File: kokkosaliases.hpp
+ * Project: libs
+ * Created Date: Saturday 14th October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Saturday 14th October 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * aliases for Kokkos views / dualviews / vectors
+ */
+
+#include <Kokkos_Core.hpp>
+#include <Kokkos_DualView.hpp>
+
+#include "sdmdomain/gridbox.hpp"
+#include "superdrops/superdrop.hpp"
+
+using dualview_gbx = Kokkos::DualView<Gridbox *>; // dual view of gridboxes
+
+using viewh_gbx = Kokkos::View<Gridbox *>;        // view in host memory of gridboxes
+using viewh_constgbx = Kokkos::View<const Gridbox *>;        // view in host memory of gridboxes
+
+using viewd_gbx = Kokkos::View<Gridbox *>;        // view in device memory of gridboxes
+using viewd_constgbx = Kokkos::View<const Gridbox *>;        // view in device memory of gridboxes
+
+using viewd_supers = Kokkos::View<Superdrop *>;   // view in device memory of superdroplets
