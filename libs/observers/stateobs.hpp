2fb3240ebd714f1cbd3b45447258ab2533b982f4
clara bayley <yoctoyotta1024@yahoo.com>
2023-10-23 15:53:30 +0200

    state buffers and observer

--- libs/observers/stateobs.hpp
+++ libs/observers/stateobs.hpp
@@ -33,6 +33,7 @@
 #include "../kokkosaliases.hpp"
 #include "./observers.hpp"
 #include "gridboxes/gridbox.hpp"
+#include "superdrops/state.hpp"
 #include "zarr/twodstorage.hpp"
 #include "zarr/statebuffers.hpp"
 
@@ -45,35 +46,54 @@ StateObserver(const unsigned int interval,
 of each gridbox with a constant timestep 'interval'
 using an instance of the DoStateObs class */
 
-// class DoStateObs
-// /* observe variables in the state of each
-// gridbox and write them to repspective arrays
-// in a store as determined by the 
-// StateStorage instance */
-// {
-// private:
-//   using store_type = StateStorage<double>;
-//   std::shared_ptr<store_type> zarr;
+class DoStateObs
+/* observe variables in the state of each
+gridbox and write them to repspective arrays
+in a store as determined by the StateBuffers
+and TwoDMulitVarStorage types */
+{
+private:
+  using store_type = TwoDMultiVarStorage<StateBuffers<double>, State>;
+  std::shared_ptr<store_type> zarr;
   
-// public:
-//   DoStateObs(FSStore &store,
-//              const int maxchunk,
-//              const size_t ngbxs)
-//       : zarr(std::make_shared<store_type>(store, maxchunk,
-//                                           "<f8", ngbxs))
-// }
+public:
+  DoStateObs(FSStore &store,
+             const int maxchunk,
+             const size_t ngbxs)
+      : zarr(std::make_shared<store_type>(store, maxchunk,
+                                          "<f8", ngbxs, "")) {}
+  
+  void before_timestepping(const viewh_constgbx h_gbxs) const
+  {
+    std::cout << "observer includes StateObserver\n";
+  }                                        
 
-// inline Observer auto
-// StateObserver(const unsigned int interval,
-//               FSStore &store,
-//               const int maxchunk,
-//               const size_t ngbxs)
-// /* constructs observer of variables in the state 
-// of each gridbox with a constant timestep 'interval'
-// using an instance of the DoStateObs class */
-// {
-//   const auto obs = DoStateObs(store, maxchunk, ngbxs);
-//   return ConstTstepObserver(interval, obs);
-// }
+  void at_start_step(const unsigned int t_mdl,
+                     const viewh_constgbx h_gbxs) const
+  /* writes some variables from gridbox state
+  to 2-D zarr storages as determined by the
+  StateBuffers struct */
+  {
+    const size_t ngbxs(h_gbxs.extent(0));
+    for (size_t ii(0); ii < ngbxs; ++ii)
+    {
+      zarr->values_to_storage(h_gbxs(ii).state);
+    }
+    ++(zarr->nobs);
+  }
+};
+
+inline Observer auto
+StateObserver(const unsigned int interval,
+              FSStore &store,
+              const int maxchunk,
+              const size_t ngbxs)
+/* constructs observer of variables in the state 
+of each gridbox with a constant timestep 'interval'
+using an instance of the DoStateObs class */
+{
+  const auto obs = DoStateObs(store, maxchunk, ngbxs);
+  return ConstTstepObserver(interval, obs);
+}
 
 #endif // STATEOBS_HPP
