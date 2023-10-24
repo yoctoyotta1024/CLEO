c6055206d6dd74ff7a7c1de0716b7e7be7c688a0
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-24 11:46:11 +0200

    tidied up thermozarr

--- pySD/sdmzarr_src/thermodata.py
+++ pySD/sdmzarr_src/thermodata.py
@@ -29,3 +29,91 @@ def getds(dataset):
 
   return xr.open_dataset(dataset, engine="zarr", consolidated=False)
 
+def var4d_fromzarr(ds, ntime, ndims, key):
+  '''' returns 4D variable with dims
+  [time, y, x, z] from zarr dataset "ds" '''
+  
+  reshape = [ntime] + list(ndims)
+
+  return np.reshape(ds[key].values, reshape) 
+
+class Thermodata:
+
+  def __init__(self, dataset, ntime, ndims, CONSTS):
+    
+    ds = getds(dataset) 
+    self.CONSTS = CONSTS
+
+    self.press = var4d_fromzarr(ds, ntime, ndims, "press")
+    self.temp = var4d_fromzarr(ds, ntime, ndims, "temp")
+    self.qvap = var4d_fromzarr(ds, ntime, ndims, "qvap")
+    self.qcond = var4d_fromzarr(ds, ntime, ndims, "qcond") 
+    self.theta = self.potential_temp() 
+
+    self.press_units = ds["press"].units # probably hecto pascals
+    self.temp_units = ds["temp"].units # probably kelvin
+    self.theta_units = ds["temp"].units # probably kelvin
+
+  def potential_temp(self):
+    ''' potential temperature, theta '''
+
+    press = self.press*100 # convert from hPa to Pa
+    theta = thermoeqns.dry_pot_temp(self.temp, press,
+                                    self.qvap, self.CONSTS)        
+  
+    return theta
+  
+  def saturationpressure(self):
+    ''' saturation pressure in hectoPascals '''
+    
+    psat = thermoeqns.saturation_pressure(self.temp) # [Pa]
+    
+    return  psat / 100 # [hPa]
+
+  def vapourpressure(self):
+    '''returns vapour pressure in hectoPascals '''
+    
+    p_pascals = self.press*100 # convert from hPa to Pa
+    Mr_ratio = self.CONSTS["Mr_ratio"]
+    pvap = thermoeqns.vapour_pressure(p_pascals, self.qvap, Mr_ratio) # [Pa]
+   
+    return pvap / 100 # [hPa]
+
+  def relative_humidity(self):
+    ''' returns relative humidty and supersaturation '''
+    
+    p_pascals = self.press*100 # convert from hPa to Pa
+    Mr_ratio = self.CONSTS["Mr_ratio"]
+    relh = thermoeqns.relative_humidity(p_pascals, self.temp, 
+                                        self.qvap, Mr_ratio)
+    return relh
+
+  def supersaturation(self):
+    ''' returns relative humidty and supersaturation '''
+    
+    p_pascals = self.press*100 # convert from hPa to Pa
+    Mr_ratio = self.CONSTS["Mr_ratio"]
+    supersat = thermoeqns.supersaturation(p_pascals, self.temp, 
+                                          self.qvap, Mr_ratio)
+    return supersat
+
+  def __getitem__(self, key):
+    if key == "press":
+      return self.press
+    elif key == "temp":
+      return self.temp
+    elif key == "qvap":
+      return self.qvap
+    elif key == "qcond":
+      return self.qcond
+    elif key == "theta":
+      return self.theta
+    elif key == "relh":
+      return self.relative_humidity()
+    elif key == "supersat":
+      return self.supersaturation()
+    else:
+      err = "no known return provided for "+key+" key"
+      raise ValueError(err)
+
+
