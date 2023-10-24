b67979231caf24f7da2c8d2de9e7c52f2dc364f7
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-24 15:31:27 +0200

    copied init conds generation from archCLEO

---
+++ scripts/create_gbxboundariesbinary_script.py
@@ -0,0 +1,78 @@
+'''
+----- CLEO -----
+File: create_gbxboundariesbinary_script.py
+Project: scripts
+Created Date: Tuesday 24th October 2023
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Tuesday 24th October 2023
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+Copyright (c) 2023 MPI-M, Clara Bayley
+-----
+File Description:
+uses pySD module to create gridbox boundaries
+binary file for input to CLEO SDM
+'''
+
+import sys
+import numpy as np
+from pathlib import Path
+
+sys.path.append(sys.argv[1]) # path to pySD (same as to CLEO)
+from pySD.gbxboundariesbinary_src.create_gbxboundaries import *
+from pySD.gbxboundariesbinary_src.read_gbxboundaries import *
+
+### ----------------------- INPUT PARAMETERS ----------------------- ###
+### absolute or relative paths for build and CLEO directories
+path2CLEO = sys.argv[1]
+path2build = sys.argv[2]
+configfile = sys.argv[3]
+
+### booleans for [making+showing, saving] figures
+isfigures = [True, True]
+
+### essential paths and filenames
+constsfile = path2CLEO+"libs/cleoconstants.hpp"
+binariespath = path2build+"/share/"
+savefigpath = path2build+"/bin/"
+
+gridfile =  binariespath+"/dimlessGBxboundaries.dat" # note this should match config.txt
+
+### input parameters for zcoords of gridbox boundaries
+zmax = 100 # maximum z coord [m]
+zmin = 0 # minimum z coord [m]
+zdelta = 100 # even spacing
+zgrid = [zmin, zmax, zdelta] 
+
+### input parameters for x coords of gridbox boundaries
+xgrid = [0, 1, 100]
+
+### input parameters for y coords of gridbox boundaries
+ygrid = np.asarray([0, 100])
+# ygrid = [0, 1500, 200]
+### ---------------------------------------------------------------- ###
+
+
+### -------------------- BINARY FILE GENERATION--------------------- ###
+### ensure build, share and bin directories exist
+if path2CLEO == path2build:
+  raise ValueError("build directory cannot be CLEO")
+else:
+  Path(path2build).mkdir(exist_ok=True) 
+  Path(binariespath).mkdir(exist_ok=True) 
+
+### write gridbox boundaries binary
+write_gridboxboundaries_binary(gridfile, zgrid, xgrid, ygrid, constsfile)
+print_domain_info(constsfile, gridfile)
+
+### plot gridbox boundaries binary
+if isfigures[0]:
+  if isfigures[1]:
+    Path(savefigpath).mkdir(exist_ok=True) 
+  plot_gridboxboundaries(constsfile, gridfile, savefigpath, isfigures[1])
+### ---------------------------------------------------------------- ###
---
+++ scripts/create_thermobinaries_script.py
@@ -0,0 +1,106 @@
+'''
+----- CLEO -----
+File: create_thermobinaries_script.py
+Project: scripts
+Created Date: Tuesday 24th October 2023
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Tuesday 24th October 2023
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+Copyright (c) 2023 MPI-M, Clara Bayley
+-----
+File Description:
+uses pySD module to create binary files
+for the dynamics to read into CLEO when
+using a from file data for coupled dynamics
+'''
+
+
+import sys
+import numpy as np
+from pathlib import Path
+
+sys.path.append(sys.argv[1]) # path to pySD (same as to CLEO)
+from pySD.thermobinary_src import thermogen
+from pySD.thermobinary_src import create_thermodynamics as cthermo
+from pySD.thermobinary_src import read_thermodynamics as rthermo
+
+### ----------------------- INPUT PARAMETERS ----------------------- ###
+### --- absolute or relative paths for --- ###
+### ---   build and CLEO directories --- ###
+path2CLEO = sys.argv[1]
+path2build = sys.argv[2]
+configfile = sys.argv[3]
+
+### booleans for [making+showing, saving] figures
+isfigures = [True, True]
+
+### essential paths and filenames
+constsfile = path2CLEO+"libs/cleoconstants.hpp"
+binariespath = path2build+"/share/"
+savefigpath = path2build+"/bin/"
+
+gridfile =  binariespath+"/dimlessGBxboundaries.dat" # note this should match config.txt
+thermofile =  binariespath+"/dimlessthermo.dat"
+
+### --- Choose Initial Thermodynamic Conditions for Gridboxes  --- ###
+
+### --- Constant and Uniform --- ###
+P_INIT = 100000.0                       # initial pressure [Pa]
+TEMP_INIT = 273.15                      # initial parcel temperature [T]
+relh_init = 95.0                        # initial relative humidity (%)
+qc_init = 0.0                           # initial liquid water content []
+W_INIT = 0.0                            # initial vertical (z) velocity [m/s]
+U_INIT = 0.0                            # initial horizontal x velocity [m/s]
+V_INIT = 0.0                            # initial horizontal y velocity [m/s]
+thermodyngen = thermogen.ConstUniformThermo(P_INIT, TEMP_INIT, None,
+                                    qc_init, W_INIT, U_INIT, V_INIT,
+                                    relh=relh_init, constsfile=constsfile)
+
+# ### --- 2D Flow Field with Hydrostatic --- ###
+# ### ---       or Simple z Profile      --- ###
+# PRESS0 = 101500 # [Pa]
+# THETA = 289 # [K]
+# qcond = 0.0 # [Kg/Kg]
+# WMAX = 0.6 # [m/s]
+# VVEL = None # [m/s]
+# Zlength = 1500 # [m]
+# Xlength = 1500 # [m]
+
+# qvapmethod = "sratio"
+# Zbase = 750 # [m]
+# sratios = [0.85, 1.0001] # s_ratio [below, above] Zbase
+# # moistlayer = False
+# moistlayer = {
+#     "z1": 700,
+#     "z2": 800,
+#     "x1": 0,
+#     "x2": 750,
+#     "mlsratio": 1.005
+# }
+# thermodyngen = thermogen.ConstHydrostaticAdiabat(configfile, constsfile, PRESS0, 
+#                                         THETA, qvapmethod, sratios, Zbase,
+#                                         qcond, WMAX, Zlength, Xlength,
+#                                         VVEL, moistlayer)
+# thermodyngen = thermogen.SimpleThermo2Dflowfield(configfile, constsfile, PRESS0,
+#                                         THETA, qvapmethod, sratios, Zbase,
+#                                         qcond, WMAX, Zlength, Xlength,
+#                                         VVEL)
+### ---------------------------------------------------------------- ###
+
+### -------------------- BINARY FILE GENERATION--------------------- ###
+cthermo.write_thermodynamics_binary(thermofile, thermodyngen, configfile,
+                                    constsfile, gridfile)
+
+if isfigures[0]:
+    if isfigures[1]:
+        Path(savefigpath).mkdir(exist_ok=True) 
+    rthermo.plot_thermodynamics(constsfile, configfile, gridfile,
+                                          thermofile, savefigpath,
+                                          isfigures[1])
+### ---------------------------------------------------------------- ###
