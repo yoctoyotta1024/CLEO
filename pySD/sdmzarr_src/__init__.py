4eaedde8d3a13aa4a053bd0d9e0af534b3e6284a
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-24 11:02:31 +0200

    new python submodule in pySD to handle zarr output data

---
+++ pySD/sdmzarr_src/__init__.py
@@ -0,0 +1,20 @@
+'''
+----- CLEO -----
+File: __init__.py
+Project: sdmzarr_src
+Created Date: Tuesday 24th October 2023
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Tuesday 24th October 2023
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+Copyright (c) 2023 MPI-M, Clara Bayley
+-----
+File Description:
+'''
+
+
