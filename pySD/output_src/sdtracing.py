8a4461d27ce2fd116532223eaf5a820391fd921c
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-24 19:01:20 +0200

    functions for tracing superdroplets

--- pySD/output_src/sdtracing.py
+++ pySD/output_src/sdtracing.py
@@ -22,8 +22,9 @@ e.g. for tracing their trajectories
 
 import numpy as np
 import awkward as ak
+import random 
 
-def superdrop_attribute(sddata, Id, attr):
+def attribute_for_superdroplet(sddata, Id, attr):
   '''selects attribute from sddata belonging
   to superdroplet with identitiy 'Id'
   at every output time '''
@@ -42,40 +43,39 @@ def superdrop_attribute(sddata, Id, attr):
 
   return ak.flatten(attr4Id, axis=1) # remove excess dimension
 
-def attrtimeseries_for_superdropssample(sddata, attr,
-                                        ndrops2sample=0,
-                                        minid=0, maxid=0,
-                                        ids=[]):
+def attribute_for_superdroplets_sample(sddata, attr, ndrops2sample=0,
+                                       minid=0, maxid=0, ids=[]):
   ''' returns 2D array with dimensions [time, SD]
-  containing attribute data over time for 'ndrops'
-  randomly selected from superdrops with id in
-  range [minid, maxid] '''
+  containing attribute data over time for a sample of 
+  superdroplets. Sample is either for superdroplets with
+  specific Ids in 'ids' list, or sample if 'ndrops2sample'
+  randomly selected superdrops with Ids in the range 
+  [minid, maxid] '''
 
   if ids == []:
     population = list(range(minid, maxid, 1))
-    sampled_ids = random.sample(population, ndrops2sample)
+    sample = random.sample(population, ndrops2sample)
   else:
-    sampled_ids = ids
+    sample = ids
 
   ndrops_attr = []
-  for id in sampled_ids: 
-    attr_timeseries = attrtimeseries_for_1superdrop(sddata, id, attr)
-    ndrops_attr.append(attr_timeseries)
+  for id in sample: 
+    attr4Id = attribute_for_superdroplet(sddata, id, attr)
+    ndrops_attr.append(attr4Id)
   
   return np.asarray(ndrops_attr).T
 
 def attr_at_times(attrdata, time, times2sel):
-  '''selects attribute at given times
-   (for all superdroplets in sddata)'''
+  '''selects attribute (for all superdroplets)
+  at times closest to 'times2sel' '''
 
-  selected_attr = [] # list containing an attribute at selected times
+  inds = [] # list containing indexes of times closest to times2sel
   for t in times2sel:
-    ind = np.argmin(abs(time-t))
-    selected_attr.append(attrdata[ind]) 
-  
-  return ak.Array(selected_attr)
+    inds.append(np.argmin(abs(time-t)))
 
-def attrs_at_times(sddata, time, times2sel, attrs2sel):
+  return attrdata[inds]
+
+def attributes_at_times(sddata, time, times2sel, attrs2sel):
   '''selects attributes at given times from
   sddata (for all superdroplets in sddata)'''
 
@@ -87,30 +87,3 @@ def attrs_at_times(sddata, time, times2sel, attrs2sel):
     selected_data[attr] = selattr_data
   
   return selected_data
-
-def surfaceprecip_fromdataset(dataset, gbxs):
-
-    ds = get_rawdataset(dataset)
-
-    sdindex = ak.unflatten(ds["sdindex"].values, ds["raggedcount"].values)
-    radius = ak.unflatten(ds["radius"].values, ds["raggedcount"].values)
-    eps = ak.unflatten(ds["eps"].values, ds["raggedcount"].values)
-
-    r3sum = []
-    for ti in range(ds.time.shape[0]-1):
-        sd_ti, r_ti, eps_ti = sdindex[ti], radius[ti], eps[ti]
-        sds_gone = set(sd_ti) - set(sdindex[ti+1]) # set of SD indexes that have left domain during timestep ti -> ti+1
-        isgone = np.where(np.isin(sd_ti, list(sds_gone))) # indexes in ragged arrays of SDs that leave during timestep ti -> ti+1
-        r3sum.append(np.dot(r_ti[isgone]**3, eps_ti[isgone])) # sum of (real) droplet radii^3 that left domain [microns^3]
-    precipvol = 4/3 * np.pi * np.asarray(r3sum) / (1e18) # volume of water that left domain [m^3]
-
-    domainy = np.amax(gbxs["yhalf"]) - np.amin(gbxs["yhalf"]) # [m]
-    domainx = np.amax(gbxs["xhalf"]) - np.amin(gbxs["xhalf"]) # [m]
-    deltat = np.diff(ds["time"].values) / 60 / 60 # [hrs]
-    preciprate = precipvol * 1000 / (domainx * domainy) / deltat # [mm/hr]
-
-    precipaccum = np.cumsum(preciprate * deltat) # [mm]
-    preciprate = np.insert(preciprate, 0, 0) # at t=0, precip rate = 0
-    precipaccum = np.insert(precipaccum, 0, 0) # at t=0, accumulated precip = 0
-
-    return preciprate, precipaccum # [mm/hr] , [mm]
