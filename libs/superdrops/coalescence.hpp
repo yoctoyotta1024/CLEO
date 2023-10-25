ed3eac07d9e6abfaffc884b78a7ec797ad11ee05
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-25 08:50:34 +0200

    file for collision-coalescence

---
+++ libs/superdrops/coalescence.hpp
@@ -0,0 +1,43 @@
+/*
+ * ----- CLEO -----
+ * File: coalescence.hpp
+ * Project: superdrops
+ * Created Date: Friday 13th October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Wednesday 25th October 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * class that enacts collision-coalescence events
+ * in superdroplet model according to Shima et al. 2009.
+ * Coalescence struct satisfies SDinGBxPairEnactX concept
+ * used in Collisions struct */
+
+#ifndef COALESCENCE_HPP
+#define COALESCENCE_HPP
+
+#include <Kokkos_Core.hpp>
+
+struct DoCoalescence
+{
+private:
+public:
+};
+
+inline MicrophysicalProcess auto
+CollCoalescence(const unsigned int interval)
+/* constructs Microphysical Process for
+collision-coalescence of superdroplets according to
+Shima et al. 2009 with a constant timestep 'interval'*/
+{
+  return ConstTstepMicrophysics(interval, DoCoalescence{});
+}
+
+#endif // COLLISIONCOALESCENCE_HPP
