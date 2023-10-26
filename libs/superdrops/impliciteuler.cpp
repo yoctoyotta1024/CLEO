a937b2a0b1c32ce56513db3e2513f133df79b110
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-26 12:55:30 +0200

    implicit euler cpp file (empty)"

---
+++ libs/superdrops/impliciteuler.cpp
@@ -0,0 +1,25 @@
+/*
+ * ----- CLEO -----
+ * File: impliciteuler.cpp
+ * Project: superdrops
+ * Created Date: Thursday 26th October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 26th October 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * Header for class implementing the implicit euler method
+ * for radial growth/shrink of each superdroplet due to
+ * condensation and diffusion of water vapour according
+ * to equations from "An Introduction To Clouds From
+ * The Microscale to Climate" by Lohmann, Luond and
+ * Mahrt, 1st edition." and Shima et al. 2009 */
+
+#include "./impliciteuler.hpp"
