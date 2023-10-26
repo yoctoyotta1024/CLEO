4468f5b22ac159fd14315edd288a883ca365c7bc
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-26 14:28:54 +0200

    condensation method tidied and given to implicit iteration

--- libs/superdrops/impliciteuler.hpp
+++ libs/superdrops/impliciteuler.hpp
@@ -28,6 +28,93 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Pair.hpp>
 
+
+struct ImplicitIteration
+{
+  unsigned int niters; // number of NR iterations to try before testing convergence
+  double subdelt;      // timestep of implicit method (at each substep >= niter NR iterations occur)
+  double rtol;         // relative tolerance for convergence of NR method
+  double atol;         // abolute tolerance for convergence of NR method
+
+  double s_ratio;
+  double akoh;
+  double bkoh;
+  double ffactor;
+
+  double initialguess(const double rprev) const;
+  /* returns appropriate initial value (ie. a reasonable guess) for
+  'ziter' to use as first iteration of newton raphson method in
+  rootfinding algorithm for timestepping condensation/evaporation ODE.
+  Criteria is as in SCALE-SDM for making initial guess for given droplet
+  much greater than its (activation radius)^2 if the
+  supersaturation > its activation supersaturation  */
+
+  double initialguess_shima(const double rprev) const;
+  /* returns appropriate initial value (ie. a reasonable guess) for
+  'ziter' to use as first iteration of newton raphson method in
+  rootfinding algorithm for timestepping condensation/evaporation ODE.
+  Criteria for modifying guess from rprev^2 are adapted from SCALE-SDM.
+  Second criteria is that initial guess >= 'r1sqrd', where r1 is the
+  equilibrium radius of a given droplet when s_ratio=1  */
+
+  double newtonraphson_niterations(const double rprev,
+                                   double ziter) const;
+  /* Timestep condensation ODE by delt given initial guess for ziter,
+  (which is usually radius^squared from previous timestep). Uses newton
+  raphson iterative method to find new value of radius that converges
+  on the root of the polynomial g(ziter) within the tolerances of the
+  ImpIter instance. Uniquesol method assumes that solution to g(ziter)=0
+  is unique and therefore Newton Raphson root finding algorithm converges
+  quickly. This means method can be used with comparitively large tolerances
+  and timesteps, and the maximum number of iterations is small. After
+  'niters' iterations, convergence criteria is tested and futher
+  iterations undertaken if not yet converged. */
+
+  double newtonraphson_untilconverged(const unsigned int iterlimit,
+                                      const double rprev,
+                                      double ziter) const;
+  /*  Timestep condensation ODE by delt given initial guess for ziter,
+  (which is usually radius^squared from previous timestep). Uses
+  newton raphson iterative method to find new value of radius that
+  converges on the root of the polynomial g(ziter) within the tolerances
+  of the ImpIter instance. After every iteration, convergence criteria is
+  tested and error is raised if method does not converge within
+  'iterlimit' iterations. Otherwise returns new value for the radius
+  (which is the radius at timestep 't+subdelt'. Refer to section 5.1.2 Shima
+  et al. 2009 and section 3.3.3 of Matsushima et al. 2023 for more details. */
+
+  std::pair<bool, double>
+  iterate_rootfinding_algorithm(const double rprev, double ziter) const;
+  /* function performs one iteration of Newton Raphson rootfinding
+  method and returns updated value of radius^2 alongside a boolean that
+  is false if algorithm has converged */
+
+  double ode_gfunc(const double rprev, const double rsqrd) const;
+  /* returns g(z) / z * delt for g(z) function used in root finding
+  Newton Raphson Method for dr/dt condensation / evaporation ODE.
+  ODE is for radial growth/shrink of each superdroplet due to
+  condensation and diffusion of water vapour according to
+  equations from "An Introduction To Clouds...."
+  (see note at top of file). Note: z = ziter = radius^2 */
+
+  double ode_gfuncderivative(const double rsqrd) const;
+  /* dg(z)/dz * delt, where dg(z)/dz is derivative of g(z) with
+  respect to z=rsqrd. g(z) is polynomial to find root of using
+  Newton Raphson Method. */
+
+  bool isnotconverged(const double gfunciter,
+                      const double gfuncprev) const
+  /* boolean where True means criteria for ending newton raphson
+  iterations has not yet been met. Criteria is standard local error
+  test: |iteration - previous iteration| < RTOL * |iteration| + ATOL */
+  {
+    const double converged = rtol * std::abs(gfunciter) + atol;
+    const double currentvalue = std::abs(gfunciter - gfuncprev);
+
+    return (currentvalue >= converged); // true means it's not yet converged
+  }
+};
+
 class ImplicitEuler
 /* class for the Implicit Euler (IR) integration of
   to superdroplet condensational growth ODE. Uses a
@@ -42,16 +129,9 @@ private:
   double subdelt;      // number of substeps to condensation method when supersat close to 1 (0 = false ie. none)
 
   KOKKOS_INLINE_FUNCTION
-  double substepped_implicitmethod(const unsigned int iters,
-                                   const double delt,
-                                   const double rtol,
-                                   const double atol,
-                                   const double s_ratio,
-                                   const double akoh,
-                                   const double bkoh,
-                                   const double ffactor,
-                                   const double rprev,
-                                   double subdelt) const;
+  double substepped_implicitmethod(const ImplicitIteration &implit,
+                                   const unsigned int nsubsteps,
+                                   const double rprev) const;
 
 public:
   ImplicitEuler(const unsigned int niters,
@@ -97,11 +177,11 @@ public:
 
 /* -----  ----- TODO: move functions below to .cpp file ----- ----- */
 
-KOKKOS_INLINE_FUNCTION
-double ImplicitEuler::solve_condensation(const double s_ratio,
-                                         const Kokkos::pair<double, double> kohler_ab,
-                                         const double ffactor,
-                                         const double rprev) const
+KOKKOS_INLINE_FUNCTION double
+ImplicitEuler::solve_condensation(const double s_ratio,
+                                  const Kokkos::pair<double, double> kohler_ab,
+                                  const double ffactor,
+                                  const double rprev) const
 /* forward timestep previous radius 'rprev' by delt using an implicit
 euler method to integrate the condensation/evaporation ODg. Implict
 timestepping equation defined in section 5.1.2 of Shima et al. 2009
@@ -121,9 +201,12 @@ Matsushima et al. 2023 for more details. */
   /* if supersaturation close to s_act, activation or
   deactivation might occur so perform subtimestepping */
   {
-    return substepped_implicitmethod(niters, delt, maxrtol, maxatol,
-                                     s_ratio, akoh, bkoh, ffactor,
-                                     rprev, subdelt);
+    const unsigned int nsubs = std::ceil(delt / subdelt);
+    const double subt = delt / (double)nsubs;
+    const ImplicitIteration implit{niters, subt, maxrtol, maxatol,
+                                   s_ratio, akoh, bkoh, ffactor};
+
+    return substepped_implicitmethod(implit, nsubs, rprev);
   }
 
   // else
@@ -141,30 +224,17 @@ Matsushima et al. 2023 for more details. */
   return rprev; // TODO delete
 }
 
-KOKKOS_INLINE_FUNCTION
-double ImplicitEuler::substepped_implicitmethod(const unsigned int iters,
-                                                const double delt,
-                                                const double rtol,
-                                                const double atol,
-                                                const double s_ratio,
-                                                const double akoh,
-                                                const double bkoh,
-                                                const double ffactor,
-                                                const double rprev,
-                                                double subdelt) const
+KOKKOS_INLINE_FUNCTION double
+ImplicitEuler::substepped_implicitmethod(const ImplicitIteration &implit,
+                                         const unsigned int nsubsteps,
+                                         const double rprev) const
 {
-  const unsigned int nsubs = std::ceil(delt / subdelt);
-  subdelt = delt / (double)nsubs;
-
-  // const ImpIter impit{niters, subdelt, maxrtol, maxatol,
-  //                       s_ratio, akoh, bkoh, ffactor};
-
   double subr(rprev);
-  // for (unsigned int n=0; n < nsubs; ++n)
-  // {
-  //   double init_ziter(impit.initialguess(subr));
-  //   subr = impit.newtonraphson_niterations(subr, init_ziter);
-  // }
+  for (unsigned int n = 0; n < nsubsteps; ++n)
+  {
+    // double init_ziter(implit.initialguess(subr));
+    // subr = implit.newtonraphson_niterations(subr, init_ziter);
+  }
   return subr;
 }
 
