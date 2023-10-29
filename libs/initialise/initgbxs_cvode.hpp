0780a99cb6fcdf1fb4881243be0d89f6e0765889
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-29 17:47:34 +0100

    initgbxs when using cvode

---
+++ libs/initialise/initgbxs_cvode.hpp
@@ -0,0 +1,112 @@
+/*
+ * ----- CLEO -----
+ * File: initgbxs_cvode.hpp
+ * Project: initialise
+ * Created Date: Tuesday 17th October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Sunday 29th October 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * struct for griboxes' initial conditions
+ * for CLEO SDM (e.g. volumes and thermodynamics)
+ * when coupled to cvode thermodynamics solver.
+ * Struct can be used by InitConds as
+ * GbxInitConds type
+ */
+
+#ifndef INITGBXS_CVODE_HPP
+#define INITGBXS_CVODE_HPP
+
+#include <vector>
+#include <utility>
+
+#include "./config.hpp"
+
+struct InitGbxsCvode
+/* struct containing functions which return data
+for the initial conditions needed to create
+gridboxes e.g. via the create_gbxs function */
+{
+private:
+  size_t ngbxs;
+
+public:
+  InitGbxsCvode(const Config &config)
+      : ngbxs(config.ngbxs) {}
+
+  auto get_ngbxs() const { return ngbxs; }
+
+  size_t get_size() const
+  {
+    return volume().size();
+  }
+
+  std::vector<double> volume() const
+  {
+    std::vector<double> volume(ngbxs, 1000);
+
+    return volume;
+  }
+
+  std::vector<double> temp() const
+  {
+    std::vector<double> temp(ngbxs, 1000);
+
+    return temp;
+  }
+
+  std::vector<double> press() const
+  {
+    std::vector<double> press(ngbxs, 1000);
+
+    return press;
+  }
+
+  std::vector<double> qvap() const
+  {
+    std::vector<double> qvap(ngbxs, 1000);
+
+    return qvap;
+  }
+
+  std::vector<double> qcond() const
+  {
+    std::vector<double> qcond(ngbxs, 1000);
+
+    return qcond;
+  }
+
+  std::vector<std::pair<double, double>> wvel() const
+  {
+    auto w = std::make_pair<double, double>(0.0, 0.0);
+    std::vector<std::pair<double, double>> wvel(ngbxs, w);
+
+    return wvel;
+  }
+
+  std::vector<std::pair<double, double>> uvel() const
+  {
+    auto u = std::make_pair<double, double>(0.0, 0.0);
+    std::vector<std::pair<double, double>> uvel(ngbxs, u);
+
+    return uvel;
+  }
+
+  std::vector<std::pair<double, double>> vvel() const
+  {
+    auto v = std::make_pair<double, double>(0.0, 0.0);
+    std::vector<std::pair<double, double>> vvel(ngbxs, v);
+
+    return vvel;
+  }
+};
+
+#endif // INITGBXS_CVODE_HPP
