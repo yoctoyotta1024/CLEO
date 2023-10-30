c7dd217380addc95b1e5ac9437e5ba457d9e6254
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-31 00:37:51 +0100

    new files for couplign when using fromfile dynamics

---
+++ libs/coupldyn_fromfile/fromfilecomms.cpp
@@ -0,0 +1,24 @@
+/*
+ * ----- CLEO -----
+ * File: fromfilecomms.cpp
+ * Project: coupldyn_fromfile
+ * Created Date: Tuesday 31st October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Tuesday 31st October 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * send and receive dynamics functions
+ * for SDM when coupled to the fromfile
+ * dynamics solver
+ */
+
+
+#include "./fromfilecomms.hpp"
---
+++ libs/coupldyn_fromfile/fromfilecomms.hpp
@@ -0,0 +1,28 @@
+/*
+ * ----- CLEO -----
+ * File: fromfilecomms.hpp
+ * Project: coupldyn_fromfile
+ * Created Date: Tuesday 31st October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Tuesday 31st October 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * send and receive dynamics functions
+ * for SDM when coupled to the fromfile
+ * dynamics solver
+ */
+
+
+#ifndef FROMFILECOMMS_HPP 
+#define FROMFILECOMMS_HPP 
+
+
+#endif // FROMFILECOMMS_HPP
