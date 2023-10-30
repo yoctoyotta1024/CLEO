247e548175a77bfaf7302ba7052d2a874529653e
Clara Levante <yoctoyotta1024@yahoo.com>
2023-10-31 00:54:57 +0100

    fromfile comms created but need filling

--- libs/coupldyn_fromfile/fromfilecomms.hpp
+++ libs/coupldyn_fromfile/fromfilecomms.hpp
@@ -20,9 +20,31 @@
  * dynamics solver
  */
 
+#ifndef FROMFILECOMMS_HPP
+#define FROMFILECOMMS_HPP
 
-#ifndef FROMFILECOMMS_HPP 
-#define FROMFILECOMMS_HPP 
+#include "../kokkosaliases.hpp"
+#include "./fromfiledynamics.hpp"
+#include "gridboxes/gridbox.hpp"
+#include "superdrops/state.hpp"
 
+struct FromFileComms
+/* 1-way coupling from coupldyn to CLEO's gridboxes where
+coupldyn sends information to h_gbxs but doesn't
+receive any back. Struct obeys coupling comms concept */
+{
+  template <typename CD = FromFileDynamics>
+  void receive_dynamics(const FromFileDynamics &ffdyn,
+                        const viewh_gbx h_gbxs) const;
+  /* update Gridboxes' states using information
+  received from FromFileDynamics solver for
+  1-way coupled to CLEO SDM */
+
+  template <typename CD = FromFileDynamics>
+  void send_dynamics(const viewh_constgbx h_gbxs,
+                     FromFileDynamics &ffdyn) const {}
+  /* send information from Gridboxes'
+  states to coupldyn is null */
+};
 
 #endif // FROMFILECOMMS_HPP
