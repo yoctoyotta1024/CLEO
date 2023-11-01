8a6b07d7cac86cf0b54893b63a6d96a652ba94a1
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-01 15:43:53 +0100

    creation files for gbxmaps

---
+++ libs/cartesiandomain/createmaps_fromgridfile.cpp
@@ -0,0 +1,21 @@
+/*
+ * ----- CLEO -----
+ * File: create_cartesianmaps_from_file.cpp
+ * Project: cartesiandomain
+ * Created Date: Wednesday 1st November 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Wednesday 1st November 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * functionality for creating a cartesian maps struct
+ * from a binary file containing gridbox's indexes and
+ * their coordinate (upper and lower) boundaries
+ */
---
+++ libs/cartesiandomain/createmaps_fromgridfile.hpp
@@ -0,0 +1,23 @@
+/*
+ * ----- CLEO -----
+ * File: create_cartesianmaps_from_file.hpp
+ * Project: cartesiandomain
+ * Created Date: Wednesday 1st November 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Wednesday 1st November 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * functions for creating a cartesian maps struct
+ * from a binary file containing gridbox's indexes and
+ * their coordinate (upper and lower) boundaries
+ */
+
+
