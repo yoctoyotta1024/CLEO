fe5e8f2883cf1264ae8efd4bd523b1b7a79ef06e
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-01 13:20:30 +0100

    use lower version of cmake

--- libs/cartesiandomain/cartesianmaps.hpp
+++ libs/cartesiandomain/cartesianmaps.hpp
@@ -6,7 +6,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Sunday 29th October 2023
+ * Last Modified: Wednesday 1st November 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -34,12 +34,25 @@ namespace dlc = dimless_constants;
 
 struct CartesianMaps 
 /* type satisfying GridboxMaps concept specifically
-for gridboxes defined on in a cartesian C grid */
+for gridboxes defined on in a cartesian C grid with
+equal area and volume for each gridbox */
 {
 private:
 
 public:
   CartesianMaps(const Config &config){}
+  /* initilaises coord[X]bounds maps (for X = 1, 2, 3,
+  corresponding to x, y, z) to map between gbxindexes and
+  gridbox boundaries in a cartiesian domain.
+  The keys of each map are the gridbox indexes. The
+  corresponding value is that gridbox's {upper boundary, lower boundary}.
+  In a non-3D case, coordinates of the gridbox boundaries for unused
+  dimensions are the min/max possible doubles of computer (numerical
+  limits), however the area and volume remain finite. E.g. In the 0-D
+  case, the idx2bounds maps have 1 {key, value} for gridbox 0 which
+  are the upper and lower numerical limits, whilst the volume is 
+  determined by reading the gridfile */
+
 
   KOKKOS_INLINE_FUNCTION
   Kokkos::pair<double, double>
@@ -62,6 +75,9 @@ public:
     return {LIMITVALUES::llim, LIMITVALUES::ulim};
   }
 
+  double get_area() const { return 0.0; } // TODO
+
+  double get_volume() const { return 0.0; } // TODO
 };
 
 #endif // CARTESIANMAPS_HPP
