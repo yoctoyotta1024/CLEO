a16a0adcf13e10c8afb748d9155b6a2272e8be0f
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-02 14:05:57 +0100

    mem leak not solute creation

--- libs/initialise/initsupers_frombinary.cpp
+++ libs/initialise/initsupers_frombinary.cpp
@@ -6,7 +6,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Tuesday 31st October 2023
+ * Last Modified: Thursday 2nd November 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
