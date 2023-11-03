102207575798d3a084d0f02e9ecd134116f7dfc5
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-03 17:42:19 +0100

    need to fix gpu comilation of maps

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Created Date: Thursday 12th October 2023
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Wednesday 25th October 2023
+Last Modified: Friday 3rd November 2023
 Modified By: CB
 -----
 License: BSD 3-Clause "New" or "Revised" License
