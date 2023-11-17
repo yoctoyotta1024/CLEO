ee67827a29bb95ad22d323bb117925f9c9102f68
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-17 16:43:53 +0100

    null coupled dynamics solver

---
+++ libs/coupldyn_null/nulldynamics.hpp
@@ -0,0 +1,49 @@
+/*
+ * ----- CLEO -----
+ * File: nulldynamics.hpp
+ * Project: coupldyn_null
+ * Created Date: Friday 17th November 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Friday 17th November 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * struct obeying coupleddynamics concept
+ * for dynamics solver in CLEO where there
+ * is no dynamics nor coupling to SDM
+ */
+
+#ifndef NULLDYNAMICS_HPP
+#define NULLDYNAMICS_HPP
+
+#include <Kokkos_Core.hpp>
+
+#include "../kokkosaliases.hpp"
+
+struct NullDynamics
+{
+  KOKKOS_INLINE_FUNCTION
+  auto get_couplstep() const
+  {
+    return LIMITVALUES::uintmax;
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  void prepare_to_timestep() const {}
+
+  KOKKOS_INLINE_FUNCTION
+  bool on_step(const unsigned int t_mdl) const { return false; }
+
+  KOKKOS_INLINE_FUNCTION
+  void run_step(const unsigned int t_mdl,
+                const unsigned int t_next) const {}
+};
+
+#endif // NULLDYNAMICS_HPP
