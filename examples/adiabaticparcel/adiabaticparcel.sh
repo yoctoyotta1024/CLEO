73425d0ed680d106ea01ccb3e21f5537e889d7b0
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-17 12:26:57 +0100

    correcy python

--- examples/adiabaticparcel/adiabaticparcel.sh
+++ examples/adiabaticparcel/adiabaticparcel.sh
@@ -16,11 +16,12 @@
 ### ----  and paths for CLEO and build directories  ---- ###
 module load gcc/11.2.0-gcc-11.2.0
 module load python3/2022.01-gcc-11.2.0
-source activate /work/mh1126/m300950/condaenvs/cleoenv
+source activate /work/mh1126/m300950/condaenvs/superdropsenv
 path2CLEO=${HOME}/testCLEOfire/
 path2build=${HOME}/testCLEOfire/build/
 configfile=${path2CLEO}/examples/adiabaticparcel/src/config/config.txt 
-python=python
+
+python=/work/mh1126/m300950/condaenvs/superdropsenv/bin/python
 gxx="g++"
 gcc="gcc"
 ### ---------------------------------------------------- ###
