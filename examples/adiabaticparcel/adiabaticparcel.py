51a645971635e70c65de89eb5c249f08cd7316f4
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-17 11:37:08 +0100

    bash and pythonn scripts for runing adia0D test

---
+++ examples/adiabaticparcel/adiabaticparcel.py
@@ -0,0 +1,93 @@
+'''
+----- CLEO -----
+File: adiabaticparcel.py
+Project: adiabaticparcel
+Created Date: Friday 17th November 2023
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Friday 17th November 2023
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+Copyright (c) 2023 MPI-M, Clara Bayley
+-----
+File Description:
+Script compiles and runs CLEO adia0D to
+create data and plots similar to Figure 5 of
+"On the CCN (de)activation nonlinearities"
+S. Arabas and S. Shima 2017 to show
+example of cusp birfucation for
+0D adaibatic parcel exapansion and contraction.
+Note: SD(M) = superdroplet (model)
+'''
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+path2CLEO = sys.argv[1]
+path2build = sys.argv[2]
+configfile = sys.argv[3]
+
+# for imports from pySD package
+sys.path.append(path2CLEO)
+from pySD.gbxboundariesbinary_src import create_gbxboundaries as cgrid
+from pySD.gbxboundariesbinary_src import read_gbxboundaries as rgrid
+from pySD.initsuperdropsbinary_src import *
+
+############### INPUTS ##################
+# path and filenames for creating SD initial conditions and for running model
+constsfile = path2CLEO+"libs/cleoconstants.hpp"
+binpath = path2build+"bin/"
+initSDsfile = binpath+"adai0D_dimlessSDsinit.dat"
+gridfile = binpath+"adia0D_dimlessGBxboundaries.dat"
+
+# path and file names for plotting results
+setupfile = binpath+"adia0Dsetup.txt"
+dataset = binpath+"adia0Dsol.zarr"
+
+# booleans for [making, showing] initialisation figures
+isfigures = [True, True]
+
+# settings for 0D Model (number of SD and grid coordinates)
+nsupers = {0: 1}
+coord_params = ["false"]
+zgrid = np.asarray([0, 100])
+xgrid = np.asarray([0, 100])
+ygrid = np.asarray([0, 100])
+
+# settings for monodisperse droplet radii
+numconc = 0.5e9  # numconc = total no. concentration of droplets [m^-3]
+monor = 0.025e-6  # monor = dry radius of all droplets [m]
+
+# monodisperse droplet radii probability distribution
+radiigen = initattributes.MonoAttrsGen(monor)
+radiiprobdist = radiiprobdistribs.DiracDelta(monor)
+
+# volume SD sample occupies (entire domain) [m^3]
+samplevol = rgrid.calc_domainvol(zgrid, xgrid, ygrid)
+coord3gen = None                        # do not generate SD coords
+coord1gen = None
+coord2gen = None
+
+def displacement(time, w_avg, thalf):
+  '''displacement z given velocity, w, is sinusoidal 
+  profile: w = w_avg * pi/2 * np.sin(np.pi * t/thalf)
+  where wmax = pi/2*w_avg and tauhalf = thalf/pi.'''
+
+  zmax = w_avg / 2 * thalf
+  z = zmax * (1 - np.cos(np.pi * time / thalf))
+  return z
+
+### 1. create files for gridbox boundaries and initial SD conditions
+Path(binpath).mkdir(parents=True, exist_ok=True)
+os.system("rm "+gridfile)
+os.system("rm "+initSDsfile)
+cgrid.write_gridboxboundaries_binary(gridfile, zgrid, xgrid,
+                                     ygrid, constsfile)
+rgrid.print_domain_info(constsfile, gridfile)
---
+++ examples/adiabaticparcel/adiabaticparcel.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --job-name=adiabaticparcel
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=30G
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=clara.bayley@mpimet.mpg.de
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh1126
+#SBATCH --output=./build/bin/adiabaticparcel_out.%j.out
+#SBATCH --error=./build/bin/adiabaticparcel_err.%j.out
+
+### ----- You need to edit these lines to set your ----- ###
+### ----- default compiler and python environment   ---- ###
+### ----  and paths for CLEO and build directories  ---- ###
+module load gcc/11.2.0-gcc-11.2.0
+module load python3/2022.01-gcc-11.2.0
+source activate /work/mh1126/m300950/condaenvs/cleoenv
+path2CLEO=${HOME}/testCLEOfire/
+path2build=${HOME}/testCLEOfire/build/
+configfile=${HOME}/examples/adiabaticparcel/src/config/config.txt 
+python=python
+gxx="g++"
+gcc="gcc"
+### ---------------------------------------------------- ###
+
+### ------------ choose Kokkos configuration ----------- ###
+kokkosflags="-DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_SERIAL=ON" # serial kokkos
+kokkoshost="-DKokkos_ENABLE_OPENMP=ON"                          # flags for host parallelism (e.g. using OpenMP)
+### ---------------------------------------------------- ###
+
+### ------------------------ build --------------------- ###
+### build CLEO using cmake (with openMP thread parallelism through Kokkos)
+buildcmd="CXX=${gxx} CC=${gcc} cmake -S ${path2CLEO} -B ${path2build} ${kokkosflags} ${kokkoshost}"
+echo ${buildcmd}
+CXX=${gxx} CC=${gcc} cmake -S ${path2CLEO} -B ${path2build} ${kokkosflags} ${kokkoshost}
+
+### ensure these directories exist (it's a good idea for later use)
+mkdir ${path2build}bin
+mkdir ${path2build}share
+### ---------------------------------------------------- ###
+
+### ------------------- compile & run ------------------ ###
+### generate input files and run adiabatic parcel example
+${python} adiabaticparcel.py ${path2CLEO} ${path2build} ${configfile}
+### ---------------------------------------------------- ###
