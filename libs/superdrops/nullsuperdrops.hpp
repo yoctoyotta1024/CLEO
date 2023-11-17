dd4f5472e53e6a575a19df77dbe14c7c9faa28a2
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-17 10:54:09 +0100

    comment out unused functions...

--- libs/superdrops/nullsuperdrops.hpp
+++ libs/superdrops/nullsuperdrops.hpp
@@ -40,24 +40,6 @@ drop = 0, ie. superdrop is null */
   return 0;
 }
 
-KOKKOS_INLINE_FUNCTION bool
-if_null_superdrop(Superdrop &drop)
-/* if multiplicity of drop = 0, ie. superdrop
-is null, so change it's sdgbxindex to be value that
-indicates superdrop is out of domain (ie. no
-longer exists) and return true */
-{
-  if (drop.get_xi() < 1) // ie. xi == 0
-  {
-    drop.set_sdgbxindex(LIMITVALUES::uintmax);
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
-}
-
 KOKKOS_INLINE_FUNCTION subviewd_supers
 is_null_supers(const subviewd_supers supers,
                const size_t nnull)
@@ -67,36 +49,54 @@ is_null_supers(const subviewd_supers supers,
   return supers; 
 }
 
-KOKKOS_INLINE_FUNCTION subviewd_supers
-remove_null_supers(subviewd_supers supers,
-                   const size_t nnull)
-/* sort view of superdroplets by their sdgbxindexes
-from lowest to highest sdgbxindex. Then set new
-subview excluding 'nnull' number of supers */
-{
-  using TeamPol = Kokkos::TeamPolicy<ExecSpace>;
+// KOKKOS_INLINE_FUNCTION bool
+// if_null_superdrop(Superdrop &drop)
+// /* if multiplicity of drop = 0, ie. superdrop
+// is null, so change it's sdgbxindex to be value that
+// indicates superdrop is out of domain (ie. no
+// longer exists) and return true */
+// {
+//   if (drop.get_xi() < 1) // ie. xi == 0
+//   {
+//     drop.set_sdgbxindex(LIMITVALUES::uintmax);
+//     return 1;
+//   }
+//   else
+//   {
+//     return 0;
+//   }
+// }
 
-  struct SortComparator
-  /* a precedes b if its sdgbxindex is smaller */
-  {
-    KOKKOS_INLINE_FUNCTION
-    bool operator()(const Superdrop &a, const Superdrop &b) const
-    {
-      return (a.get_sdgbxindex()) < (b.get_sdgbxindex());
-    }
-  };
+// KOKKOS_INLINE_FUNCTION subviewd_supers
+// remove_null_supers(subviewd_supers supers,
+//                    const size_t nnull)
+// /* sort view of superdroplets by their sdgbxindexes
+// from lowest to highest sdgbxindex. Then set new
+// subview excluding 'nnull' number of supers */
+// {
+//   using TeamPol = Kokkos::TeamPolicy<ExecSpace>;
 
-  Kokkos::parallel_for(
-      "sortingsupers_thread", TeamPol(1, Kokkos::AUTO()),
-      KOKKOS_LAMBDA(const TeamPol::member_type &t) {
-        Kokkos::Experimental::sort_thread(t, supers, SortComparator{});
-      });
+//   struct SortComparator
+//   /* a precedes b if its sdgbxindex is smaller */
+//   {
+//     KOKKOS_INLINE_FUNCTION
+//     bool operator()(const Superdrop &a, const Superdrop &b) const
+//     {
+//       return (a.get_sdgbxindex()) < (b.get_sdgbxindex());
+//     }
+//   };
 
-  const size_t nsupers(supers.extent(0) - nnull); // exclude null supers from no. supers
-  const Kokkos::pair<size_t, size_t> new_refs({0, nsupers});
+//   Kokkos::parallel_for(
+//       "sortingsupers_thread", TeamPol(1, Kokkos::AUTO()),
+//       KOKKOS_LAMBDA(const TeamPol::member_type &t) {
+//         Kokkos::Experimental::sort_thread(t, supers, SortComparator{});
+//       });
 
-  return Kokkos::subview(supers, new_refs);
-}
+//   const size_t nsupers(supers.extent(0) - nnull); // exclude null supers from no. supers
+//   const Kokkos::pair<size_t, size_t> new_refs({0, nsupers});
+
+//   return Kokkos::subview(supers, new_refs);
+// }
 
 #endif // NULLSUPERDROPS_HPP 
 
