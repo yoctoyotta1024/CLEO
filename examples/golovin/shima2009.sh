18f147f0f892b9353d3dbc2f7f2e1d8d92a114fd
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-17 16:54:43 +0100

    nulldyn comms is nullcoms specific to null dyn

---
+++ examples/golovin/shima2009.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --job-name=cuspbifurc
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=30G
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=clara.bayley@mpimet.mpg.de
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh1126
+#SBATCH --output=./cuspbifurc_out.%j.out
+#SBATCH --error=./cuspbifurc_err.%j.out
+
+### ----- You need to edit these lines to set your ----- ###
+### ----- default compiler and python environment   ---- ###
+### ----  and paths for CLEO and build directories  ---- ###
+module load gcc/11.2.0-gcc-11.2.0
+module load python3/2022.01-gcc-11.2.0
+source activate /work/mh1126/m300950/condaenvs/superdropsenv
+path2CLEO=${HOME}/testCLEOfire/
+path2build=${HOME}/testCLEOfire/build/
+configfile=${path2CLEO}/examples/golovin/src/config/golovin_config.txt 
+
+python=/work/mh1126/m300950/condaenvs/superdropsenv/bin/python
+gxx="g++"
+gcc="gcc"
+### ---------------------------------------------------- ###
+
+### ------------ choose Kokkos configuration ----------- ###
+kokkosflags="-DKokkos_ARCH_NATIVE=ON -DKokkos_ENABLE_SERIAL=ON" # serial kokkos
+kokkoshost="-DKokkos_ENABLE_OPENMP=ON"                          # flags for host parallelism (e.g. using OpenMP)
+### ---------------------------------------------------- ###
+
+### ------------------------ build --------------------- ###
+### build CLEO using cmake (with openMP thread parallelism through Kokkos)
+buildcmd="CXX=${gxx} CC=${gcc} cmake -S ${path2CLEO} -B ${path2build} ${kokkosflags} ${kokkoshost}"
+echo ${buildcmd}
+CXX=${gxx} CC=${gcc} cmake -S ${path2CLEO} -B ${path2build} ${kokkosflags} ${kokkoshost}
+
+### ensure these directories exist (it's a good idea for later use)
+mkdir ${path2build}bin
+mkdir ${path2build}share
+### ---------------------------------------------------- ###
+
+### ------------------- compile & run ------------------ ###
+### generate input files and run adiabatic parcel example
+${python} cuspbifurc.py ${path2CLEO} ${path2build} ${configfile}
+
+### ---------------------------------------------------- ###
---
+++ libs/coupldyn_null/nulldyncomms.hpp
@@ -0,0 +1,49 @@
+/*
+ * ----- CLEO -----
+ * File: nullcomms.hpp
+ * Project: coupldyn_null
+ * Created Date: Friday 17th November 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Friday 17th November 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * struct obeying coupleddynamics concept
+ * for dynamics solver in CLEO where there
+ * is no coupling / communication to SDM
+ */
+
+
+#ifndef NULLCOMMS_HPP 
+#define NULLCOMMS_HPP 
+
+#include <Kokkos_Core.hpp>
+
+#include "../kokkosaliases.hpp"
+#include "./nulldynamics.hpp"
+
+struct NullDynComms
+/* empty (no) coupling to/from to CLEO's gridboxes.
+Struct obeys coupling comms concept */
+{
+  KOKKOS_INLINE_FUNCTION
+  void receive_dynamics(const NullDynamics &nulldyn,
+                        const viewh_gbx h_gbxs) const {}
+  /* receive information from NullDynamics
+  solver if null for no coupling to CLEO SDM */
+
+  KOKKOS_INLINE_FUNCTION
+  void send_dynamics(const viewh_constgbx h_gbxs,
+                     const NullDynamics &nulldyn) const {}
+  /* send information from Gridboxes' states
+  to coupldyn is null for NullDynamics */
+};
+
+#endif // NULLCOMMS_HPP 
