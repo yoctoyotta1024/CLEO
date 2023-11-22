e6e7fac877d3ca03f6610721fe6cff96226a9fca
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-22 11:13:45 +0100

    file for collision kinetics

---
+++ libs/superdrops/collisionprobs/collisionkinetics.hpp
@@ -0,0 +1,28 @@
+/*
+ * ----- CLEO -----
+ * File: collisionkinetics.hpp
+ * Project: collisionprobs
+ * Created Date: Wednesday 22nd November 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Wednesday 22nd November 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * functions for kinetic calculations involved in
+ * the collisions of two (real) droplets e.g.
+ * used in the probability of coalescence or
+ * breakup according to  Low and List 1982(a).
+ */
+
+#ifndef COLLISIONKINETCS_HPP
+#define COLLISIONKINETCS_HPP
+
+#endif // COLLISIONKINETCS_HPP
+
