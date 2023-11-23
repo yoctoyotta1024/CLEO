b8350edc70c3d94167fd7e519ad418ea3a7e68c3
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-23 09:56:57 +0100

    files for breakup

---
+++ libs/superdrops/breakup.cpp
@@ -0,0 +1,23 @@
+/*
+ * ----- CLEO -----
+ * File: breakup.hpp
+ * Project: superdrops
+ * Created Date: Friday 13th October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 16th November 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * functionality to enact collision-breakup events
+ * in SDM analagous to to Shima et al. 2009.
+ * Breakup struct satisfies SDinGBxPairEnactX
+ * concept used in Collisions struct */
+
+#include "./breakup.hpp"
