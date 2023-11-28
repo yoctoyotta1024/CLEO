8a49b039f1c852e2c24b73da44fb56b59c31c4fb
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-28 17:24:04 +0100

    old set_refs algorithm

--- roughpaper/main.cpp
+++ roughpaper/main.cpp
@@ -48,23 +48,54 @@ KOKKOS_INLINE_FUNCTION
 }
 };
 
+namespace SetRefPreds
+/* namespace containing values of
+constants with dimensions */
+{
+
+  struct Ref0
+  /* struct for SupersInGbx::set_refs()
+  predicate to find first superdrop in
+  view which has matching sdgbxindex to idx */
+  {
+    unsigned int idx;
+
+    KOKKOS_INLINE_FUNCTION bool operator()(const Superdrop &op) const
+    {
+      return op.get_sdgbxindex() < idx;
+    }
+  };
+
+  struct Ref1
+  /* struct for SupersInGbx::set_refs()
+  predicate to find last superdrop in
+  view which has matching sdgbxindex to idx */
+  {
+    unsigned int idx;
+
+    KOKKOS_INLINE_FUNCTION bool operator()(const Superdrop &op) const
+    {
+      return op.get_sdgbxindex() <= idx;
+    }
+  };
+}
+
 using viewd_supers = Kokkos::View<Superdrop *>;
 using viewd_constsupers = Kokkos::View<const Superdrop *>; // view in device memory of const superdroplets
 using ExecSpace = Kokkos::DefaultExecutionSpace;
 using kkpair = Kokkos::pair<size_t, size_t>;
 
+kkpair set_refs_old(const unsigned int ii,
+                    viewd_constsupers totsupers);
 
-kkpair set_refs(const unsigned int ii, viewd_constsupers supers)
-{
-  const kkpair new_refs = {0,0};
-  return new_refs;
-}
-
+template <typename Pred>
+size_t find_ref_old(const Pred pred,
+                    viewd_constsupers totsupers);
 
 int main(int argc, char *argv[])
 {
-  const size_t nsupers(20);
-  const size_t ngbxs(1);
+  const size_t nsupers(21);
+  const size_t ngbxs(10);
 
   Kokkos::initialize(argc, argv);
   {
@@ -72,7 +103,7 @@ int main(int argc, char *argv[])
     auto h_supers = Kokkos::create_mirror_view(supers);
     for (size_t kk(0); kk < nsupers; ++kk)
     {
-      const unsigned int ii(kk);
+      const unsigned int ii(kk/2);
       h_supers(kk) = Superdrop();
       h_supers(kk).set_sdgbxindex(ii);
       std::cout << "ii: " << h_supers(kk).get_sdgbxindex() << "\n";
@@ -83,7 +114,7 @@ int main(int argc, char *argv[])
     for (size_t ii(0); ii < ngbxs; ++ii)
     {
       kkpair refs{0, 0};
-      refs = set_refs(ii, supers);
+      refs = set_refs_old(ii, supers);
       std::cout << "refs: " << refs.first << ", " << refs.second << "\n";
       
       viewd_refs(ii) = refs;
@@ -105,4 +136,32 @@ int main(int argc, char *argv[])
     }
   }
   Kokkos::finalize();
+}
+
+
+kkpair set_refs_old(const unsigned int ii, viewd_constsupers totsupers)
+{
+  namespace SRP = SetRefPreds;
+  const kkpair new_refs = {find_ref_old(SRP::Ref0{ii}, totsupers),
+                           find_ref_old(SRP::Ref1{ii}, totsupers)};
+  return new_refs;
+}
+
+template <typename Pred>
+size_t find_ref_old(const Pred pred,
+                    viewd_constsupers totsupers)
+{
+  namespace KE = Kokkos::Experimental;
+
+  /* iterator to first superdrop in
+  totsupers that fails to satisfy pred */
+  const auto iter(KE::partition_point("findref",
+                                      ExecSpace(),
+                                      totsupers,
+                                      pred));
+
+  /* distance form start of totsupers
+  (casting away signd-ness)*/
+  const auto ref0 = KE::distance(KE::begin(totsupers), iter);
+  return static_cast<size_t>(ref0);
 }
