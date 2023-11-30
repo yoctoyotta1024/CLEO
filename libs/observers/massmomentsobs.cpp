4c6e8a03ed12ed3a47cb09a53d678591b28fc38f
Clara Levante <yoctoyotta1024@yahoo.com>
2023-11-30 17:20:31 +0100

    merge conflict fixed with new massmom calcs

--- libs/observers/massmomentsobs.cpp
+++ libs/observers/massmomentsobs.cpp
@@ -6,7 +6,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Thursday 30th November 2023
+ * Last Modified: Monday 23rd October 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -22,11 +22,81 @@
 
 #include "./massmomentsobs.hpp"
 
-void DoMassMomentsObs::
-    massmoments_to_storage(const mirrorh_constsupers h_supers) const
-/* calculated 0th, 1st and 2nd moment of the (real) droplet mass
-distribution and then writes them to zarr storage. (i.e.
-0th, 3rd and 6th moment of the droplet radius distribution) */
+std::array<double, 3>
+calc_massmoments(const subviewd_constsupers supers)
+/* calculated 0th, 1st and 2nd moment of the (real)
+droplet mass distribution, i.e. 0th, 3rd and 6th
+moment of the droplet radius distribution.
+Kokkos::parallel_reduce([...]) is equivalent in serial to:
+for (size_t kk(0); kk < supers.extent(0); ++kk){[...]},
+see calc_massmoments_serial.
+ * WARNING! * When using OpenMP (supers in Host Space)
+ and there are only a few superdroplets in supers,
+ calc_massmoments is much slower then calc_massmoments_serial
+ (probably because opening threads is more costly than the
+ time saved in a parallel calculation over few elements) */
+{
+  const size_t nsupers(supers.extent(0));
+
+  std::array<double, 3> moms({0.0, 0.0, 0.0}); // {0th, 1st, 2nd} mass moments
+  Kokkos::parallel_reduce(
+      "calc_massmoments",
+      Kokkos::RangePolicy<ExecSpace>(0, nsupers),
+      KOKKOS_LAMBDA(const size_t kk, double &m0, double &m1, double &m2) {
+        
+        const double xi = (double)(supers(kk).get_xi()); // cast multiplicity from unsigned int to double
+        const double mass(supers(kk).mass()); 
+        m0 += xi;
+        m1 += xi * mass;
+        m2 += xi * mass * mass;
+      },
+      moms.at(0), moms.at(1), moms.at(2)); // {0th, 1st, 2nd} mass moments
+
+  return moms;
+}
+
+std::array<double, 3>
+calc_rainmassmoments(const subviewd_constsupers supers)
+/* calculated 0th, 1st and 2nd moment of the
+(real) raindroplet mass distribution, i.e. 0th, 3rd and 6th
+moment of the droplet radius disttribution. Raindrops are 
+all droplets with r >= rlim = 40 microns.
+Kokkos::parallel_reduce([...]) is equivalent in serial to:
+for (size_t kk(0); kk < supers.extent(0); ++kk){[...]},
+see calc_rainmassmoments_serial
+ * WARNING! * When using OpenMP (supers in Host Space)
+ and there are only a few superdroplets in supers,
+ calc_rainmassmoments is much slower then calc_rainmassmoments_serial
+ (probably because opening threads is more costly than the
+ time saved in a parallel calculation over few elements) */
+{
+  constexpr double rlim(40e-6 / dlc::R0); // dimless minimum radius of raindrop
+  const size_t nsupers(supers.extent(0));
+
+  std::array<double, 3> moms({0.0, 0.0, 0.0}); // {0th, 1st, 2nd} rain mass moments
+  Kokkos::parallel_reduce(
+      "calc_rainmassmoments",
+      Kokkos::RangePolicy<ExecSpace>(0, nsupers),
+      KOKKOS_LAMBDA(const size_t kk, double &m0, double &m1, double &m2) {
+        if (supers(kk).get_radius() >= rlim)
+        {
+          const double xi = (double)(supers(kk).get_xi()); // cast multiplicity from unsigned int to double
+          const double mass(supers(kk).mass());
+          m0 += xi;
+          m1 += xi * mass;
+          m2 += xi * mass * mass;
+        }
+      },
+      moms.at(0), moms.at(1), moms.at(2)); // {0th, 1st, 2nd} rain mass moments
+
+  return moms;
+}
+
+std::array<double, 3>
+calc_massmoments_serial(const subviewd_constsupers supers)
+/* calculated 0th, 1st and 2nd moment of the (real)
+droplet mass distribution, i.e. 0th, 3rd and 6th
+moment of the droplet radius distribution */
 {
   auto h_supers = Kokkos::create_mirror_view(supers);
   Kokkos::deep_copy(h_supers, supers);
@@ -44,11 +114,12 @@ distribution and then writes them to zarr storage. (i.e.
   return moms;
 }
 
-void DoRainMassMomentsObs::
-    rainmassmoments_to_storage(const mirrorh_constsupers h_supers) const
-/* calculated 0th, 1st and 2nd moment of the (real) droplet mass
-distribution and then writes them to zarr storage. (i.e.
-0th, 3rd and 6th moment of the droplet radius distribution) */
+std::array<double, 3>
+calc_rainmassmoments_serial(const subviewd_constsupers supers)
+/* calculated 0th, 1st and 2nd moment of the (real)
+raindroplet mass distribution, i.e. 0th, 3rd and 6th
+moment of the droplet radius disttribution. Raindrops
+are all droplets with r >= rlim = 40 microns */
 {
   constexpr double rlim(40e-6 / dlc::R0); // dimless minimum radius of raindrop
 
