0747c0c38e128603f8f9091f581cd8b47ecc4b49
clara bayley <yoctoyotta1024@yahoo.com>
2023-11-30 11:12:16 +0100

    formatting

--- libs/observers/massmomentsobs.cpp
+++ libs/observers/massmomentsobs.cpp
@@ -6,7 +6,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Monday 23rd October 2023
+ * Last Modified: Thursday 30th November 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
