860ef202b418829e944c51caec9963e58306f38b
clara bayley <yoctoyotta1024@yahoo.com>
2023-12-12 13:19:41 +0100

    publish docs

--- libs/observers/massmomentsobs.cpp
+++ libs/observers/massmomentsobs.cpp
@@ -6,7 +6,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Monday 23rd October 2023
+ * Last Modified: Tuesday 12th December 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
