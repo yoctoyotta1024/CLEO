5a311eae92e9405fcb8b4c5ff422d88f940a8452
Clara Levante <yoctoyotta1024@yahoo.com>
2023-12-19 13:06:10 +0100

    new file

---
+++ libs/cartesiandomain/cartesianmotion_withreset.hpp
@@ -0,0 +1,32 @@
+/*
+ * ----- CLEO -----
+ * File: cartesianmotion_withreset.hpp
+ * Project: cartesiandomain
+ * Created Date: Tuesday 19th December 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Tuesday 19th December 2023
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * Copyright (c) 2023 MPI-M, Clara Bayley
+ * -----
+ * File Description:
+ * Motion of a superdroplet using predictor-corrector
+ * method to update a superdroplet's coordinates and
+ * the sdgbxindex updated accordingly for a
+ * cartesian domain with finite/periodic boundary
+ * conditions and reset of superdroplets that leave
+ * the domain through the lower domain boundary
+ */
+
+#ifndef CARTESIANMOTION_WITHRESET_HPP
+#define CARTESIANMOTION_WITHRESET_HPP
+
+#include "./cartesianmotion.hpp"
+
+#endif // CARTESIANMOTION_WITHRESET_HPP
+
