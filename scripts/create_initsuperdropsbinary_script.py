01ef19e08cf943f06b19dd3edeb10018a2b2f494
Clara Levante <yoctoyotta1024@yahoo.com>
2023-12-21 15:19:45 +0100

    diff initial dry areosol distrib

--- scripts/create_initsuperdropsbinary_script.py
+++ scripts/create_initsuperdropsbinary_script.py
@@ -6,7 +6,7 @@ Created Date: Tuesday 24th October 2023
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Wednesday 20th December 2023
+Last Modified: Thursday 21st December 2023
 Modified By: CB
 -----
 License: BSD 3-Clause "New" or "Revised" License
@@ -62,8 +62,7 @@ nsupers = 40
 # monor                = 0.05e-6                        # all SDs have this same radius [m]
 # radiigen  =  iattrs.MonoAttrsGen(monor)                  # all SDs have the same dryradius [m]
 
-# rspan                = [5e-7, 1e-3]                 # min and max range of radii to sample [m]
-rspan                = [5e-7, 8e-5]                 # min and max range of radii to sample [m]
+rspan                = [6e-9, 6e-6]                 # min and max range of radii to sample [m]
 randomr              = True                         # sample radii range randomly or not
 radiigen =  iattrs.SampleDryradiiGen(rspan, randomr)   # radii are sampled from rspan [m]
 ### ---------------------------------------------- ###
@@ -77,18 +76,18 @@ radiigen =  iattrs.SampleDryradiiGen(rspan, randomr)   # radii are sampled from 
 # geomeans           = [0.075e-6]                  # lnnormal modes' geometric mean droplet radius [m] 
 # geosigs            = [1.5]                       # lnnormal modes' geometric standard deviation
 # scalefacs          = [1e9]                       # relative heights of modes         
-# geomeans             = [0.02e-6, 0.2e-6, 3.5e-6]               
-# geosigs              = [1.55, 2.3, 2]                    
-# scalefacs            = [1e6, 0.3e6, 0.025e6]   
+geomeans             = [0.02e-6, 0.2e-6, 3.5e-6]               
+geosigs              = [1.55, 2.3, 2]                    
+scalefacs            = [1e6, 0.3e6, 0.025e6]   
 # geomeans             = [0.02e-6, 0.15e-6]               
 # geosigs              = [1.4, 1.6]                    
 # scalefacs            = [6e6, 4e6]   
-# numconc = np.sum(scalefacs)
-# radiiprobdist = rprobs.LnNormal(geomeans, geosigs, scalefacs)
+numconc = np.sum(scalefacs)
+radiiprobdist = rprobs.LnNormal(geomeans, geosigs, scalefacs)
  
-volexpr0             = 30.531e-6                   # peak of volume exponential distribution [m]
-numconc              = 2**(23)                     # total no. conc of real droplets [m^-3]
-radiiprobdist = rprobs.VolExponential(volexpr0, rspan)
+# volexpr0             = 30.531e-6                   # peak of volume exponential distribution [m]
+# numconc              = 2**(23)                     # total no. conc of real droplets [m^-3]
+# radiiprobdist = rprobs.VolExponential(volexpr0, rspan)
 
 # reff                 = 7e-6                     # effective radius [m]
 # nueff                = 0.08                     # effective variance 
