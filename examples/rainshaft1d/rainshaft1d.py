2e6e069e2612e4c91ff9691a75166a5eb1c2046e
Clara Levante <yoctoyotta1024@yahoo.com>
2024-01-09 13:47:42 +0100

    1d rainshaft python script

---
+++ examples/rainshaft1d/rainshaft1d.py
@@ -0,0 +1,234 @@
+'''
+----- CLEO -----
+File: rainshaft1d.py
+Project: rainshaft1d
+Created Date: Friday 17th November 2023
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Tuesday 9th January 2024
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+Copyright (c) 2023 MPI-M, Clara Bayley
+-----
+File Description:
+Script compiles and runs CLEO exmpl2D to create the
+data and plots precipitation example given constant 
+1-D rainshaft thermodynamics read from a file
+'''
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from matplotlib.colors import LogNorm, Normalize
+
+path2CLEO = sys.argv[1]
+path2build = sys.argv[2]
+configfile = sys.argv[3]
+
+sys.path.append(path2CLEO)  # for imports from pySD package
+sys.path.append(path2CLEO+"/examples/exampleplotting/") # for imports from example plotting package
+
+from plotssrc import pltsds, pltmoms, animations
+from pySD.sdmout_src import *
+from pySD.gbxboundariesbinary_src import read_gbxboundaries as rgrid
+from pySD.gbxboundariesbinary_src import create_gbxboundaries as cgrid
+from pySD.initsuperdropsbinary_src import *
+from pySD.initsuperdropsbinary_src import create_initsuperdrops as csupers 
+from pySD.initsuperdropsbinary_src import read_initsuperdrops as rsupers 
+from pySD.thermobinary_src import thermogen
+from pySD.thermobinary_src import create_thermodynamics as cthermo
+from pySD.thermobinary_src import read_thermodynamics as rthermo
+
+### ---------------------------------------------------------------- ###
+### ----------------------- INPUT PARAMETERS ----------------------- ###
+### ---------------------------------------------------------------- ###
+### --- essential paths and filenames --- ###
+# path and filenames for creating initial SD conditions
+constsfile = path2CLEO+"/libs/cleoconstants.hpp"
+binpath = path2build+"/bin/"
+sharepath = path2build+"/share/"
+gridfile = sharepath+"rain1d_dimlessGBxboundaries.dat"
+initSDsfile = sharepath+"rain1d_dimlessSDsinit.dat"
+thermofile =  sharepath+"rain12d_dimlessthermo.dat"
+
+# path and file names for plotting results
+setupfile = binpath+"rain1d_setup.txt"
+dataset = binpath+"rain1d_sol.zarr"
+
+### --- plotting initialisation figures --- ###
+isfigures = [True, True] # booleans for [making, saving] initialisation figures
+savefigpath = path2build+"/bin/" # directory for saving figures
+SDgbxs2plt = [0] # gbxindex of SDs to plot (nb. "all" can be very slow)
+
+### --- settings for 2-D gridbox boundaries --- ###
+zgrid = [0, 1500, 75]      # evenly spaced zhalf coords [zmin, zmax, zdelta] [m]
+xgrid = [0, 1500, 75]      # evenly spaced xhalf coords [m]
+ygrid = np.array([0, 20])  # array of yhalf coords [m]
+
+### --- settings for initial superdroplets --- ###
+# settings for initial superdroplet coordinates
+zlim = 500        # max z coord of superdroplets
+npergbx = 8       # number of superdroplets per gridbox 
+
+# [min, max] range of initial superdroplet radii (and implicitly solute masses)
+rspan                = [3e-9, 3e-6] # [m]
+
+# settings for initial superdroplet multiplicies
+# (from bimodal Lognormal distribution)
+geomeans             = [0.02e-6, 0.15e-6]               
+geosigs              = [1.4, 1.6]                    
+scalefacs            = [6e6, 4e6]   
+numconc = np.sum(scalefacs)
+
+### --- settings for 2D Thermodyanmics --- ###
+PRESS0 = 101315 # [Pa]
+THETA = 288.15 # [K]
+qcond = 0.0 # [Kg/Kg]
+WMAX = 0.6 # [m/s]
+VVEL = None # [m/s]
+Zlength = 1500 # [m]
+Xlength = 1500 # [m]
+qvapmethod = "sratio"
+Zbase = 750 # [m]
+sratios = [0.99, 1.0025] # s_ratio [below, above] Zbase
+moistlayer=False
+### ---------------------------------------------------------------- ###
+### ---------------------------------------------------------------- ###
+
+
+### ---------------------------------------------------------------- ###
+### ------------------- BINARY FILES GENERATION--------------------- ###
+### ---------------------------------------------------------------- ###
+### --- ensure build, share and bin directories exist --- ###
+if path2CLEO == path2build:
+  raise ValueError("build directory cannot be CLEO")
+else:
+  Path(path2build).mkdir(exist_ok=True) 
+  Path(sharepath).mkdir(exist_ok=True) 
+  Path(binpath).mkdir(exist_ok=True) 
+os.system("rm "+gridfile)
+os.system("rm "+initSDsfile)
+os.system("rm "+thermofile[:-4]+"*")
+
+### ----- write gridbox boundaries binary ----- ###
+cgrid.write_gridboxboundaries_binary(gridfile, zgrid, xgrid, ygrid, constsfile)
+rgrid.print_domain_info(constsfile, gridfile)
+
+### ----- write thermodyanmics binaries ----- ###
+thermodyngen = thermogen.ConstHydrostaticAdiabat(configfile, constsfile, PRESS0, 
+                                                 THETA, qvapmethod, sratios, Zbase,
+                                                 qcond, WMAX, Zlength, Xlength,
+                                                 VVEL, moistlayer)
+cthermo.write_thermodynamics_binary(thermofile, thermodyngen, configfile,
+                                    constsfile, gridfile)
+
+
+### ----- write initial superdroplets binary ----- ###
+nsupers = crdgens.nsupers_at_domain_base(gridfile, constsfile, npergbx, zlim)
+coord3gen = crdgens.SampleCoordGen(True) # sample coord3 randomly
+coord1gen = crdgens.SampleCoordGen(True) # sample coord1 randomly
+coord2gen = None                        # do not generate superdroplet coord2s
+xiprobdist = probdists.LnNormal(geomeans, geosigs, scalefacs)
+radiigen = rgens.SampleLog10RadiiGen(rspan) # randomly sample radii from rspan [m]
+dryradiigen = dryrgens.ScaledRadiiGen(1.0)
+
+initattrsgen = attrsgen.AttrsGenerator(radiigen, dryradiigen, xiprobdist,
+                                        coord3gen, coord1gen, coord2gen)
+csupers.write_initsuperdrops_binary(initSDsfile, initattrsgen, 
+                                      configfile, constsfile,
+                                      gridfile, nsupers, numconc)
+
+### ----- show (and save) plots of binary file data ----- ###
+if isfigures[0]:
+  if isfigures[1]:
+    Path(savefigpath).mkdir(exist_ok=True) 
+  rgrid.plot_gridboxboundaries(constsfile, gridfile,
+                               savefigpath, isfigures[1])
+  rthermo.plot_thermodynamics(constsfile, configfile, gridfile,
+                              thermofile, savefigpath, isfigures[1])
+  rsupers.plot_initGBxs_distribs(configfile, constsfile, initSDsfile,
+                              gridfile, savefigpath, isfigures[1],
+                              SDgbxs2plt) 
+### ---------------------------------------------------------------- ###
+### ---------------------------------------------------------------- ###
+
+### ---------------------------------------------------------------- ###
+### -------------------- COMPILE AND RUN CLEO ---------------------- ###
+### ---------------------------------------------------------------- ###
+# 2. compile and the run model
+os.chdir(path2build)
+os.system('pwd')
+os.system('rm -rf '+dataset)
+os.system('make clean && make -j 64 rain1D')
+executable = path2build+'/examples/constthermo2d/src/rain1D'
+os.system(executable + ' ' + configfile)
+### ---------------------------------------------------------------- ###
+### ---------------------------------------------------------------- ###
+
+### ------------------------------------------------------------ ###
+### ----------------------- PLOT RESULTS ----------------------- ###
+### ------------------------------------------------------------ ###
+# read in constants and intial setup from setup .txt file
+config = pysetuptxt.get_config(setupfile, nattrs=3, isprint=True)
+consts = pysetuptxt.get_consts(setupfile, isprint=True)
+gbxs = pygbxsdat.get_gridboxes(gridfile, consts["COORD0"], isprint=True)
+
+time = pyzarr.get_time(dataset)
+sddata = pyzarr.get_supers(dataset, consts)
+totnsupers = pyzarr.get_totnsupers(dataset)
+massmoms = pyzarr.get_massmoms(dataset, config["ntime"], gbxs["ndims"])
+
+# plot figures
+savename = savefigpath + "rain1d_totnsupers.png"
+pltmoms.plot_totnsupers(time, totnsupers, savename=savename)
+
+savename = savefigpath + "rain1d_domainmassmoms.png"
+pltmoms.plot_domainmassmoments(time, massmoms, savename=savename)
+
+nsample = 500
+savename = savefigpath + "rain1d_randomsample.png"
+pltsds.plot_randomsample_superdrops(time, sddata,
+                                        config["totnsupers"],
+                                        nsample,
+                                        savename=savename)
+
+### ----- plot 1-D .gif animations ----- ###
+nframes = len(time.mins)
+mom2ani = np.sum(massmoms.nsupers, axis=(1,2))
+xlims = [0, np.amax(mom2ani)]
+xlabel = "number of super-droplets"
+savename=savefigpath+"rain1d_nsupers1d"
+animations.animate1dprofile(gbxs, mom2ani, time.mins, nframes,
+                            xlabel=xlabel, xlims=xlims,
+                            color="green", saveani=True,
+                            savename=savename, fps=5)   
+
+nframes = len(time.mins)
+norm = gbxs["gbxvols"] * 1e6 # volume [cm^3]
+mom2ani = np.sum(massmoms.mom0, axis=(1,2)) / norm[None,:]
+xlims = [0, np.amax(mom2ani)]
+xlabel = "number concentration /cm$^{-3}$"
+savename=savefigpath+"rain1d_numconc1d"
+animations.animate1dprofile(gbxs, mom2ani, time.mins, nframes,
+                            xlabel=xlabel, xlims=xlims,
+                            color="green", saveani=True,
+                            savename=savename, fps=5)
+
+nframes = len(time.mins)
+norm = gbxs["gbxvols"] # volume [m^3]
+mom2ani = np.sum(massmoms.mom1, axis=(1,2)) / norm[None,:]
+xlims = [0, np.amax(mom2ani)]
+xlabel = "mass concentration /g m$^{-3}$"
+savename=savefigpath+"rain1d_massconc1d"
+animations.animate1dprofile(gbxs, mom2ani, time.mins, nframes,
+                            xlabel=xlabel, xlims=xlims,
+                            color="green", saveani=True,
+                            savename=savename, fps=5)                        
+### ------------------------------------------------------------ ###
+### ------------------------------------------------------------ ###                                
---
+++ examples/rainshaft1d/rainshaft1d.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#SBATCH --job-name=rain1d
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=30G
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=clara.bayley@mpimet.mpg.de
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh1126
+#SBATCH --output=./rain1d_out.%j.out
+#SBATCH --error=./rain1d_err.%j.out
+
+### ----- You need to edit these lines to set your ----- ###
+### ----- default compiler and python environment   ---- ###
+### ----  and paths for CLEO and build directories  ---- ###
+module load gcc/11.2.0-gcc-11.2.0
+module load python3/2022.01-gcc-11.2.0
+module load nvhpc/23.7-gcc-11.2.0
+spack load cmake@3.23.1%gcc
+source activate /work/mh1126/m300950/condaenvs/superdropsenv
+
+path2CLEO=${HOME}/CLEO/
+path2build=${HOME}/CLEO/build/
+configfile=${path2CLEO}/examples/rainshaft1d/src/config/rain1d_config.txt 
+
+python=/work/mh1126/m300950/condaenvs/superdropsenv/bin/python
+gxx="g++"
+gcc="gcc"
+cuda="nvc++"
+### ---------------------------------------------------- ###
+
+### ------------ choose Kokkos configuration ----------- ###
+kokkosflags="-DKokkos_ARCH_NATIVE=ON -DKokkos_ARCH_AMPERE80=ON -DKokkos_ENABLE_SERIAL=ON" # serial kokkos
+kokkoshost="-DKokkos_ENABLE_OPENMP=ON"                                          # flags for host parallelism (e.g. using OpenMP)
+kokkosdevice="-DKokkos_ENABLE_CUDA=ON -DKokkos_ENABLE_CUDA_LAMBDA=ON"           # flags for device parallelism (e.g. using CUDA)
+### ---------------------------------------------------- ###
+
+### ------------------------ build --------------------- ###
+### build CLEO using cmake (with openMP thread parallelism through Kokkos)
+buildcmd="CXX=${gxx} CC=${gcc} CUDA=${cuda} cmake -S ${path2CLEO} -B ${path2build} ${kokkosflags} ${kokkoshost} ${kokkosdevice}"
+echo ${buildcmd}
+CXX=${gxx} CC=${gcc} CUDA=${cuda} cmake -S ${path2CLEO} -B ${path2build} ${kokkosflags} ${kokkoshost} ${kokkosdevice}
+
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+
+### ensure these directories exist (it's a good idea for later use)
+mkdir ${path2build}bin
+mkdir ${path2build}share
+### ---------------------------------------------------- ###
+
+### ------------------- compile & run ------------------ ###
+### generate input files and run 1-D rainshaft example
+${python} rainshaft1d.py ${path2CLEO} ${path2build} ${configfile}
+
+### ---------------------------------------------------- ###
