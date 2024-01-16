94d36f6edf694e05d61012878cb61094f05f2f45
Clara Levante <yoctoyotta1024@yahoo.com>
2024-01-16 10:32:10 +0100

    comment on mass of SD return

--- scripts/create_thermobinaries_script.py
+++ scripts/create_thermobinaries_script.py
@@ -6,7 +6,7 @@ Created Date: Tuesday 24th October 2023
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Monday 15th January 2024
+Last Modified: Tuesday 16th January 2024
 Modified By: CB
 -----
 License: BSD 3-Clause "New" or "Revised" License
