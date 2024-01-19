f5dd56ae85d0e654e5e6509320589a7da66452cc
wiltonloch <wilton.loch@gmail.com>
2024-01-19 10:08:24 +0100

    chore: linter changes to fsstore.hpp

--- libs/zarr/fsstore.hpp
+++ libs/zarr/fsstore.hpp
@@ -19,86 +19,74 @@
  * https://zarr.readthedocs.io/en/stable/spec/v2.html
  */
 
+#ifndef LIBS_ZARR_FSSTORE_HPP_
+#define LIBS_ZARR_FSSTORE_HPP_
 
-#ifndef FSSTORE_HPP
-#define FSSTORE_HPP
-
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <span>
 #include <string>
 #include <string_view>
-#include <span>
-#include <iostream>
-#include <fstream>
-#include <filesystem>
 
-template <typename Store>
-struct StoreAccessor
 /* functions for converting types (e.g. vectors of
 unsigned integers or doubles) into vectors of single bytes to
 write to store under a given key. Store can be anything that
 satisfies the zarr storage specifcaiton version 2 */
-{
+template <typename Store>
+struct StoreAccessor {
   Store &store;
   std::string_view key;
 
-  StoreAccessor &operator=(std::span<const uint8_t> buffer)
   /* write range of memory representing uint8_ts to store */
-  {
+  StoreAccessor &operator=(std::span<const uint8_t> buffer) {
     store.write(key, buffer);
     return *this;
   }
 
-  StoreAccessor &operator=(std::string_view buffer)
   /* reinterpret range of memory representing string as
   a range of memory representing uint8_ts, then write to store */
-  {
-    return operator=(std::span<const uint8_t>(
-        reinterpret_cast<const uint8_t *>(buffer.data()),
-        buffer.size()));
+  StoreAccessor &operator=(std::string_view buffer) {
+    return operator=(
+        std::span<const uint8_t>(reinterpret_cast<const uint8_t *>(buffer.data()), buffer.size()));
   }
 
-  template <typename T>
-  StoreAccessor &operator=(std::span<const T> buffer)
   /* re-interpret range of memory representing vector of type T as
   a range of memory representing uint8_ts, then write to store */
-  {
-    return operator=(std::span<const uint8_t>(
-        reinterpret_cast<const uint8_t *>(buffer.data()),
-        buffer.size() * sizeof(T)));
+  template <typename T>
+  StoreAccessor &operator=(std::span<const T> buffer) {
+    return operator=(std::span<const uint8_t>(reinterpret_cast<const uint8_t *>(buffer.data()),
+                                              buffer.size() * sizeof(T)));
   }
 };
 
-class FSStore
 /* A file system (with root in 'basedir' directory) obeying Zarr
 version 2 requirements for a Store. Store contins a series
 of key, values where values may be data arrays or groups in the store.
 data for a given key is written to the store via the functions
 in StoreAccessor */
-{
-private:
+class FSStore {
+ private:
   const std::filesystem::path basedir;
 
-public:
-  FSStore(const std::filesystem::path basedir) : basedir(basedir)
-  {
+ public:
+  explicit FSStore(const std::filesystem::path basedir) : basedir(basedir) {
     // initialize a zarr group (i.e. dataset)
-    const std::string zarr_format("2"); // storage spec. version 2
-    const std::string zgroupjson("{\"zarr_format\": " +
-                                   zarr_format + "}");
+    const std::string zarr_format("2");  // storage spec. version 2
+    const std::string zgroupjson("{\"zarr_format\": " + zarr_format + "}");
     (*this)[".zgroup"] = zgroupjson;
 
     // global metadata (optional)
-    (*this)[".zattrs"] = "{\"creator\": \"Clara Bayley\", "
-                         "\"title\": \"store for output of coupled SDM\"}";
+    (*this)[".zattrs"] =
+        "{\"creator\": \"Clara Bayley\", "
+        "\"title\": \"store for output of coupled SDM\"}";
   }
 
-  StoreAccessor<FSStore> operator[](const std::string_view key)
-  {
-    return {*this, key};
-  }
+  StoreAccessor<FSStore> operator[](const std::string_view key) { return {*this, key}; }
 
-  bool write(const std::string_view key, const std::span<const uint8_t> buffer);
   /* write function called by StoreAccessor once data has been
   converted into a vector of unsigned integer types */
+  bool write(const std::string_view key, const std::span<const uint8_t> buffer);
 };
 
-#endif // FSSTORE_HPP
+#endif  // LIBS_ZARR_FSSTORE_HPP_
