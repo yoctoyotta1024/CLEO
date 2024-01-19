d783123ee9a08899c1fb829f4a9bea02f04c4137
wiltonloch <wilton.loch@gmail.com>
2024-01-19 09:52:37 +0100

    chore: linter changes to fsstore.cpp

--- libs/zarr/fsstore.cpp
+++ libs/zarr/fsstore.cpp
@@ -19,30 +19,28 @@
  * https://zarr.readthedocs.io/en/stable/spec/v2.html
  */
 
+#include "zarr/fsstore.hpp"
 
-#include "./fsstore.hpp"
-
-bool FSStore::write(std::string_view key,
-                    std::span<const uint8_t> buffer)
 /* write function called by StoreAccessor
 once data has been converted into a vector
 of unsigned integer types */
-{
+bool FSStore::write(std::string_view key, std::span<const uint8_t> buffer) {
   auto path = basedir / key;
   auto mode = std::ios::out | std::ios::binary;
   std::ofstream out(path, mode);
-  if (!out.good())
-  {
+
+  if (!out.good()) {
     std::cout << "couldn't open " << path << ",\n "
               << "making directory " << path.parent_path() << "\n";
     std::filesystem::create_directories(path.parent_path());
     out.open(path, mode);
   }
-  if (!out.good())
-  {
+
+  if (!out.good()) {
     std::cout << "can't write to " << path << "\n";
     return false;
   }
+
   out.write(reinterpret_cast<const char *>(buffer.data()), buffer.size());
   return true;
 }
