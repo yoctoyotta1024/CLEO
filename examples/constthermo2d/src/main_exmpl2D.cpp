218b5bccda25c36ec0a5ba2c0d98ddfaf7535b9b
wiltonloch <wilton.loch@gmail.com>
2024-01-23 13:40:46 +0100

    chore: linter cosmetic changes to examples

--- examples/constthermo2d/src/main_exmpl2D.cpp
+++ examples/constthermo2d/src/main_exmpl2D.cpp
@@ -19,198 +19,142 @@
  * ./src/exmpl2D ../src/config/config.txt
  */
 
+#include <cmath>
+#include <concepts>
 #include <iostream>
 #include <stdexcept>
 #include <string_view>
-#include <concepts>
-#include <cmath>
 
 #include <Kokkos_Core.hpp>
 
 #include "cartesiandomain/cartesianmaps.hpp"
 #include "cartesiandomain/cartesianmotion.hpp"
 #include "cartesiandomain/createcartesianmaps.hpp"
-
-#include "coupldyn_fromfile/fromfilecomms.hpp"
 #include "coupldyn_fromfile/fromfile_cartesian_dynamics.hpp"
-
+#include "coupldyn_fromfile/fromfilecomms.hpp"
 #include "gridboxes/gridboxmaps.hpp"
-
 #include "initialise/config.hpp"
-#include "initialise/timesteps.hpp"
 #include "initialise/initgbxs_null.hpp"
 #include "initialise/initsupers_frombinary.hpp"
-
+#include "initialise/timesteps.hpp"
 #include "observers/massmomentsobs.hpp"
 #include "observers/nsupersobs.hpp"
 #include "observers/observers.hpp"
 #include "observers/printobs.hpp"
-#include "observers/timeobs.hpp"
 #include "observers/supersattrsobs.hpp"
-
+#include "observers/timeobs.hpp"
 #include "runcleo/coupleddynamics.hpp"
 #include "runcleo/couplingcomms.hpp"
 #include "runcleo/initialconditions.hpp"
 #include "runcleo/runcleo.hpp"
 #include "runcleo/sdmmethods.hpp"
-
 #include "superdrops/coalescence.hpp"
 #include "superdrops/collisionprobs/longhydroprob.hpp"
 #include "superdrops/condensation.hpp"
-#include "superdrops/motion.hpp"
 #include "superdrops/microphysicalprocess.hpp"
+#include "superdrops/motion.hpp"
 #include "superdrops/terminalvelocity.hpp"
-
 #include "zarr/fsstore.hpp"
 #include "zarr/superdropattrsbuffers.hpp"
 #include "zarr/superdropsbuffers.hpp"
 
-inline CoupledDynamics auto
-create_coupldyn(const Config &config,
-                const CartesianMaps &gbxmaps,
-                const unsigned int couplstep,
-                const unsigned int t_end)
-
-{
+inline CoupledDynamics auto create_coupldyn(const Config &config, const CartesianMaps &gbxmaps,
+                                            const unsigned int couplstep,
+                                            const unsigned int t_end) {
   const auto h_ndims(gbxmaps.ndims_hostcopy());
-  const std::array<size_t, 3>
-      ndims({h_ndims(0), h_ndims(1), h_ndims(2)});
+  const std::array<size_t, 3> ndims({h_ndims(0), h_ndims(1), h_ndims(2)});
 
   const auto nsteps = (unsigned int)(std::ceil(t_end / couplstep) + 1);
 
   return FromFileDynamics(config, couplstep, ndims, nsteps);
 }
 
-inline InitialConditions auto
-create_initconds(const Config &config)
-{
+inline InitialConditions auto create_initconds(const Config &config) {
   const InitSupersFromBinary initsupers(config);
   const InitGbxsNull initgbxs(config);
 
   return InitConds(initsupers, initgbxs);
 }
 
-inline GridboxMaps auto
-create_gbxmaps(const Config &config)
-{
-  const auto gbxmaps = create_cartesian_maps(config.ngbxs,
-                                             config.nspacedims,
-                                             config.grid_filename);
+inline GridboxMaps auto create_gbxmaps(const Config &config) {
+  const auto gbxmaps = create_cartesian_maps(config.ngbxs, config.nspacedims, config.grid_filename);
   return gbxmaps;
 }
 
-inline MicrophysicalProcess auto
-config_condensation(const Config &config, const Timesteps &tsteps)
-{
-  return Condensation(tsteps.get_condstep(),
-                      config.doAlterThermo,
-                      config.cond_iters,
-                      &step2dimlesstime,
-                      config.cond_rtol,
-                      config.cond_atol,
-                      config.cond_SUBTSTEP,
+inline MicrophysicalProcess auto config_condensation(const Config &config,
+                                                     const Timesteps &tsteps) {
+  return Condensation(tsteps.get_condstep(), config.doAlterThermo, config.cond_iters,
+                      &step2dimlesstime, config.cond_rtol, config.cond_atol, config.cond_SUBTSTEP,
                       &realtime2dimless);
 }
 
-inline MicrophysicalProcess auto
-config_collisions(const Config &config, const Timesteps &tsteps)
-{
+inline MicrophysicalProcess auto config_collisions(const Config &config, const Timesteps &tsteps) {
   const PairProbability auto prob = LongHydroProb();
-  const MicrophysicalProcess auto colls = CollCoal(tsteps.get_collstep(),
-                                                   &step2realtime,
-                                                   prob);
+  const MicrophysicalProcess auto colls = CollCoal(tsteps.get_collstep(), &step2realtime, prob);
   return colls;
 }
 
-inline MicrophysicalProcess auto
-create_microphysics(const Config &config, const Timesteps &tsteps)
-{
-  const MicrophysicalProcess auto cond = config_condensation(config,
-                                                             tsteps);
+inline MicrophysicalProcess auto create_microphysics(const Config &config,
+                                                     const Timesteps &tsteps) {
+  const MicrophysicalProcess auto cond = config_condensation(config, tsteps);
 
-  const MicrophysicalProcess auto colls = config_collisions(config,
-                                                            tsteps);
+  const MicrophysicalProcess auto colls = config_collisions(config, tsteps);
 
   return cond >> colls;
 }
 
-inline Motion<CartesianMaps> auto
-create_motion(const unsigned int motionstep)
-{
+inline Motion<CartesianMaps> auto create_motion(const unsigned int motionstep) {
   const auto terminalv = RogersGKTerminalVelocity{};
 
-  return CartesianMotion(motionstep,
-                         &step2dimlesstime,
-                         terminalv);
+  return CartesianMotion(motionstep, &step2dimlesstime, terminalv);
 }
 
-inline Observer auto
-create_supersattrs_observer(const unsigned int interval,
-                            FSStore &store,
-                            const int maxchunk)
-{
-  SuperdropsBuffers auto buffers = SdIdBuffer() >>
-                                   XiBuffer() >>
-                                   MsolBuffer() >>
-                                   RadiusBuffer() >>
-                                   Coord3Buffer() >>
-                                   Coord1Buffer() >>
-                                   SdgbxindexBuffer();
+inline Observer auto create_supersattrs_observer(const unsigned int interval, FSStore &store,
+                                                 const int maxchunk) {
+  SuperdropsBuffers auto buffers = SdIdBuffer() >> XiBuffer() >> MsolBuffer() >> RadiusBuffer() >>
+                                   Coord3Buffer() >> Coord1Buffer() >> SdgbxindexBuffer();
   return SupersAttrsObserver(interval, store, maxchunk, buffers);
 }
 
-inline Observer auto
-create_observer(const Config &config,
-                const Timesteps &tsteps,
-                FSStore &store)
-{
+inline Observer auto create_observer(const Config &config, const Timesteps &tsteps,
+                                     FSStore &store) {
   const auto obsstep = (unsigned int)tsteps.get_obsstep();
   const auto maxchunk = int{config.maxchunk};
 
-  const Observer auto obs1 = PrintObserver(obsstep*10, &step2realtime);
+  const Observer auto obs1 = PrintObserver(obsstep * 10, &step2realtime);
 
-  const Observer auto obs2 = TimeObserver(obsstep, store, maxchunk,
-                                          &step2dimlesstime);
+  const Observer auto obs2 = TimeObserver(obsstep, store, maxchunk, &step2dimlesstime);
 
-  const Observer auto obs3 = NsupersObserver(obsstep, store, maxchunk,
-                                             config.ngbxs);
+  const Observer auto obs3 = NsupersObserver(obsstep, store, maxchunk, config.ngbxs);
 
-  const Observer auto obs4 = MassMomentsObserver(obsstep, store, maxchunk,
-                                                 config.ngbxs);
+  const Observer auto obs4 = MassMomentsObserver(obsstep, store, maxchunk, config.ngbxs);
 
-  const Observer auto obs5 = create_supersattrs_observer(obsstep, store,
-                                                          maxchunk);
+  const Observer auto obs5 = create_supersattrs_observer(obsstep, store, maxchunk);
 
   return obs1 >> obs2 >> obs3 >> obs4 >> obs5;
 }
 
-inline auto create_sdm(const Config &config,
-                       const Timesteps &tsteps,
-                       FSStore &store)
-{
+inline auto create_sdm(const Config &config, const Timesteps &tsteps, FSStore &store) {
   const auto couplstep = (unsigned int)tsteps.get_couplstep();
   const GridboxMaps auto gbxmaps(create_gbxmaps(config));
   const MicrophysicalProcess auto microphys(create_microphysics(config, tsteps));
   const Motion<CartesianMaps> auto movesupers(create_motion(tsteps.get_motionstep()));
   const Observer auto obs(create_observer(config, tsteps, store));
 
-  return SDMMethods(couplstep, gbxmaps,
-                    microphys, movesupers, obs);
+  return SDMMethods(couplstep, gbxmaps, microphys, movesupers, obs);
 }
 
-int main(int argc, char *argv[])
-{
-  if (argc < 2)
-  {
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
     throw std::invalid_argument("configuration file(s) not specified");
   }
 
   Kokkos::Timer kokkostimer;
 
   /* Read input parameters from configuration file(s) */
-  const std::string_view config_filename(argv[1]); // path to configuration file
+  const std::string_view config_filename(argv[1]);  // path to configuration file
   const Config config(config_filename);
-  const Timesteps tsteps(config); // timesteps for model (e.g. coupling and end time)
+  const Timesteps tsteps(config);  // timesteps for model (e.g. coupling and end time)
 
   /* Create zarr store for writing output to storage */
   FSStore fsstore(config.zarrbasedir);
@@ -226,9 +170,7 @@ int main(int argc, char *argv[])
 
     /* Solver of dynamics coupled to CLEO SDM */
     CoupledDynamics auto coupldyn(
-        create_coupldyn(config, sdm.gbxmaps,
-                        tsteps.get_couplstep(),
-                        tsteps.get_t_end()));
+        create_coupldyn(config, sdm.gbxmaps, tsteps.get_couplstep(), tsteps.get_t_end()));
 
     /* coupling between coupldyn and SDM */
     const CouplingComms<FromFileDynamics> auto comms = FromFileComms{};
@@ -240,8 +182,7 @@ int main(int argc, char *argv[])
   Kokkos::finalize();
 
   const auto ttot = double{kokkostimer.seconds()};
-  std::cout << "-----\n Total Program Duration: "
-            << ttot << "s \n-----\n";
+  std::cout << "-----\n Total Program Duration: " << ttot << "s \n-----\n";
 
   return 0;
 }
