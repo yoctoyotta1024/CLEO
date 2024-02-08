35841b97b83a0718c3535826a7edb84dadf5c781
clara bayley <yoctoyotta1024@yahoo.com>
2024-02-08 22:51:05 +0100

    more docs for SDs

--- docs/source/cxx/superdrops/superdrop_ids.rst
+++ docs/source/cxx/superdrops/superdrop_ids.rst
@@ -4,19 +4,19 @@ SuperdropIDs
 Header file: ``<libs/superdrops/superdrop_ids.hpp>``
 `[source] <https://github.com/yoctoyotta1024/CLEO/blob/main/libs/superdrops/superdrop_ids.hpp>`_
 
-.. doxygenclass:: IntID
+.. doxygenstruct:: IntID
    :project: superdrops
    :private-members:
    :protected-members:
    :members:
    :undoc-members:
 
-.. doxygenclass:: EmptyID
+.. doxygenstruct:: EmptyID
    :project: superdrops
    :private-members:
    :protected-members:
    :members:
    :undoc-members:
 
-.. doxygenfunction:: operator<<
+.. doxygenfunction:: operator<< // WIP TODO(CB) fix error
    :project: superdrops
--- libs/superdrops/superdrop.hpp
+++ libs/superdrops/superdrop.hpp
@@ -1,4 +1,5 @@
-/* Copyright (c) 2023 MPI-M, Clara Bayley
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
  *
  * ----- CLEO -----
  * File: superdrop.hpp
@@ -7,7 +8,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Saturday 30th December 2023
+ * Last Modified: Thursday 8th February 2024
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -26,22 +27,45 @@
 #include "./superdrop_attrs.hpp"
 #include "./superdrop_ids.hpp"
 
+/**
+ * @brief Class representing a super-droplet (synonyms: superdroplet, superdrop, SD).
+ *
+ * This class defines properties and operations of a super-droplet
+ * (synonyms: superdroplet, superdrop, SD).
+ */
 class Superdrop {
  private:
-  unsigned int sdgbxindex;  // matches index of gridbox the superdrop occupies
-  double coord3;            // a 3rd spatial coordinate of superdroplet (z)
-  double coord1;            // a 1st spatial coordinate of superdroplet (x)
-  double coord2;            // a 2nd spatial coordinate of superdroplet (y)
-  SuperdropAttrs attrs;     // attributes of a superdroplet
+  unsigned int sdgbxindex;  /**< Index of the gridbox the superdrop occupies */
+  double coord3;            /**< 3rd spatial coordinate of the superdroplet (z) */
+  double coord1;            /**< 1st spatial coordinate of the superdroplet (x) */
+  double coord2;            /**< 2nd spatial coordinate of the superdroplet (y) */
+  SuperdropAttrs attrs;     /**< Attributes of the superdroplet */
 
  public:
-  using IDType = IntID;  // type of ID (from superdrop_ids.hpp) to identify superdrop via integer
-  // using IDType = EmptyID; // empty type of ID (for non-existent superdrop identity)
+  using IDType = IntID;     /**< Type of ID to identify superdrop via 8 byte integer */
+  // using IDType = EmptyID; // Type of ID (for non-existent superdrop identity)
   [[no_unique_address]] IDType sdId;  // superdroplet (unique) identity
 
-  KOKKOS_INLINE_FUNCTION Superdrop() = default;   // Kokkos requirement for a (dual)View
-  KOKKOS_INLINE_FUNCTION ~Superdrop() = default;  // Kokkos requirement for a (dual)View
+  /**
+   * @brief Default constructor requirement for use of Superdrop in Kokkos View
+   */
+  KOKKOS_INLINE_FUNCTION Superdrop() = default;
 
+  /**
+   * @brief Default destructor requirement for use of Superdrop in Kokkos View
+   */
+  KOKKOS_INLINE_FUNCTION ~Superdrop() = default;
+
+  /**
+   * @brief Parameterized constructor.
+   *
+   * @param i_sdgbxindex Index of the gridbox the superdrop occupies.
+   * @param i_coord3 3rd spatial coordinate of the superdroplet.
+   * @param i_coord1 1st spatial coordinate of the superdroplet.
+   * @param i_coord2 2nd spatial coordinate of the superdroplet.
+   * @param i_attrs Attributes of the superdroplet.
+   * @param i_sdId Unique ID of the superdroplet.
+   */
   KOKKOS_INLINE_FUNCTION
   Superdrop(const unsigned int i_sdgbxindex, const double i_coord3, const double i_coord1,
             const double i_coord2, const SuperdropAttrs i_attrs, const IDType i_sdId)
@@ -52,24 +76,109 @@ class Superdrop {
         attrs(i_attrs),
         sdId(i_sdId) {}
 
+  /**
+   * @brief Get the index of the Gridbox the superdrop currently occupies.
+   *
+   * @return Gridbox Index.
+   */
   KOKKOS_INLINE_FUNCTION auto get_sdgbxindex() const { return sdgbxindex; }
+
+  /**
+   * @brief Get the 3rd spatial coordinate of the superdroplet.
+   *
+   * @return 3rd spatial coordinate.
+   */
   KOKKOS_INLINE_FUNCTION auto get_coord3() const { return coord3; }
+
+  /**
+   * @brief Get the 1st spatial coordinate of the superdroplet.
+   *
+   * @return 1st spatial coordinate.
+   */
   KOKKOS_INLINE_FUNCTION auto get_coord1() const { return coord1; }
+
+  /**
+   * @brief Get the 2nd spatial coordinate of the superdroplet.
+   *
+   * @return 2nd spatial coordinate.
+   */
   KOKKOS_INLINE_FUNCTION auto get_coord2() const { return coord2; }
 
+  /**
+   * @brief Returns 'true' if super-droplet has solute.
+   *
+   * @return boolean for existance of solute.
+   */
   KOKKOS_INLINE_FUNCTION auto is_solute() const { return attrs.is_solute(); }
+
+  /**
+   * @brief Get the solute of the superdroplet.
+   *
+   * @return super-droplet's solute
+   */
   KOKKOS_INLINE_FUNCTION auto get_solute() const { return attrs.get_solute(); }
+
+  /**
+   * @brief Get the density of the super-droplet's solute.
+   *
+   * @return density of the super-droplet's solute.
+   */
   KOKKOS_INLINE_FUNCTION auto get_rho_sol() const { return attrs.get_rho_sol(); }
+
+  /**
+   * @brief Get the molecular mass of the super-droplet's solute.
+   *
+   * @return molecular mass of the super-droplet's solute.
+   */
   KOKKOS_INLINE_FUNCTION auto get_mr_sol() const { return attrs.get_mr_sol(); }
+
+  /**
+   * @brief Get the van't Hoff ionic factor of the super-droplet's solute.
+   *
+   * @return van't Hoff ionic factor of the super-droplet's solute.
+   */
   KOKKOS_INLINE_FUNCTION auto get_ionic() const { return attrs.get_ionic(); }
 
+  /**
+   * @brief Get the multiplicity 'xi' of the super-droplet.
+   *
+   * @return multiplicity 'xi' of the super-droplet.
+   */
   KOKKOS_INLINE_FUNCTION auto get_xi() const { return attrs.xi; }
+
+  /**
+   * @brief Get the spherical radius of the super-droplet.
+   *
+   * @return spherical radius of the super-droplet.
+   */
   KOKKOS_INLINE_FUNCTION auto get_radius() const { return attrs.radius; }
+
+  /**
+   * @brief Get the mass of solute dissolved in the super-droplet.
+   *
+   * @return mass of solute in the super-droplet.
+   */
   KOKKOS_INLINE_FUNCTION auto get_msol() const { return attrs.msol; }
 
+  /**
+   * @brief Get the mass of the super-droplet.
+   *
+   * @return mass of the super-droplet.
+   */
   KOKKOS_INLINE_FUNCTION double mass() const { return attrs.mass(); }
+
+  /**
+   * @brief Get the volume of the super-droplet.
+   *
+   * @return volume of the super-droplet.
+   */
   KOKKOS_INLINE_FUNCTION double vol() const { return attrs.vol(); }
 
+  /**
+   * @brief Get the radius of the super-droplet cubed.
+   *
+   * @return radius of the super-droplet cubed.
+   */
   KOKKOS_INLINE_FUNCTION double rcubed() const { return attrs.rcubed(); }
 
   KOKKOS_INLINE_FUNCTION
