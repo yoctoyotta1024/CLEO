6a1d952a673e23663db4b9f66866fb249cae079e
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-01 13:47:29 +0100

    change job time allocation

--- examples/constthermo2d/constthermo2d.sh
+++ examples/constthermo2d/constthermo2d.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=128
 #SBATCH --mem=30G
-#SBATCH --time=00:30:00
+#SBATCH --time=00:10:00
 #SBATCH --mail-user=clara.bayley@mpimet.mpg.de
 #SBATCH --mail-type=FAIL
 #SBATCH --account=mh1126
