6b23222950e5d9062b2d5c3586b5214835c98b70
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-01 15:35:28 +0100

    move funcs into cpp file

--- libs/superdrops/collisionkinetics.hpp
+++ libs/superdrops/collisionkinetics.hpp
@@ -37,9 +37,15 @@ namespace DC = dimmed_constants;
 /* returns cke, where cke = collision kinetic energy
 as formulated in Low and List 1982(a) eqn 3.1 given
 radii r1 and r2 and terminal velocities of droplets */
-KOKKOS_INLINE_FUNCTION
+KOKKOS_FUNCTION
 double collision_kinetic_energy(const double r1, const double r2, const double terminalv1,
                                 const double terminalv2);
+
+/* returns surface energy of single spherical equivalent, ie.
+coalesced state of two drops, divided by pi as in
+equation 4.3 of Low and List 1982 */
+KOKKOS_FUNCTION
+double coal_surfenergy(const double r1, const double r2);
 
 /* returns energy due to surface tension of a single
 drop, analogous to equation 4.2 of Low and List 1982 */
@@ -58,48 +64,6 @@ double total_surfenergy(const double r1, const double r2) {
   const auto rsqrdsum = double{(r1 * r1) + (r2 * r2)};  // * R0sqrd to convert to [m^2]
 
   return dlc::surfconst * rsqrdsum;  // = tot_surfe, total surface energy
-}
-
-/* returns surface energy of single spherical equivalent, ie.
-coalesced state of two drops, divided by pi as in
-equation 4.3 of Low and List 1982 */
-KOKKOS_INLINE_FUNCTION
-double coal_surfenergy(const double r1, const double r2);
-
-/* -----  ----- TODO: move functions below to .cpp file ----- ----- */
-
-/* returns cke, where cke = collision kinetic energy
-as formulated in Low and List 1982(a) eqn 3.1 given
-radii r1 and r2 and terminal velocities of droplets */
-KOKKOS_INLINE_FUNCTION
-double collision_kinetic_energy(const double r1, const double r2, const double terminalv1,
-                                const double terminalv2) {
-  constexpr double R0cubed = dlc::R0 * dlc::R0 * dlc::R0;  // convert r^3 to [m^3]
-  constexpr double ckeconst =
-      R0cubed * 2.0 / 3.0 * DC::RHO_L * Kokkos::numbers::pi * dlc::W0 * dlc::W0;
-
-  const auto r1cubed = double{r1 * r1 * r1};
-  const auto r1_r2cubed = double{(r1 / r2) * (r1 / r2) * (r1 / r2)};
-  const auto rratio = double{r1cubed / (1 + r1_r2cubed)};  // * R0cubed to convert to [m^3]
-
-  const auto vdiff = double{terminalv1 - terminalv2};  // * dlc::W0 to convert to [m/s]
-  const auto cke = double{ckeconst * rratio * vdiff * vdiff};
-
-  return cke;
-}
-
-/* returns surface energy of single spherical equivalent, ie.
-coalesced state of two drops, divided by pi as in
-equation 4.3 of Low and List 1982 */
-KOKKOS_INLINE_FUNCTION
-double coal_surfenergy(const double r1, const double r2) {
-  const auto r1cubed = double{r1 * r1 * r1};
-  const auto r2cubed = double{r2 * r2 * r2};
-  const auto rcubedsum = double{r1cubed + r2cubed};
-
-  const auto equiv_surfe = double{dlc::surfconst * Kokkos::pow(rcubedsum, 2.0 / 3.0)};
-
-  return equiv_surfe;  // coalesced (spherical equivalent) surface energy
 }
 
 #endif  // LIBS_SUPERDROPS_COLLISIONKINETICS_HPP_
