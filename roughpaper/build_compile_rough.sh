a3cacc5a1ec224be21698228a0976b85edaf793a
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-02 11:46:59 +0100

    much nicer bash script

--- roughpaper/build_compile_rough.sh
+++ roughpaper/build_compile_rough.sh
@@ -16,29 +16,41 @@
 ### ----- default compiler and python environment   ---- ###
 ### ----  and paths for CLEO and build directories  ---- ###
 module load gcc/11.2.0-gcc-11.2.0
-module load python3/2022.01-gcc-11.2.0
 module load nvhpc/23.7-gcc-11.2.0
 spack load cmake@3.23.1%gcc
 source activate /work/mh1126/m300950/condaenvs/cleoenv
 path2build=${HOME}/CLEO/roughpaper/build/
-python=python
 gxx="g++"
 gcc="gcc"
-cuda="nvc++"
+cuda="nvcc"
 ### ---------------------------------------------------- ###
 
 ### ------------ choose Kokkos configuration ----------- ###
-kokkosflags="-DKokkos_ARCH_NATIVE=ON -DKokkos_ARCH_AMPERE80=ON -DKokkos_ENABLE_SERIAL=ON" # serial kokkos
-# kokkosdevice="-DKokkos_ENABLE_CUDA=ON -DKokkos_ENABLE_CUDA_LAMBDA=ON"                     # flags for device parallelism (e.g. on gpus)
-# kokkoshost="-DKokkos_ENABLE_OPENMP=ON"                                                  # flags for host parallelism (e.g. using OpenMP)
-kokkoshost=""                                                                             # flags for host parallelism (e.g. using OpenMP)
+
+kokkosflags="-DKokkos_ARCH_NATIVE=ON -DKokkos_ARCH_AMPERE80=ON -DKokkos_ENABLE_SERIAL=ON"                 # serial kokkos
+# kokkosdevice="-DKokkos_ENABLE_CUDA=ON -DKokkos_ENABLE_CUDA_LAMBDA=ON -DKokkos_ENABLE_CUDA_CONSTEXPR=ON" # flags for device parallelism (e.g. on gpus)
+# kokkoshost="-DKokkos_ENABLE_OPENMP=ON"                                                                  # flags for host parallelism (e.g. using OpenMP)
+### ---------------------------------------------------- ###
+
+### ------------ choose extra compiler flags ----------- ###
+flags="-g -O0 -mpc64"                                            # correctness
+# flags="-O3"                                                    # performance
 ### ---------------------------------------------------- ###
 
 ### ------------------ build_compile.sh ---------------- ###
 ### build CLEO using cmake (with optional thread parallelism through Kokkos)
-buildcmd="CXX=${gxx} CC=${gcc} CUDA=${cuda} cmake -S ./ -B ${path2build} ${kokkosflags} ${kokkosdevice} ${kokkoshost}"
-echo ${buildcmd}
-CXX=${gxx} CC=${gcc} CUDA=${cuda} cmake -S ./ -B ${path2build} ${kokkosflags} ${kokkosdevice} ${kokkoshost}
+echo "CXX=${gxx} CC=${gcc} CUDA=${cuda}"
+echo "BUILD_DIR: ${path2build}"
+echo "KOKKOS_FLAGS: ${kokkosflags} ${kokkosdevice} ${kokkoshost}"
+echo "CXX_COMPILER_FLAGS: ${flags}"
+
+CXX=${gxx} CC=${gcc} CUDA=${cuda} cmake -DCMAKE_CXX_COMPILER=${gxx} \
+                                      -DCMAKE_CC_COMPILER=${gcc} \
+                                      -DCMAKE_CUDA_COMPILER=${cuda} \
+                                      -DCMAKE_CXX_FLAGS="${flags}" \
+                                      -S ../ -B ${path2build} \
+                                      ${kokkosflags} ${kokkosdevice} ${kokkoshost} && \
+                                      cmake --build ${path2build}  --parallel
 
 ### compile CLEO
 cd ${path2build} && pwd
