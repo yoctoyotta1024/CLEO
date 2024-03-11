067d095a8353729824d4e11611cd4a9ab8b232ef
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-11 09:58:55 +0100

    more functions into cpp files

--- libs/superdrops/superdrop_attrs.cpp
+++ libs/superdrops/superdrop_attrs.cpp
@@ -1,13 +1,15 @@
-/* Copyright (c) 2023 MPI-M, Clara Bayley
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
  *
  * ----- CLEO -----
  * File: superdrop_attrs.cpp
  * Project: superdrops
- * Created Date: Friday 13th October 2023
+ * Created Date: Wednesday 8th November 2023
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Wednesday 8th November 2023
+ * Last Modified: Monday 11th March 2024
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -20,4 +22,45 @@
  * to Climate" by Lohmann, Luond and Mahrt, 1st edition.
  */
 
+
 #include "./superdrop_attrs.hpp"
+
+/**
+ * @brief Change the radius of droplet.
+ *
+ * Update droplet radius to larger out of new radius 'newr' or dry radius and return the
+ * resultant change in radius = new radius - old radius. Prevents drops shrinking further once
+ * they are size of dry radius.
+ *
+ * @param newr The new radius to set.
+ * @return The change in radius.
+ */
+KOKKOS_FUNCTION
+double SuperdropAttrs::change_radius(const double newr) {
+  const auto oldradius = radius;
+
+  /* if droplets are dry, do not shrink further */
+  const auto dryr = dryradius();
+  radius = Kokkos::fmax(newr, dryr);  // Kokkos equivalent to std::max() for floats (gpu compatible)
+
+  /* return change in radius due to growth/shrinking of droplet */
+  return radius - oldradius;
+}
+
+/**
+ * @brief Get the total droplet mass.
+ *
+ * Calculates and returns total droplet mass = water + dry areosol.
+ *
+ * @return The total droplet mass.
+ */
+KOKKOS_FUNCTION
+double SuperdropAttrs::mass() const {
+  constexpr double massconst(4.0 / 3.0 * Kokkos::numbers::pi * dlc::Rho_l);  // 4/3 * pi * density
+  const auto density_factor = double{1.0 - dlc::Rho_l / solute.rho_sol()};   // to account for msol
+
+  auto mass = double{msol * density_factor};  // mass contribution of solute
+  mass += massconst * rcubed();
+
+  return mass;
+}
