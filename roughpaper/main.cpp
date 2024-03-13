43596a5ba4f17ea588a3f469c5b91b61375387f8
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-14 00:16:59 +0100

    missing update of json in destructor after writing chunk

--- roughpaper/main.cpp
+++ roughpaper/main.cpp
@@ -9,7 +9,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Wednesday 13th March 2024
+ * Last Modified: Thursday 14th March 2024
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -28,7 +28,7 @@
 #include "./zarr/fsstore_array.hpp"
 
 dualview_type observer() {
-  auto data = dualview_type("data", 5);
+  auto data = dualview_type("data", 6);
 
   // initialise data in host view
   auto h_data = data.view_host();
@@ -37,6 +37,7 @@ dualview_type observer() {
   h_data(2) = 3.3;
   h_data(3) = 4.4;
   h_data(4) = 5.5;
+  h_data(5) = 6.6;
   data.modify_host();
 
   // match device data with host
@@ -51,8 +52,8 @@ int main(int argc, char *argv[]) {
     const std::filesystem::path basedir("/home/m/m300950/CLEO/roughpaper/build/bin/");
     auto store = FSStore(basedir);
 
-    auto zarr = FSStoreArrayViaBuffer(store, std::vector<size_t>({2, 2}), "radius", "micro-m", 10.0,
-      "<f8", std::vector<std::string>({ "sdId", "time"}));
+    auto zarr = FSStoreArrayViaBuffer(store, std::vector<size_t>({8}), "radius", "micro-m", 10.0,
+      "<f8", std::vector<std::string>({ "sdId"}));
 
     // arrays of data returned by observer (maybe on device)
     auto data = observer();
