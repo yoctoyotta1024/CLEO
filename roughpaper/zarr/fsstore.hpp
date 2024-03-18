cde0333a240e289c1f79378fb1a411337fa90fac
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-18 15:07:15 +0100

    zarr group created

--- roughpaper/zarr/fsstore.hpp
+++ roughpaper/zarr/fsstore.hpp
@@ -7,7 +7,7 @@
  * Project: zarr
  * Created Date: Monday 18th March 2024
  * Author: Clara Bayley (CB)
- * Additional Contributors:
+ * Additional Contributors: Tobias Kölling (TB)
  * -----
  * Last Modified: Monday 18th March 2024
  * Modified By: CB
@@ -31,28 +31,63 @@
 
 #include "./store_accessor.hpp"
 
-/* A file system (with root in 'basedir' directory) obeying Zarr
-version 2 requirements for a Store. Store contins a series
-of key, values where values may be data arrays or groups in the store.
-data for a given key is written to the store via the functions
-in StoreAccessor */
+/**
+ * @brief A file system store e.g. for Zarr arrays or groups.
+ *
+ * This class represents a file system store for a series of key-value pairs for example for
+ * storing Zarr data arrays or groups. Data for a given key is written to the store via helper
+ * functions from the StoreAccessor struct.
+ *
+ */
 class FSStore {
  private:
-  const std::filesystem::path basedir;
+  const std::filesystem::path basedir;   /**< The root directory of the file system store. */
 
  public:
+  /**
+   * @brief Constructs an FSStore object with the specified base directory.
+   *
+   * @param basedir The root directory of the file system store.
+   */
   explicit FSStore(const std::filesystem::path basedir) : basedir(basedir) {}
 
+  /**
+   * @brief Operator to use a StoreAccessor to write values under a given key.
+   *
+   * Usage: `operator[y] = x;` writes values x under a key called 'y' using the StoreAccessor.
+   *
+   * @param key The key for which the StoreAccessor is accessed.
+   * @return A StoreAccessor object associated with the specified key.
+   */
   StoreAccessor<FSStore> operator[](const std::string_view key) { return { *this, key }; }
 
-  /* write function called by StoreAccessor once data has been
-  converted into a vector of unsigned integer types */
+  /**
+   * @brief Write function called by StoreAccessor to write data to file system storage after the
+   * data has been converted into a vector of unsigned integer types.
+   *
+   * This function can be used by a StoreAccessor object to write data represented as a vector of
+   * unsigned integer types to the file system store under the specified key.
+   *
+   * @param key The key under which the data will be stored in the file system store.
+   * @param buffer A span representing the range of memory containing the unsigned bytes to be written.
+   * @return True if the write operation is successful, false otherwise.
+   */
   inline bool write(const std::string_view key, const std::span<const uint8_t> buffer);
 };
 
-/* write function called by StoreAccessor
-once data has been converted into a vector
-of unsigned integer types TODO(CB): move to .cpp file*/
+/**
+ * @brief Write function called by StoreAccessor to write data to file system storage after the
+ * data has been converted into a vector of unsigned integer types.
+ *
+ * This function can be used by a StoreAccessor object to write data represented as a vector of
+ * unsigned integer types to the file system store under the specified key.
+ *
+ * TODO(ALL): move this function to a .cpp file
+ *
+ * @param key The key under which the data will be stored in the file system store.
+ * @param buffer A span representing the range of memory containing the unsigned bytes to be written.
+ * @return True if the write operation is successful, false otherwise.
+ */
 inline bool FSStore::write(std::string_view key, std::span<const uint8_t> buffer) {
   auto path = basedir / key;
   auto mode = std::ios::out | std::ios::binary;
--- roughpaper/zarr/zarr_group.hpp
+++ roughpaper/zarr/zarr_group.hpp
@@ -16,10 +16,53 @@
  * https://opensource.org/licenses/BSD-3-Clause
  * -----
  * File Description:
+ * Structure to create a group obeying the Zarr storage specification version 2
+ * (https://zarr.readthedocs.io/en/stable/spec/v2.html) in a given memory store.
  */
 
 
 #ifndef ROUGHPAPER_ZARR_ZARR_GROUP_HPP_
 #define ROUGHPAPER_ZARR_ZARR_GROUP_HPP_
+
+#include <string>
+
+/**
+* @brief A class representing a Zarr group (i.e. collection of Zarr arrays) in a storage system.
+*
+* This class provides functionality to create a group of arrays obeying the Zarr storage
+* specification version 2 (https://zarr.readthedocs.io/en/stable/spec/v2.html) within a store
+* object that manages the storage and retrieval of data and metadata.
+*
+* @tparam Store The type of the store object used by the Zarr group.
+*/
+template <typename Store>
+class ZarrGroup {
+ private:
+  Store& store;   ///< Reference to the store object.
+
+ public:
+  /**
+  * @brief Constructs a ZarrGroup with the specified store object.
+  *
+  * This constructor initializes a ZarrGroup with the provided store object.
+  * It also writes the required metatdata for the group in order to obey the Zarr storage
+  * specification version 2 (https://zarr.readthedocs.io/en/stable/spec/v2.html).
+  *
+  * @param store The store object associated with the Zarr group.
+  */
+  explicit ZarrGroup(const Store store) : store(store) {
+    /* write compulsory metadata for Zarr group */
+    const std::string zarr_format("2");   // storage specification version 2
+    const std::string zgroupjson("{\n  \"zarr_format\": " + zarr_format + "\n}");
+    store[".zgroup"] = zgroupjson;
+
+    /* write optional metadata about group (maybe useful for xarray and netCDF compatibility) */
+    store[".zattrs"] =
+      "{\n"
+      "  \"creator\": \"Clara Bayley\",\n"
+      "  \"title\": \"Zarr Group for Data Output from CLEO\""
+      "\n}";
+  }
+};
 
 #endif   // ROUGHPAPER_ZARR_ZARR_GROUP_HPP_
