f8994b101a4647568a7812a42d656a666076d9c3
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-18 21:52:15 +0100

    correct includes and helper funcs

--- roughpaper/zarr/buffer.hpp
+++ roughpaper/zarr/buffer.hpp
@@ -23,6 +23,30 @@
 #ifndef ROUGHPAPER_ZARR_BUFFER_HPP_
 #define ROUGHPAPER_ZARR_BUFFER_HPP_
 
+#include <limits>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+/* returns product of a vector of size_t numbers */
+inline size_t vec_product(const std::vector<size_t>& vec) {
+  auto value = size_t{1};
+  for (const auto& v : vec) {
+    value *= v;
+  }
+  return value;
+}
+
+/* returns product of a vector of size_t numbers starting from aa'th index of vector */
+inline size_t vec_product(const std::vector<size_t>& vec, const size_t aa) {
+  auto value = size_t{1};
+  for (auto it = vec.begin() + aa; it != vec.end(); ++it) {
+    value *= *it;
+  }
+  return value;
+}
+
 /**
  * @brief A class template for managing a buffer of elements of data type T.
  *
