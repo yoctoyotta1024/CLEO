97b4516dc78bcd2887ff3f562b0ef9552a788ad1
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-18 14:41:17 +0100

    helper functions in store_accessor

--- roughpaper/zarr/store_accessor.hpp
+++ roughpaper/zarr/store_accessor.hpp
@@ -109,4 +109,42 @@ struct StoreAccessor {
   }
 };
 
+
+/**
+ * @brief Write metadata string to a store under a .zarray key.
+ *
+ * write metadata under .zarray key in store for an array called 'name'. The key and metadata
+ * could be anything, but for example .zarray could be a json file in a file system store
+ * (see FSStore) for the metadata which must exist in order to decode chunks of an array according
+ * to zarr storage specification version 2 (https://zarr.readthedocs.io/en/stable/spec/v2.html),
+ *
+ * @tparam Store The type of the store object where the metadata will be written.
+ * @param store The store object where the metadata will be written.
+ * @param name The name under which the .zarray key will be stored in the store.
+ * @param metadata The metadata to write for the .zarray key.
+ */
+template <typename Store>
+inline void write_zarray_json(Store& store, std::string_view name, std::string_view metadata) {
+  store[std::string(name) + "/.zarray"] = metadata;
+}
+
+/**
+ * @brief Write attributes string to a store under a .zattrs key.
+ *
+ * Write some data under .zattrs key in store for an array called 'name'. The key and attrs data
+ * could be anything, but for example .zattrs could be a json file in a file system store
+ * (see FSStore) for the extra metadata which must exist in order to make xarray and netCDF
+ * happy when opening a zarr dataset, e.g. by naming the dimensions of the
+ * "{\"_ARRAY_DIMENSIONS\": [\"dimension_name\"]}";.
+ *
+ * @tparam Store The type of the store object where the metadata will be written.
+ * @param store The store object where the metadata will be written.
+ * @param name The name under which the .zarray key will be stored in the store.
+ * @param metadata The metadata to write for the .zarray key.
+ */
+template <typename Store>
+inline void write_zattrs_json(Store& store, std::string_view name, std::string_view attrs) {
+  store[std::string(name) + "/.zattrs"] = attrs;
+}
+
 #endif   // ROUGHPAPER_ZARR_STORE_ACCESSOR_HPP_
