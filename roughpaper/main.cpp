8f05c4dd720c416704cc7d328689c642274e6db5
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-18 12:01:54 +0100

    worksi in 2-D too

--- roughpaper/main.cpp
+++ roughpaper/main.cpp
@@ -51,12 +51,12 @@ int main(int argc, char *argv[]) {
     const std::filesystem::path basedir("/home/m/m300950/CLEO/roughpaper/build/bin/dataset.zarr");
     auto store = FSStore(basedir);
 
-    auto zarr = FSStoreArrayViaBuffer<double>(store, std::vector<size_t>({6}), "radius",
-      "micro-m", 10.0, "<f8", std::vector<std::string>({"sdId"}));
+    // auto zarr = FSStoreArrayViaBuffer<double>(store, std::vector<size_t>({6}), "radius",
+    //   "micro-m", 10.0, "<f8", std::vector<std::string>({"sdId"}));
 
-    // auto zarr = FSStoreArrayViaBuffer<double>(store, std::vector<size_t>({8, 2}), "massmom",
-    //   "", 1.0, "<f8", std::vector<std::string>({"time", "gbx"}),
-    //   std::vector<size_t>({2}));
+    auto zarr = FSStoreArrayViaBuffer<double>(store, std::vector<size_t>({12, 2}), "massmom",
+      "", 1.0, "<f8", std::vector<std::string>({"time", "gbx"}),
+      std::vector<size_t>({2}));
 
     // arrays of data returned by observer (maybe on device)
     auto data = observer();
