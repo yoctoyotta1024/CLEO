a144de2d6dce007709f72e7de465e30c225db07c
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-19 09:34:04 +0100

    new file for xarray compatible zarr array

---
+++ roughpaper/zarr/xarray_zarr_array.hpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
+ *
+ * ----- CLEO -----
+ * File: xarray_zarr_array.hpp
+ * Project: zarr
+ * Created Date: Monday 18th March 2024
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Tuesday 19th March 2024
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * Structure to create a group obeying the Zarr storage specification version 2
+ * (https://zarr.readthedocs.io/en/stable/spec/v2.html) in a given memory store.
+ */
+
+
+#ifndef ROUGHPAPER_ZARR_XARRAY_ZARR_ARRAY_HPP_
+#define ROUGHPAPER_ZARR_XARRAY_ZARR_ARRAY_HPP_
+
+#include "./zarr_array.hpp"
+
+template <typename Store, typename T>
+class XarrayZarrArray {
+ private:
+  ZarrArray<Store, T> zarr;
+
+ public:
+  XarrayZarrArray(Store& store, const std::string_view name, const std::string_view units,
+    const double scale_factor, const std::string_view dtype, const std::vector<std::string>& dims,
+    const std::vector<size_t>& chunkshape,
+    const std::vector<size_t>& reduced_arrayshape = std::vector<size_t>({}))
+    : ZarrArray(store, name, units, scale_factor, dtype, dims, chunkshape, reduced_arrayshape) {
+    // /* make string of zattrs attribute information for array in zarr store */
+    // const auto arrayattrs = std::string(
+    //   "{\n"
+    //   "  \"_ARRAY_DIMENSIONS\": " +
+    //   vecstr_to_string(dims) +                // names of each dimension of array
+    //   ",\n"
+    //   "  \"units\": " +
+    //   "\"" + std::string(units) + "\"" +    // units of coordinate being stored
+    //   ",\n"
+    //   "  \"scale_factor\": " +
+    //   std::to_string(scale_factor) +        // scale_factor of data
+    //   "\n}");
+
+    // write_zattrs_json(store, name, arrayattrs);
+    }
+};
+
+#endif   // ROUGHPAPER_ZARR_XARRAY_ZARR_ARRAY_HPP_
