d35caecace35a8b1b8e3790b6b5070686787b885
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-19 09:33:17 +0100

    docstrings

--- roughpaper/zarr/zarr_array.hpp
+++ roughpaper/zarr/zarr_array.hpp
@@ -9,7 +9,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Monday 18th March 2024
+ * Last Modified: Tuesday 19th March 2024
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -24,10 +24,11 @@
 #ifndef ROUGHPAPER_ZARR_ZARR_ARRAY_HPP_
 #define ROUGHPAPER_ZARR_ZARR_ARRAY_HPP_
 
-#include <iostream>
 #include <string>
-#include <string_view>
 #include <vector>
+#include <iostream>
+#include <cassert>
+#include <string_view>
 
 #include "./buffer.hpp"
 #include "./chunks.hpp"
@@ -50,8 +51,60 @@ inline void write_zarray_json(Store& store, std::string_view name, std::string_v
   store[std::string(name) + "/.zarray"] = metadata;
 }
 
-/* converts vector of integers, e.g. for shape of chunks and array in zarr_metadata, into a single
-list written as a string */
+/**
+ * @brief Generates part of the metadata for a Zarr array .zarray json file.
+ *
+ * This function constructs a string containing all the compulsory metadata of a Zarr array for its
+ * .zarray json file, excluding the array's shape.
+ *
+ * @param chunkshape The shape of individual data chunks along each dimension.
+ * @param dtype The data type stored in the arrays (e.g., "<f8").
+ * @return A string view containing the partial metadata for the Zarr array.
+ */
+inline std::string_view make_part_metadata(const std::vector<size_t>& chunkshape,
+  const std::string_view dtype) {
+  const auto order = 'C';        // layout of bytes in each chunk of array in storage ('C' or 'F')
+  const auto compressor = std::string{ "null" };   // compression of data when writing to store
+  const auto fill_value = std::string{ "null" };   // fill value for empty datapoints in array
+  const auto filters = std::string{ "null" };      // codec configurations for compression
+  const auto zarr_format = '2';                    // storage spec. version 2
+
+  const auto part_metadata = std::string_view(
+    "  \"chunks\": " +
+    vec_to_string(chunkshape) +
+    ",\n"
+    "  \"dtype\": \"" +
+    std::string(dtype) +
+    "\",\n"
+    "  \"order\": \"" +
+    order +
+    "\",\n"
+    "  \"compressor\": " +
+    compressor +
+    ",\n"
+    "  \"fill_value\": " +
+    fill_value +
+    ",\n"
+    "  \"filters\": " +
+    filters +
+    ",\n"
+    "  \"zarr_format\": " +
+    zarr_format);
+
+  return part_metadata;
+}
+
+/**
+ * @brief Converts a vector of integers into a single list written as a string.
+ *
+ * Given vector of a type convertible to a string with values [a, b, c, ..., z], function returns
+ * the string "[a, b, c, ..., z]" with elements separated by commas and enclosed in square brackets.
+ * Function is useful for converting vectors representing the shape of chunks and arrays etc. into
+ * a string format for metadata json files.
+ *
+ * @param vals The vector values of a type convertible to a string.
+ * @return A string representation of the vector.
+ */
 inline std::string vec_to_string(const std::vector<size_t> &vals) {
   auto vals_str = std::string{ "[" };
   for (const auto& v : vals) { vals_str += std::to_string(v) + ", "; }
@@ -60,6 +113,15 @@ inline std::string vec_to_string(const std::vector<size_t> &vals) {
   return vals_str;
 }
 
+/**
+ * @brief A template class representing a Zarr array.
+ *
+ * This class provides functionality to write an array to a specified store via a buffer according
+ * to the Zarr storage specification version 2 (https://zarr.readthedocs.io/en/stable/spec/v2.html).
+ *
+ * @tparam Store The type of store where the array will be stored.
+ * @tparam T The data type stored in the arrays.
+ */
 template <typename Store, typename T>
 class ZarrArray {
  private:
@@ -72,24 +134,38 @@ class ZarrArray {
   std::vector<size_t> arrayshape;   ///< Number of elements in array along each dimension in store.
   Chunks chunks;                    ///< Method to write chunks of array in store.
   Buffer<T> buffer;                 ///< Buffer to hold data before writing chunks to store.
-  std::string partial_metadata;     ///< Metadata required for zarr array excluding array's shape
+  std::string_view part_metadata;   ///< Metadata required for zarr array excluding array's shape
 
-  /* make string of metadata for array in zarr store */
+  /**
+   * @brief Generates the compulsory metadata for the Zarr array .zarray json file.
+   *
+   * @return A string containing the metadata for the Zarr array.
+   */
   std::string zarr_metadata() const {
     const auto metadata = std::string(
       "{\n"
       "  \"shape\": " +
       vec_to_string(arrayshape) +
       ",\n" +
-      std::string(partial_metadata) +
+      std::string(part_metadata) +
       "\n}");
 
     return metadata;
   }
 
-  /* boolean returns increment to add to first dimension of array shape if chunk number indicates
-  shape of array (and metadata) should be updated because the reduced array shape is complete,
-  i.e. because the array along all but its outermost dimension is full of data elements */
+  /**
+   * @brief Determines and returns the change in shape of the array based on the chunk number.
+   *
+   * Returns increment to add to first dimension of the array's shape. Return value is 0 (= false)
+   * if no change to the shape of the array is determined. If the chunk number "chunk_num" completes
+   * the reduced array shape, i.e. writing to this chunk number would indicate that the array
+   * along all but its outermost dimension is full of data elements, then the return is the
+   * increase in the number of elements along the array's outermost dimension.
+   *
+   * @param chunk_num The current chunk number being written.
+   * @param shape_increment The increment to add to the array's outermost dimesion if necessary.
+   * @return The increment to add to the shape of the array's outermost dimesion.
+   */
   size_t arrayshape_change(const size_t chunk_num, const size_t shape_increment) const {
     if (chunk_num % vec_product(chunks.get_reducedarray_nchunks()) == 0) {
       return shape_increment;   // true
@@ -98,24 +174,40 @@ class ZarrArray {
     }
   }
 
-  /* increment shape of outermost dimension of N-Dimensional array and update the array's metadata
-  .zarray json correspondingly. Function should only be called if is_arrayshape_change flag is
-  curerntly true */
+  /**
+   * @brief Updates the shape of the array (including its metadata) along its outermost dimension.
+   *
+   * Increase the shape of the outermost dimension of an N-Dimensional array by "shape_increment",
+   * and then update the .zarray json file for the array's metadata accordingly. Function should
+   * only be called if the return of arrayshape_change is true.
+   *
+   * @param shape_increment The increment to add to the shape of the array's outermost dimension.
+   */
   void update_arrayshape(const size_t shape_increment) {
-    arrayshape.at(0) += shape_increment;   // increase in shape of outermost dimension
-    write_zarray_json(store, name, zarr_metadata());   // update metadata
+    arrayshape.at(0) += shape_increment;
+    write_zarray_json(store, name, zarr_metadata());
   }
 
+  /**
+   * @brief Writes chunks of data from a kokkos view in host memory to the Zarr array in a store.
+   *
+   * First writes the buffer to a chunk of the array if it's full. Secondly writes whole chunks
+   * directly from the Kokkos view if the view contains enough elements for whole chunk(s) to be
+   * written. Thirdly updates the shape of the array with the cumulative change in shape of the
+   * array due to the chunks that have been written. Finally returns a (sub)view of the remaining
+   * data not written to a chunk (number of elements in subview < chunksize).
+   *
+   * @param h_data Kokkos view of the data to write to the store in host memory.
+   * @return The remaining data that was not written to chunks.
+   */
   subviewh_buffer write_chunks_to_store(const subviewh_buffer h_data) {
     auto shape_increment = size_t{ 0 };
 
-    // write buffer to chunk if it's full
     if (buffer.get_space() == 0) {
       shape_increment += arrayshape_change(totnchunks, chunks.get_chunkshape().at(0));
       totnchunks = chunks.write_chunk<T>(store, name, totnchunks, buffer);
     }
 
-    // write whole chunks of h_data_remaining
     const auto nchunks_data = size_t{ h_data.extent(0) / buffer.get_chunksize() };
     for (size_t nn = 0; nn < nchunks_data; ++nn) {
       const auto csz = buffer.get_chunksize();
@@ -124,12 +216,10 @@ class ZarrArray {
       totnchunks = chunks.write_chunk<T>(store, name, totnchunks, Kokkos::subview(h_data, refs));
     }
 
-    // update shape of array if neccessary
     if (shape_increment) {
       update_arrayshape(store, shape_increment);
     }
 
-    // return remainder of data not written to chunks
     const auto n_to_chunks = nchunks_data * buffer.get_chunksize();
     const auto refs = kkpair_size_t({ n_to_chunks, h_data.extent(0) });
     return Kokkos::subview(h_data, refs);
@@ -137,12 +227,12 @@ class ZarrArray {
 
  public:
   /**
-  * @brief Writes a zarr array to a specified store via a buffer.
+  * @brief Constructs a ZarrArray object.
   *
-  * Initializes an empty array in the provided store in order to writes chunks of array to the
-  * store via a buffer. The assertions in this constructor ensure chunks are an appropriate size and
-  * shape for the array such that the final array dimensions are exactly integer multiples of its
-  * chunks along all but outermost (0th) dimension.
+  * Initializes an empty Zarr array in the provided store in order to writes chunks of an array to
+  * the store via a buffer. The assertions in this constructor ensure chunks are an appropriate size
+  * and shape for the array such that the final array dimensions are exactly integer multiples of
+  * its chunks along all but its outermost (0th) dimension.
   *
   * @param store The store where the array will be stored.
   * @param chunkshape The shape of individual data chunks along each dimension.
@@ -158,78 +248,37 @@ class ZarrArray {
     const std::vector<size_t>& chunkshape,
     const std::vector<size_t>& reduced_arrayshape = std::vector<size_t>({}))
     : store(store), name(name), totnchunks(0), arrayshape(chunkshape.size(), 0),
-    chunks(chunkshape, reduced_arrayshape), buffer(vec_product(chunks.get_chunkshape())) {
-    /* number of names of dimensions must match number of dimensions of chunks */
+    chunks(chunkshape, reduced_arrayshape), buffer(vec_product(chunks.get_chunkshape())),
+    part_metadata(make_part_metadata(chunkshape, dtype)) {
     assert((chunkshape.size() == dims.size()) &&
       "number of named dimensions of array must match number dimensinos of chunks");
 
-    /* number of dimensions for number of chunks must match number of dimensions of array */
     assert((chunkshape.size() == arrayshape.size()) &&
       "number of dimensions of chunks must match number of dimensions of array");
 
-    /* make string of zarray metadata for array in zarr store (incomplete because missing shape) */
-    const auto order = 'C';        // layout of bytes in each chunk of array in storage ('C' or 'F')
-    const auto compressor = std::string{ "null" };   // compression of data when writing to store
-    const auto fill_value = std::string{ "null" };   // fill value for empty datapoints in array
-    const auto filters = std::string{ "null" };      // codec configurations for compression
-    const auto zarr_format = '2';                    // storage spec. version 2
-
-    /* set array shape along all but the outermost dimension to the number of
-    elements given by the reduced array shape along that same dimension */
+    /* Along all but the outermost dimension, set array shape to the number of
+    elements given by the reduced array shape along that dimension */
     for (size_t aa = 0; aa < reduced_arrayshape.size(); ++aa) {
       arrayshape.at(aa + 1) = reduced_arrayshape.at(aa);
     }
 
-    partial_metadata = std::string(
-      "  \"chunks\": " +
-      vec_to_string(chunkshape) +
-      ",\n"
-      "  \"dtype\": \"" +
-      std::string(dtype) +  // dtype = datatype stored in arrays e.g. "<f8"
-      "\",\n"
-      "  \"order\": \"" +
-      order +
-      "\",\n"
-      "  \"compressor\": " +
-      compressor +
-      ",\n"
-      "  \"fill_value\": " +
-      fill_value +
-      ",\n"
-      "  \"filters\": " +
-      filters +
-      ",\n"
-      "  \"zarr_format\": " +
-      zarr_format);
-
     write_zarray_json(store, name, zarr_metadata());
-
-    // TODO(CB) move xarray metadata to dataset
-    // /* make string of zattrs attribute information for array in zarr store */
-    // const auto arrayattrs = std::string(
-    //   "{\n"
-    //   "  \"_ARRAY_DIMENSIONS\": " +
-    //   vecstr_to_string(dims) +                // names of each dimension of array
-    //   ",\n"
-    //   "  \"units\": " +
-    //   "\"" + std::string(units) + "\"" +    // units of coordinate being stored
-    //   ",\n"
-    //   "  \"scale_factor\": " +
-    //   std::to_string(scale_factor) +        // scale_factor of data
-    //   "\n}");
-
-    // write_zattrs_json(store, name, arrayattrs);
   };
 
+  /**
+   * @brief Destroys the ZarrArray object.
+   *
+   * Writes the buffer to a chunk of the array in the store if it isn't empty and updates the
+   * array's shape correspondingly.
+   */
   ~ZarrArray() {
-    /* write buffer to chunk if it isn't empty */
     if (buffer.get_fill() > 0) {
       const auto reduced_arraysize = chunks.get_reduced_arraysize();   // excluding outer dimension
       if (buffer.get_fill() % reduced_arraysize != 0) {
         const auto warning = std::string_view("WARNING: number of data elements in the buffer"
         " should be completely divisible by the number of elements in the array excluding its"
         " outermost dimension.\n         Some data in this array may be ignored or filled with"
-        " null / nan fill value.\n");
+        " null / nan fill value.\n");    // TODO(CB) check this
         std::cout << warning;
       }
 
@@ -240,6 +289,13 @@ class ZarrArray {
     }
   };
 
+  /**
+   * @brief Writes data to the Zarr array via a buffer.
+   *
+   * Copies data from TODO WIP
+   *
+   * @param h_data The data to write to the array.
+   */
   void write_data_to_zarr_array(const viewh_buffer h_data) {
     auto h_data_rem = buffer.copy_to_buffer(h_data);
 
