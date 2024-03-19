33ffc63b08527cc6f17a271ae9dd0e1be37fdde7
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-19 10:43:29 +0100

    move xarray info into xarray and format doc

--- roughpaper/zarr/xarray_zarr_array.hpp
+++ roughpaper/zarr/xarray_zarr_array.hpp
@@ -20,14 +20,16 @@
  * (https://zarr.readthedocs.io/en/stable/spec/v2.html) in a given memory store.
  */
 
-
 #ifndef ROUGHPAPER_ZARR_XARRAY_ZARR_ARRAY_HPP_
 #define ROUGHPAPER_ZARR_XARRAY_ZARR_ARRAY_HPP_
 
 #include "./zarr_array.hpp"
 
-/* Zarr array with additional metadata and constraining of shape of array to shape of dimensions
-in order to ensure Zarr array is compatibile with NetCDF and Xarray */
+/** Zarr array with additional metadata and constraining of shape of array to shape of dimensions
+ * in order to ensure Zarr array is compatibile with NetCDF and Xarray
+ * @param units The units of the array's coordinates.
+ * @param dims The names of each dimension of the array.
+ */
 template <typename Store, typename T>
 class XarrayZarrArray {
  private:
@@ -35,25 +37,28 @@ class XarrayZarrArray {
 
  public:
   XarrayZarrArray(Store& store, const std::string_view name, const std::string_view units,
-    const double scale_factor, const std::string_view dtype, const std::vector<std::string>& dims,
-    const std::vector<size_t>& chunkshape,
-    const std::vector<size_t>& reduced_arrayshape = std::vector<size_t>({}))
-    : ZarrArray(store, name, units, scale_factor, dtype, dims, chunkshape, reduced_arrayshape) {
+                  const double scale_factor, const std::string_view dtype,
+                  const std::vector<std::string>& dims, const std::vector<size_t>& chunkshape,
+                  const std::vector<size_t>& reduced_arrayshape = std::vector<size_t>({}))
+      : ZarrArray(store, name, dtype, chunkshape, reduced_arrayshape) {
+    assert((chunkshape.size() == dims.size()) &&
+           "number of named dimensions of array must match number dimensinos of chunks");
+
     /* make string of zattrs attribute information for array in zarr store */
     const auto arrayattrs = std::string(
-      "{\n"
-      "  \"_ARRAY_DIMENSIONS\": " +
-      vecstr_to_string(dims) +                // names of each dimension of array
-      ",\n"
-      "  \"units\": " +
-      "\"" + std::string(units) + "\"" +    // units of coordinate being stored
-      ",\n"
-      "  \"scale_factor\": " +
-      std::to_string(scale_factor) +        // scale_factor of data
-      "\n}");
+        "{\n"
+        "  \"_ARRAY_DIMENSIONS\": " +
+        vecstr_to_string(dims) +  // names of each dimension of array
+        ",\n"
+        "  \"units\": " +
+        "\"" + std::string(units) + "\"" +  // units of coordinate being stored
+        ",\n"
+        "  \"scale_factor\": " +
+        std::to_string(scale_factor) +  // scale_factor of data
+        "\n}");
 
     write_zattrs_json(store, name, arrayattrs);
-    }
+  }
 };
 
-#endif   // ROUGHPAPER_ZARR_XARRAY_ZARR_ARRAY_HPP_
+#endif  // ROUGHPAPER_ZARR_XARRAY_ZARR_ARRAY_HPP_
