122284fb65c7a20ed15eac1f9fb2ca89bbe5c367
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-20 09:47:20 +0100

    better bash

--- roughpaper/build_compile_test.sh
+++ roughpaper/build_compile_test.sh
@@ -79,14 +79,18 @@ echo "KOKKOS_DEVICE_PARALLELISM: ${kokkosdevice}"
 echo "KOKKOS_HOST_PARALLELISM: ${kokkoshost}"
 echo "CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}"
 
-# # build then compile in parallel
-# cmake -DCMAKE_CXX_COMPILER=${CXX} \
-#     -DCMAKE_CC_COMPILER=${CC} \
-#     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
-#     -S ${path2CLEO} -B ${path2build} \
-#     ${kokkosflags} ${kokkosdevice} ${kokkoshost} && \
-#     cmake --build --target test --parallel
+# build then compile in parallel
+if [[ $1 == "build" ]];
+then
+  cmake -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_CC_COMPILER=${CC} \
+      -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
+      -S ${path2CLEO} -B ${path2build} \
+      ${kokkosflags} ${kokkosdevice} ${kokkoshost} && \
+      cmake --build --target test --parallel
+else
+  cd ${path2build} && make -j 64 test
+fi
 
-cd ${path2build} && make -j 64 test
 ${path2CLEO}/roughpaper/build/roughpaper/test
 ### ---------------------------------------------------- ###
