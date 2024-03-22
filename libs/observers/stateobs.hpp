c7d3544e00ec7ae586b4a300333e0a2ae83718c6
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-22 09:25:22 +0100

    include wind buffers in state observer

--- libs/observers/stateobs.hpp
+++ libs/observers/stateobs.hpp
@@ -1,32 +1,31 @@
-/* Copyright (c) 2023 MPI-M, Clara Bayley
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
  *
  * ----- CLEO -----
  * File: stateobs.hpp
  * Project: observers
- * Created Date: Monday 23rd October 2023
+ * Created Date: Wednesday 24th January 2024
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Monday 23rd October 2023
+ * Last Modified: Friday 22nd March 2024
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
  * https://opensource.org/licenses/BSD-3-Clause
  * -----
  * File Description:
- * Observers to output variables from
- * gridboxes' state to arrays in a zarr
- * file system storage
+ * Observers to output variables from gridboxes' state to arrays in a zarr file system storage
  */
 
 #ifndef LIBS_OBSERVERS_STATEOBS_HPP_
 #define LIBS_OBSERVERS_STATEOBS_HPP_
 
+#include <Kokkos_Core.hpp>
 #include <concepts>
 #include <iostream>
 #include <memory>
-
-#include <Kokkos_Core.hpp>
 
 #include "../cleoconstants.hpp"
 #include "../kokkosaliases.hpp"
@@ -35,6 +34,7 @@
 #include "superdrops/state.hpp"
 #include "zarr/statebuffers.hpp"
 #include "zarr/twodstorage.hpp"
+#include "zarr/windbuffers.hpp"
 
 /* constructs observer of variables in the state
 of each gridbox with a constant timestep 'interval'
@@ -48,12 +48,15 @@ in a store as determined by the StateBuffers
 and TwoDMulitVarStorage types */
 class DoStateObs {
  private:
-  using store_type = TwoDMultiVarStorage<StateBuffers<double>, State>;
-  std::shared_ptr<store_type> zarr;
+  using store_type_thermo = TwoDMultiVarStorage<StateBuffers<double>, State>;
+  using store_type_winds = TwoDMultiVarStorage<WindBuffers<double>, State>;
+  std::shared_ptr<store_type_thermo> zarr_thermo;
+  std::shared_ptr<store_type_winds> zarr_winds;
 
  public:
   DoStateObs(FSStore &store, const int maxchunk, const size_t ngbxs)
-      : zarr(std::make_shared<store_type>(store, maxchunk, "<f8", ngbxs, "")) {}
+      : zarr_thermo(std::make_shared<store_type_thermo>(store, maxchunk, "<f8", ngbxs, "")),
+      : zarr_winds(std::make_shared<store_type_winds>(store, maxchunk, "<f8", ngbxs, "")) {}
 
   void before_timestepping(const viewh_constgbx h_gbxs) const {
     std::cout << "observer includes StateObserver\n";
@@ -68,7 +71,8 @@ class DoStateObs {
   to 2-D zarr storages as determined by the
   StateBuffers struct */
   void at_start_step(const unsigned int t_mdl, const Gridbox &gbx) const {
-    zarr->values_to_storage(gbx.state);
+    zarr_thermo->values_to_storage(gbx.state);
+    zarr_winds->values_to_storage(gbx.state);
   }
 };
 
