fc208e907d4950070bdd9eab468ebedef133ab6b
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-25 14:40:35 +0100

    winddata class for output from cleo

--- pySD/sdmout_src/thermodata.py
+++ pySD/sdmout_src/thermodata.py
@@ -6,7 +6,7 @@ Created Date: Tuesday 24th October 2023
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Monday 20th November 2023
+Last Modified: Monday 25th March 2024
 Modified By: CB
 -----
 License: BSD 3-Clause "New" or "Revised" License
@@ -15,7 +15,7 @@ https://opensource.org/licenses/BSD-3-Clause
 Copyright (c) 2023 MPI-M, Clara Bayley
 -----
 File Description:
-python class to handle thermodynamic
+python class to handle thermodynamics and wind fields
 data from SDM zarr store in cartesian
 domain
 '''
@@ -25,38 +25,38 @@ import xarray as xr
 
 from . import thermoeqns
 
+def thermotryopen_dataset(dataset):
+
+  if type(dataset) == str:
+    print("thermodynamic fields dataset: ", dataset)
+    return xr.open_dataset(dataset, engine="zarr", consolidated=False)
+  else:
+    return dataset
+
+def thermovar4d_fromzarr(ds, reshape, key):
+  '''' returns 4D variable with dims
+  [time, y, x, z] from zarr dataset "ds" '''
+
+  return np.reshape(ds[key].values, reshape)
+
 class Thermodata:
 
   def __init__(self, dataset, ntime, ndims, consts):
 
-    ds = self.tryopen_dataset(dataset)
+    ds = thermotryopen_dataset(dataset)
 
     self.consts = consts
 
     reshape = [ntime] + list(ndims)
-    self.press = self.var4d_fromzarr(ds, reshape, "press")
-    self.temp = self.var4d_fromzarr(ds, reshape, "temp")
-    self.qvap = self.var4d_fromzarr(ds, reshape, "qvap")
-    self.qcond = self.var4d_fromzarr(ds, reshape, "qcond")
+    self.press = thermovar4d_fromzarr(ds, reshape, "press")
+    self.temp = thermovar4d_fromzarr(ds, reshape, "temp")
+    self.qvap = thermovar4d_fromzarr(ds, reshape, "qvap")
+    self.qcond = thermovar4d_fromzarr(ds, reshape, "qcond")
     self.theta = self.potential_temp()
 
     self.press_units = ds["press"].units # probably hecto pascals
     self.temp_units = ds["temp"].units # probably kelvin
     self.theta_units = ds["temp"].units # probably kelvin
-
-  def tryopen_dataset(self, dataset):
-
-    if type(dataset) == str:
-      print("thermodata dataset: ", dataset)
-      return xr.open_dataset(dataset, engine="zarr", consolidated=False)
-    else:
-      return dataset
-
-  def var4d_fromzarr(self, ds, reshape, key):
-    '''' returns 4D variable with dims
-    [time, y, x, z] from zarr dataset "ds" '''
-
-    return np.reshape(ds[key].values, reshape)
 
   def potential_temp(self):
     ''' potential temperature, theta '''
@@ -116,6 +116,34 @@ class Thermodata:
       return self.relative_humidity()
     elif key == "supersat":
       return self.supersaturation()
+    else:
+      err = "no known return provided for "+key+" key"
+      raise ValueError(err)
+
+class Winddata:
+
+  def __init__(self, dataset, ntime, ndims, consts):
+
+    ds = thermotryopen_dataset(dataset)
+
+    self.consts = consts
+
+    reshape = [ntime] + list(ndims)
+    self.wvel = thermovar4d_fromzarr(ds, reshape, "wvel")
+    self.uvel = thermovar4d_fromzarr(ds, reshape, "uvel")
+    self.vvel = thermovar4d_fromzarr(ds, reshape, "vvel")
+
+    self.wvel_units = ds["wvel"].units # probably hecto pascals
+    self.uvel_units = ds["uvel"].units # probably hecto pascals
+    self.vvel_units = ds["vvel"].units # probably hecto pascals
+
+  def __getitem__(self, key):
+    if key == "wvel":
+      return self.wvel
+    elif key == "uvel":
+      return self.uvel
+    elif key == "vvel":
+      return self.vvel
     else:
       err = "no known return provided for "+key+" key"
       raise ValueError(err)
