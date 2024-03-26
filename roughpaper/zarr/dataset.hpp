72fb4dc0bb8b1c53bea373b7f9fc736c04051727
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-26 14:00:58 +0100

    docstrings

--- roughpaper/zarr/dataset.hpp
+++ roughpaper/zarr/dataset.hpp
@@ -31,7 +31,7 @@
  * @brief A class representing a dataset made from a Zarr group (i.e. collection of Zarr arrays)
  * in a storage system.
  *
- * This class provides functionality to create a dataset  as a group of arrays obeying the Zarr
+ * This class provides functionality to create a dataset as a group of arrays obeying the Zarr
  * storage specification version 2 (https://zarr.readthedocs.io/en/stable/spec/v2.html) that is also
  * compatible with Xarray and NetCDF.
  *
@@ -43,7 +43,7 @@ class Dataset {
   ZarrGroup<Store> group;  ///< Reference to the zarr group object.
   std::unordered_map<std::string, size_t>
       datasetdims;    ///< map from name of each dimension in dataset to their size
-  bool ischangedims;  ///< boolean is true if dimensions of dataset change
+  bool ischangedims;  ///< boolean is true if dimensions of the dataset change
 
  public:
   /**
@@ -62,16 +62,38 @@ class Dataset {
         "\n}";
   }
 
+  /**
+   * @brief Adds a dimension to the dataset.
+   *
+   * @param dim A pair containing the name and size of the dimension to be added.
+   */
   void add_dimension(const std::pair<std::string, std::size_t> &dim) {
     datasetdims.insert({dim.first, dim.second});
     ischangedims = true;
   }
 
+  /**
+   * @brief Sets the size of an existing dimension in the dataset.
+   *
+   * @param dim A pair containing the name of the dimension and its new size to be set.
+   */
   void set_dimension(const std::pair<std::string, std::size_t> &dim) {
     datasetdims.at(dim.first) = dim.second;
     ischangedims = true;
   }
 
+  /**
+   * @brief Creates a new array in the dataset.
+   *
+   * @tparam T The data type of the array.
+   * @param name The name of the new array.
+   * @param units The units of the array data.
+   * @param dtype The data type of the array.
+   * @param scale_factor The scale factor of array data.
+   * @param chunkshape The shape of the chunks of the array.
+   * @param dimnames The names of each dimension of the array.
+   * @return An instance of XarrayZarrArray representing the newly created array.
+   */
   template <typename T>
   XarrayZarrArray<Store, T> create_array(const std::string_view name, const std::string_view units,
                                          const std::string_view dtype, const double scale_factor,
@@ -81,6 +103,17 @@ class Dataset {
                                      chunkshape, dimnames);
   }
 
+  /**
+   * @brief Writes data from Kokkos view in host memory to a Zarr array in the dataset and ensures
+   * the shape of the array matches the dimensions of the dataset.
+   *
+   * Function writes data to an array in the dataset and updates the metadata for the shape of the
+   * array if the dimensions of the dataset have changed.
+   *
+   * @tparam T The data type of the array.
+   * @param xzarr An instance of XarrayZarrArray representing the array.
+   * @param h_data The data to be written to the array.
+   */
   template <typename T>
   void write_to_array(XarrayZarrArray<Store, T> &xzarr,
                       const Buffer<T>::viewh_buffer h_data) const {
