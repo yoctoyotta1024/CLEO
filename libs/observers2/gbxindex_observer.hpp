e4b36e490f427254c4327aad8ec4f6e67a0d4e63
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-28 22:26:51 +0100

    functions to write out gbxindex

--- libs/observers2/gbxindex_observer.hpp
+++ libs/observers2/gbxindex_observer.hpp
@@ -35,6 +35,18 @@
 #include "zarr2/dataset.hpp"
 #include "zarr2/xarray_zarr_array.hpp"
 
+struct GbxIndexFunctor {
+  viewd_constgbx d_gbxs;    // view of gridboxes
+  mirrorviewd_data d_data;  // mirror view on device for value of gbxindex from every gridbox
+
+  GbxIndexFunctor(const viewd_constgbx d_gbxs, mirrorviewd_data d_data)
+      : d_gbxs(d_gbxs), d_data(d_data) {}
+
+  // Functor operator to perform copy of gbxindex of each gridbox to d_data in parallel
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii) const { d_data(ii) = d_gbxs(ii).get_gbxindex(); }
+};
+
 /* template observer which writes gbxindex for every gridbox out to a 1-D array
 as a coordinate of an xarray dataset */
 template <typename Store>
@@ -43,10 +55,15 @@ class GbxindexObserver {
   Dataset<Store> &dataset;                                    ///< dataset to write gbxindex data to
   std::shared_ptr<XarrayZarrArray<Store, size_t>> xzarr_ptr;  ///< pointer to gbxindex array
 
-  /* increment size of gbxindex dimension in dataset and write out gbxindex data
-  to array in the dataset. */
-  void write_gbxindex(const viewd_constgbx d_gbxs) const {
-    // dataset.write_to_array(xzarr_ptr, h_data);
+  /* returns a view in the host memor of the gbxindex of every gridbox in d_gbxs */
+  Buffer<size_t>::viewh_buffer collect_gbxindexes(const viewd_constgbx d_gbxs) const {
+    const size_t ngbxs(d_gbxs.extent(0));
+    auto h_data = Buffer<size_t>::viewh_buffer("h_data", ngbxs);
+    auto d_data = Kokkos::create_mirror_view(ExecSpace(), h_data);
+    Kokkos::parallel_for("collect_gbxs_data", Kokkos::RangePolicy<ExecSpace>(0, ngbxs),
+                         GbxIndexFunctor(d_gbxs, d_data));
+    Kokkos::deep_copy(h_data, d_data);
+    return h_data;
   }
 
  public:
@@ -58,9 +75,14 @@ class GbxindexObserver {
 
   ~GbxindexObserver() { dataset.write_arrayshape(xzarr_ptr); }
 
+  /* write the gbxindex of every gridbox in d_gbxs to the gbxindex array in the dataset and assert
+  the size of the gbxindex dimension in the dataset is correct */
   void before_timestepping(const viewd_constgbx d_gbxs) const {
     std::cout << "observer includes gbxindex observer\n";
-    write_gbxindex(d_gbxs);
+    auto h_data = collect_gbxindexes(d_gbxs);
+    dataset.write_to_array(xzarr_ptr, h_data);
+    assert((dataset.get_dimension("gbxindex") == h_data.extent(0)) &&
+           "inconsistent size of gbxindex data and dataset dimension");
   }
 
   void after_timestepping() const {}
