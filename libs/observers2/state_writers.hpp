2356aa1f06996b50cd6ae4828d5593f2c129fec0
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-28 17:54:37 +0100

    state writers complete

---
+++ libs/observers2/state_writers.hpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
+ *
+ * ----- CLEO -----
+ * File: state_writers.hpp
+ * Project: observers2
+ * Created Date: Wednesday 24th January 2024
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 28th March 2024
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * Functions to create GridboxDataWriters which write out state variables from
+ * each Gridbox, e.g. to use in StateObserver.
+ */
+
+#ifndef LIBS_OBSERVERS2_STATE_WRITERS_HPP_
+#define LIBS_OBSERVERS2_STATE_WRITERS_HPP_
+
+#include <Kokkos_Core.hpp>
+#include <concepts>
+#include <memory>
+
+#include "../cleoconstants.hpp"
+#include "../kokkosaliases.hpp"
+#include "./write_gridboxes.hpp"
+#include "gridboxes/gridbox.hpp"
+#include "superdrops/state.hpp"
+#include "zarr2/dataset.hpp"
+#include "zarr2/xarray_zarr_array.hpp"
+#include "zarr2/zarr_array.hpp"
+
+// Operator is functor to perform copy of pressure in each gridbox to d_data in parallel.
+// Note conversion of pressure from double (8 bytes) to single precision (4 bytes float) in output
+struct PressFunc {
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii, viewd_constgbx d_gbxs,
+                  Buffer<float>::mirrorviewd_buffer d_data) const {
+    auto press = static_cast<float>(d_gbxs(ii).state.press);
+    d_data(ii) = press;
+  }
+};
+
+// Operator is functor to perform copy of temperature in each gridbox to d_data in parallel
+// Note conversion of temperature from double (8 bytes) to single precision (4 bytes float) in
+// output
+struct TempFunc {
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii, viewd_constgbx d_gbxs,
+                  Buffer<float>::mirrorviewd_buffer d_data) const {
+    auto temp = static_cast<float>(d_gbxs(ii).state.temp);
+    d_data(ii) = temp;
+  }
+};
+
+// Operator is functor to perform copy of vapour mass mixing ratio (qvap) in each gridbox to d_data
+// in parallel Note conversion of qvap from double (8 bytes) to single precision (4 bytes float) in
+// output
+struct QvapFunc {
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii, viewd_constgbx d_gbxs,
+                  Buffer<float>::mirrorviewd_buffer d_data) const {
+    auto qvap = static_cast<float>(d_gbxs(ii).state.qvap);
+    d_data(ii) = qvap;
+  }
+};
+
+// Operator is functor to perform copy of liquid mass mixing ratio (qcond) in each gridbox to d_data
+// in parallel Note conversion of qcond from double (8 bytes) to single precision (4 bytes
+// float) in output
+struct QcondFunc {
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii, viewd_constgbx d_gbxs,
+                  Buffer<float>::mirrorviewd_buffer d_data) const {
+    auto qcond = static_cast<float>(d_gbxs(ii).state.qcond);
+    d_data(ii) = qcond;
+  }
+};
+
+// returns GridboxDataWriter which writes the pressure, temperature, qvap, and qcond from
+// each gridbox to an array in a dataset in a store
+template <typename Store>
+GridboxDataWriter<Store> auto ThermoStateWriter(Dataset<Store> &dataset, const int maxchunk,
+                                                const size_t ngbxs) {
+  const auto chunkshape = good2Dchunkshape(maxchunk, ngbxs);
+
+  // create shared pointer to 2-D array in dataset for pressure in each gridbox over time
+  std::shared_ptr<XarrayZarrArray<Store, float>> press_ptr =
+      std::make_shared<XarrayZarrArray<Store, float>>(dataset.template create_array<float>(
+          "press", "hPa", "<f4", dlc::P0 / 100, chunkshape, {"time", "gbxindex"}));
+
+  // create shared pointer to 2-D array in dataset for temperature in each gridbox over time
+  std::shared_ptr<XarrayZarrArray<Store, float>> temp_ptr =
+      std::make_shared<XarrayZarrArray<Store, float>>(dataset.template create_array<float>(
+          "temp", "K", "<f4", dlc::TEMP0, chunkshape, {"time", "gbxindex"}));
+
+  // create shared pointer to 2-D array in dataset for temperature in each gridbox over time
+  std::shared_ptr<XarrayZarrArray<Store, float>> qvap_ptr =
+      std::make_shared<XarrayZarrArray<Store, float>>(dataset.template create_array<float>(
+          "qvap", "g/Kg", "<f4", 1000.0, chunkshape, {"time", "gbxindex"}));
+
+  // create shared pointer to 2-D array in dataset for temperature in each gridbox over time
+  std::shared_ptr<XarrayZarrArray<Store, float>> qcond_ptr =
+      std::make_shared<XarrayZarrArray<Store, float>>(dataset.template create_array<float>(
+          "qcond", "g/Kg", "<f4", 1000.0, chunkshape, {"time", "gbxindex"}));
+
+  auto press = GenericGbxWriter<Store, float, PressFunc>(dataset, PressFunc{}, press_ptr, ngbxs);
+  auto temp = GenericGbxWriter<Store, float, TempFunc>(dataset, TempFunc{}, temp_ptr, ngbxs);
+  auto qvap = GenericGbxWriter<Store, float, QvapFunc>(dataset, QvapFunc{}, qvap_ptr, ngbxs);
+  auto qcond = GenericGbxWriter<Store, float, QcondFunc>(dataset, QcondFunc{}, qcond_ptr, ngbxs);
+
+  const auto c = CombineGDW<Store>{};
+  return c(qcond, c(qvap, c(press, temp)));
+}
+
+#endif  // LIBS_OBSERVERS2_STATE_WRITERS_HPP_
