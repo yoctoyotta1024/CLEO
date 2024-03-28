55f0f52e208b32b75974be67779b1e551f0516be
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-28 17:20:43 +0100

    more write funcs into seperate file

--- libs/observers2/state_observer.hpp
+++ libs/observers2/state_observer.hpp
@@ -24,40 +24,11 @@
 #define LIBS_OBSERVERS2_STATE_OBSERVER_HPP_
 
 #include <Kokkos_Core.hpp>
-#include <cassert>
 #include <concepts>
-#include <iostream>
-#include <memory>
 
-#include "../cleoconstants.hpp"
-#include "../kokkosaliases.hpp"
 #include "./observers.hpp"
 #include "./write_gridboxes.hpp"
-#include "gridboxes/gridbox.hpp"
-#include "superdrops/state.hpp"
-
-// returns GridboxDataWriter which writes the pressure in each gridbox to an array in a dataset in a
-// store
-template <typename Store>
-GridboxDataWriter<Store> auto PressureGbxWriter(Dataset<Store> &dataset, const int maxchunk,
-                                                const size_t ngbxs) {
-  // Operator is functor to perform copy of pressure in each gridbox to d_data in parallel
-  struct PressureFunc {
-    KOKKOS_INLINE_FUNCTION
-    void operator()(const size_t ii, viewd_constgbx d_gbxs,
-                    Buffer<double>::mirrorviewd_buffer d_data) const {
-      d_data(ii) = d_gbxs(ii).state.press;
-    }
-  };
-
-  // create shared pointer to 2-D array in dataset for pressure in each gridbox over time
-  std::shared_ptr<XarrayZarrArray<Store, double>> xzarr_ptr =
-      std::make_shared<XarrayZarrArray<Store, double>>(dataset.template create_array<double>(
-          "press", "hPa", "<f8", dlc::P0 / 100, good2Dchunkshape(maxchunk, ngbxs),
-          {"time", "gbxindex"}));
-
-  return GenericGbxWriter<Store, double, PressureFunc>(dataset, PressureFunc{}, xzarr_ptr, ngbxs);
-}
+#include "./write_state.hpp"
 
 /* constructs observer which writes variables from the state of each gridbox
 with a constant timestep 'interval' using an instance of the ConstTstepObserver class */
@@ -66,8 +37,8 @@ inline Observer auto StateObserver(const unsigned int interval, Dataset<Store> &
                                    const int maxchunk, const size_t ngbxs) {
   const auto c = CombineGDW<Store>{};
 
-  auto pressure_writer = get_pressure_gbxwriter(dataset, maxchunk, ngbxs);
-  const GridboxDataWriter<Store> auto writer = c(pressure_writer, NullGbxWriter<Store>{});
+  auto press = PressureWriter(dataset, maxchunk, ngbxs);
+  const GridboxDataWriter<Store> auto writer = c(press, NullGbxWriter<Store>{});
 
   return ConstTstepObserver(interval, WriteGridboxes(dataset, writer));
 }
