438e36e45d1269090b991de793856a0b50b62cab
Clara Levante <yoctoyotta1024@yahoo.com>
2024-03-29 18:15:07 +0100

    calc mass moment functions

--- libs/observers2/massmoments_observer.hpp
+++ libs/observers2/massmoments_observer.hpp
@@ -92,6 +92,48 @@ struct MassMomArrays {
   }
 };
 
+/* calculated 0th, 1st and 2nd moment of the (real)
+droplet mass distribution, i.e. 0th, 3rd and 6th
+moment of the droplet radius distribution for one gridbox.
+Kokkos::parallel_reduce([...]) is equivalent in serial to:
+for (size_t kk(0); kk < supers.extent(0); ++kk){[...]} */
+KOKKOS_INLINE_FUNCTION
+void calculate_massmoments(const TeamMember &team_member, const int ii,
+                           const subviewd_constsupers supers,
+                           Buffer<uint32_t>::mirrorviewd_buffer d_mom0,
+                           Buffer<float>::mirrorviewd_buffer d_mom1,
+                           Buffer<float>::mirrorviewd_buffer d_mom2) {
+  const size_t nsupers(supers.extent(0));
+  Kokkos::parallel_reduce(
+      Kokkos::TeamThreadRange(team_member, nsupers),
+      [supers](const size_t kk, uint32_t &m0, float &m1, float &m2) {
+        const auto xi = static_cast<double>(
+            supers(kk).get_xi());  // cast multiplicity from unsigned int to double
+        const auto mass = supers(kk).mass();
+        m0 += static_cast<uint32_t>(supers(kk).get_xi());
+        m1 += static_cast<float>(xi * mass);
+        m2 += static_cast<float>(xi * mass * mass);
+      },
+      d_mom0(ii), d_mom1(ii), d_mom2(ii));  // {0th, 1st, 2nd} mass moments
+}
+
+/* calculated 0th, 1st and 2nd moment of the (real)
+droplet mass distribution, i.e. 0th, 3rd and 6th
+moment of the droplet radius distribution for one gridbox.
+For all gridboxes in parallel */
+void calculate_massmoments(const viewd_constgbx d_gbxs, Buffer<uint32_t>::mirrorviewd_buffer d_mom0,
+                           Buffer<float>::mirrorviewd_buffer d_mom1,
+                           Buffer<float>::mirrorviewd_buffer d_mom2) {
+  const size_t ngbxs(d_gbxs.extent(0));
+  Kokkos::parallel_for("calculate_massmoments", TeamPolicy(ngbxs, Kokkos::AUTO()),
+                       [d_gbxs, d_mom0, d_mom1, d_mom2](const TeamMember &team_member) {
+                         const int ii = team_member.league_rank();
+
+                         auto supers(d_gbxs(ii).supersingbx.readonly());
+                         calculate_massmoments(team_member, ii, supers, d_mom0, d_mom1, d_mom2);
+                       });
+}
+
 /* template class for observing 0th, 1st and 2nd mass moment (i.e. 0th, 3rd and 6th radius moment)
 of droplet distribution in each gridbox to arrays in a dataset in a store */
 template <typename Store>
@@ -115,7 +157,7 @@ class DoMassMomsObs {
 
   void at_start_step(const unsigned int t_mdl, const viewd_constgbx d_gbxs,
                      const viewd_constsupers totsupers) const {
-    calculate_massmoments(d_gbxs);
+    calculate_massmoments(d_gbxs, xzarrs_ptr->d_mom0, xzarrs_ptr->d_mom1, xzarrs_ptr->d_mom2);
     xzarrs_ptr->write_massmoments_to_arrays(dataset);
   }
 };
