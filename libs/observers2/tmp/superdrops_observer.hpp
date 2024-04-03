b9d3900f25515b7b42e65a49d86c4e6ea7c40945
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-04 00:47:56 +0200

    new file WIP

---
+++ libs/observers2/tmp/superdrops_observer.hpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
+ *
+ * ----- CLEO -----
+ * File: superdrops_observer.hpp
+ * Project: tmp
+ * Created Date: Wednesday 24th January 2024
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 4th April 2024
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * Observer to write variables related to superdroplet attributes at the start of
+ * a constant interval timestep to ragged arrays in a dataset
+ */
+
+#ifndef LIBS_OBSERVERS2_TMP_SUPERDROPS_OBSERVER_HPP_
+#define LIBS_OBSERVERS2_TMP_SUPERDROPS_OBSERVER_HPP_
+
+// TODO(CB) WIP
+
+#endif  // LIBS_OBSERVERS2_TMP_SUPERDROPS_OBSERVER_HPP_
