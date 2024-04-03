fc35af993bae0a2b0a01aac45597bae3e293df17
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-03 16:41:06 +0200

    do write supers outline

--- libs/observers2/do_write_supers.hpp
+++ libs/observers2/do_write_supers.hpp
@@ -16,8 +16,8 @@
  * https://opensource.org/licenses/BSD-3-Clause
  * -----
  * File Description:
- * Template for an struct which writes data collected from super-droplets in parallel
- * to individual ragged arrays in a dataset
+ * Template for an struct which writes data collected from superdroplets in parallel
+ * to individual arrays in a dataset
  */
 
 #ifndef LIBS_OBSERVERS2_DO_WRITE_SUPERS_HPP_
@@ -29,56 +29,66 @@
 #include <memory>
 
 #include "../kokkosaliases.hpp"
-#include "./nsupers_observer.hpp"
 #include "./write_gridbox_to_array.hpp"
 #include "gridboxes/gridbox.hpp"
+#include "superdrops/superdrop.hpp"
 #include "zarr2/dataset.hpp"
 #include "zarr2/xarray_zarr_array.hpp"
 
-struct ParallelSupersRangePolicy {
-  template <typename Functor>
-  void operator()(Functor functor, const viewd_constsupers totsupers) const {
-    const size_t totnsupers(totsupers.extent(0));
-    Kokkos::parallel_for("range_policy_collect_totsupers_data",
-                         Kokkos::RangePolicy<ExecSpace>(0, ntotsupers), functor);
-  }
-};
-
-/* template class for observer with at_start_step function that collects variables from all the
-super-droplets in each gridbox in parallel and then writes them to their respective ragged arrays in
-a dataset */
-template <typename Store, WriteGridboxToArray<Store> WriteSupersToArray>
+/* template class for observer with at_start_step function that collects variables from each
+superdroplet in each gridbox in parallel and then writes them to their repspective ragged arrays in
+a dataset alongside the raggedcount for the arrays */
+template <typename Store, WriteGridboxToArray<Store> WriteSupersToArray,
+          WriteGridboxToArray<Store> WriteRaggedCountToArray>
 class DoWriteSupers {
  private:
-  ParallelSupersRangePolicy parallel_loop;  ///< function like object to call during at_start_step
-                                            ///< to loop over gridboxes
-  const Dataset<Store> &dataset;            ///< dataset to write data to
-  WriteSupersToArray write2array;  ///< object collects superdrops data and writes it to arrays
-                                   ///< in dataset, including array for raggedcount
+  const Dataset<Store> &dataset;  ///< dataset to write data to
+  WriteSupersToArray
+      write2array;  ///< object collects superdrops data and writes it to arrays in dataset
+  WriteRaggedCountToArray write2raggedcount;  ///< raggedcount array in dataset
 
   /* Use the writer's functor to collect data from superdroplets in parallel.
-  Then write the data to arrays in the dataset */
-  void at_start_step(const viewd_constsupers totsupers) const {
+  Then write the data to ragged arrays in the dataset */
+  void write_superdrops_data(const viewd_constsupers totsupers) const {
     auto functor = write2array.get_functor(totsupers);
-    parallel_loop(functor, totsupers);
+    const size_t totnsupers(totsupers.extent(0));
+    Kokkos::parallel_for("range_policy_collect_totsupers_data",
+                         Kokkos::RangePolicy<ExecSpace>(0, totnsupers), functor);
     write2array.write_to_array(dataset);
+  }
+
+  /* Use the raggedcount writer's functor to collect the raggedcount (i.e. the number of
+  superdroplets in each gridbox) and write it to an array in the dataset */
+  void write_raggedcount(const viewd_constgbx d_gbxs) const {
+    auto functor = write2raggedcount.get_functor(d_gbxs);
+    const size_t ngbxs(d_gbxs.extent(0));
+    Kokkos::parallel_for("range_policy_collect_gbxs_data", Kokkos::RangePolicy<ExecSpace>(0, ngbxs),
+                         functor);
+    write2raggedcount.write_to_array(dataset);
+  }
+
+  /* Collect data from superdroplets and write into ragged arrays in the dataset alongside the
+   * raggedcount */
+  void at_start_step(const viewd_constgbx d_gbxs, const viewd_constsupers totsupers) const {
+    write_superdrops_data(totsupers);
+    write_raggedcount(d_gbxs);
   }
 
  public:
   DoWriteSupers(const Dataset<Store> &dataset, WriteSupersToArray write2array)
-      : parallel_loop(ParallelSupersRangePolicy{}), dataset(dataset), write2array(write2array) {}
+      : dataset(dataset), write2array(write2array) {}
 
   ~DoWriteSupers() { write2array.write_arrayshape(dataset); }
 
   void before_timestepping(const viewd_constgbx d_gbxs) const {
-    std::cout << "observer includes write supers observer\n";
+    std::cout << "observer includes write superdrops observer\n";
   }
 
   void after_timestepping() const {}
 
   void at_start_step(const unsigned int t_mdl, const viewd_constgbx d_gbxs,
                      const viewd_constsupers totsupers) const {
-    at_start_step(totsupers);
+    at_start_step(d_gbxs, totsupers);
   }
 };
 
