f127d55669f8889477adad07e4c1ff2e7230b4af
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-03 17:47:14 +0200

    delet unwanted struct

--- libs/observers2/xarray_for_supers_data.hpp
+++ libs/observers2/xarray_for_supers_data.hpp
@@ -62,34 +62,4 @@ struct XarrayForSupersData {
   void write_arrayshape(const Dataset<Store> &dataset) { dataset.write_arrayshape(xzarr); }
 };
 
-/* struct holding an array in a dataset as well a view and its mirror view
-which can be useful when collecting data for 1 variable from 'ngbxs' gridboxes
-(in parallel) to then write the the array */
-template <typename Store, typename T>
-struct XarrayForSupersRaggedCount {
-  XarrayZarrArray<Store, T> xzarr;                         // array in a dataset
-  using viewh_data = Buffer<T>::viewh_buffer;              // type of view for h_data
-  using mirrorviewd_data = Buffer<T>::mirrorviewd_buffer;  // mirror view type for d_data
-  viewh_data h_data;        // view on host for value of 1 variable from every gridbox
-  mirrorviewd_data d_data;  // mirror view of h_data on device
-
-  /* Constructor to initialize views and pointer to raggedcount array in dataset */
-  XarrayForSupersRaggedCount(const Dataset<Store> &dataset, const std::string_view name,
-                             const std::string_view dtype, const size_t maxchunk,
-                             const size_t ngbxs)
-      : xzarr(dataset.template create_array<T>(name, dtype, good2Dchunkshape(maxchunk, ngbxs),
-                                               {"time", "gbxindex"}, "superdroplets")),
-        h_data("h_data", ngbxs),
-        d_data(Kokkos::create_mirror_view(ExecSpace(), h_data)) {}
-
-  // copy data from device view directly to host and then write to array in dataset
-  void write_to_array(const Dataset<Store> &dataset) {
-    Kokkos::deep_copy(h_data, d_data);
-    dataset.write_to_array(xzarr, h_data);
-  }
-
-  // call function to write shape of array according to dataset
-  void write_arrayshape(const Dataset<Store> &dataset) { dataset.write_arrayshape(xzarr); }
-};
-
 #endif  // LIBS_OBSERVERS2_XARRAY_FOR_SUPERS_DATA_HPP_
