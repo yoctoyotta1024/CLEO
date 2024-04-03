276d93d26b3dd17530bde2636b3a58113d8b8968
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-03 16:24:37 +0200

    use supers range policy to collect supers data

--- libs/observers2/do_write_supers.hpp
+++ libs/observers2/do_write_supers.hpp
@@ -35,36 +35,38 @@
 #include "zarr2/dataset.hpp"
 #include "zarr2/xarray_zarr_array.hpp"
 
+struct ParallelSupersRangePolicy {
+  template <typename Functor>
+  void operator()(Functor functor, const viewd_constsupers totsupers) const {
+    const size_t totnsupers(totsupers.extent(0));
+    Kokkos::parallel_for("range_policy_collect_totsupers_data",
+                         Kokkos::RangePolicy<ExecSpace>(0, ntotsupers), functor);
+  }
+};
+
 /* template class for observer with at_start_step function that collects variables from all the
 super-droplets in each gridbox in parallel and then writes them to their respective ragged arrays in
 a dataset */
-template <typename ParallelLoopPolicy, typename Store,
-          WriteGridboxToArray<Store> WriteSupersToArray>
+template <typename Store, WriteGridboxToArray<Store> WriteSupersToArray>
 class DoWriteSupers {
  private:
-  const Dataset<Store> &dataset;     ///< dataset to write data to
-  WriteSupersToArray write2array;    ///< object collects superdrops data and writes it to arrays
-                                     ///< in dataset, including array for raggedcount
-  ParallelLoopPolicy parallel_loop;  ///< function like object to call during at_start_step to
-                                     ///< loop over gridboxes
+  ParallelSupersRangePolicy parallel_loop;  ///< function like object to call during at_start_step
+                                            ///< to loop over gridboxes
+  const Dataset<Store> &dataset;            ///< dataset to write data to
+  WriteSupersToArray write2array;  ///< object collects superdrops data and writes it to arrays
+                                   ///< in dataset, including array for raggedcount
 
   /* Use the writer's functor to collect data from superdroplets in parallel.
   Then write the data to arrays in the dataset */
   void at_start_step(const viewd_constsupers totsupers) const {
     auto functor = write2array.get_functor(totsupers);
-
-    const size_t ngbxs(d_gbxs.extent(0));
-    Kokkos::parallel_for("team_policy_collect_supers_data", TeamPolicy(ngbxs, Kokkos::AUTO()),
-                         // TODO(CB) assign d_data views here?
-                         functor);
-
+    parallel_loop(functor, totsupers);
     write2array.write_to_array(dataset);
   }
 
  public:
-  DoWriteSupers(ParallelLoopPolicy parallel_loop, const Dataset<Store> &dataset,
-                WriteSupersToArray write2array)
-      : parallel_loop(parallel_loop), dataset(dataset), write2array(write2array) {}
+  DoWriteSupers(const Dataset<Store> &dataset, WriteSupersToArray write2array)
+      : parallel_loop(ParallelSupersRangePolicy{}), dataset(dataset), write2array(write2array) {}
 
   ~DoWriteSupers() { write2array.write_arrayshape(dataset); }
 
