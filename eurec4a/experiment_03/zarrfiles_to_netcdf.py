329c897afedcf15a6b298cdc52168017a3dcc6ac
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-04-03 13:38:08 +0200

    only create regular gridded netcdf file if it does not exist

--- eurec4a/experiment_03/zarrfiles_to_netcdf.py
+++ eurec4a/experiment_03/zarrfiles_to_netcdf.py
@@ -7,6 +7,7 @@
 
 # %%
 import sys
+import os
 import numpy as np
 from pathlib import Path
 import yaml
@@ -49,6 +50,7 @@ cloud_id = config_yaml['cloud']["cloud_id"]
 # get cloud imformation
 cloud_id = config_yaml['cloud']['cloud_id']
 identification_type = config_yaml['cloud']['identification_type']
+
 savefigpath = path2CLEO / "results/experiment_03" / f"{identification_type}_{cloud_id}" # directory for saving figures
 savefigpath.mkdir(exist_ok=True, parents=True)
 ### ----------------------- INPUT PARAMETERS ----------------------- ###
@@ -61,91 +63,98 @@ dataset       = rawdirectory / f"{identification_type}_{cloud_id}/rain1d_sol.zar
 
 
 
-# %% [markdown]
-# Get the config, constants and also an initial superdroplet dataset
-
 # %%
-# read in constants and intial setup from setup .txt file
-config = pysetuptxt.get_config(setupfile, nattrs=3, isprint=False)
-consts = pysetuptxt.get_consts(setupfile, isprint=False)
+def main():
+    # %% [markdown]
+    # Get the config, constants and also an initial superdroplet dataset
 
-# Create a first simple dataset to have the coordinates for later netcdf creation
-sddata = pyzarr.get_supers(str(dataset), consts)
-simple_ds = xr.open_dataset(dataset, engine="zarr",
-                                consolidated=False);
+    # %%
+    # read in constants and intial setup from setup .txt file
+    config = pysetuptxt.get_config(setupfile, nattrs=3, isprint=False)
+    consts = pysetuptxt.get_consts(setupfile, isprint=False)
 
-# %% [markdown]
-# ## Create a xarray dataset from the zarr file
-# 
-# 
-# For this, the ``sdtracing.attribute_for_superdroplets_sample`` function will be used for a subset of superdroplet ids.
-# Using a size of 100 makes sense, and is not too slow.
-# 
-# The subdatasets will be stored in a temporary folder.
+    # Create a first simple dataset to have the coordinates for later netcdf creation
+    sddata = pyzarr.get_supers(str(dataset), consts)
+    simple_ds = xr.open_dataset(dataset, engine="zarr",
+                                    consolidated=False);
 
-# %% [markdown]
-# To do so, we create a list of temporaray filenames
+    # %% [markdown]
+    # ## Create a xarray dataset from the zarr file
+    # 
+    # 
+    # For this, the ``sdtracing.attribute_for_superdroplets_sample`` function will be used for a subset of superdroplet ids.
+    # Using a size of 100 makes sense, and is not too slow.
+    # 
+    # The subdatasets will be stored in a temporary folder.
+
+    # %% [markdown]
+    # To do so, we create a list of temporaray filenames
+
+    all_ids = np.arange(config['totnsupers'], dtype = int)
+    len(all_ids)
+    # all_ids_reshaped = all_ids.reshape(100)
+    # use each gridcell on its own
+    n = config['totnsupers'] / 256
+    all_ids = all_ids.reshape(int(n), -1)
+    # temporary file names based on seed
+
+    seed = 2
+    np.random.seed(seed)
+    hashs = np.random.choice(int(1e6), size = all_ids.shape[0])
+    str_func = np.vectorize(lambda x : TEMPORARY_DIR / f"{hex(x)}.nc")
+    temp_filepaths = str_func(hashs)
+
+    assert len(temp_filepaths) == all_ids.shape[0]
+
+
+    print(f"Temporary files will be stored in {TEMPORARY_DIR}")
+
+    # %%
+    attributes = ["xi", "radius", "coord3", "sdgbxindex", "msol"]
+
+    print(f"Processing {all_ids.shape[0]} files")
+
+    for ids, temp_filepath in zip(all_ids, temp_filepaths):
+        print(f"Processing {ids.min()}-{ids.max()} to \t{temp_filepath}")
+        ids = list(ids)
+        list_dataarrays = []
+        for attr in attributes:
+            data = sdtracing.attribute_for_superdroplets_sample(
+                sddata,
+                attr,
+                ids=ids,
+            )
+            da = xr.DataArray(
+                data=data,
+                dims=["time", "sd_id"],
+                coords={"time": simple_ds.time, "sd_id": ids}
+            )
+            da.name = attr
+            list_dataarrays.append(da)
+
+        ds = xr.merge(list_dataarrays)
+        ds.to_netcdf(temp_filepath)
+
+    # %% [markdown]
+    # ## Combine datasets and store in folder
+
+    # %%
+    print(f"Combine datasets and store in folder {OUTPUT_DIR}")
+
+    full_dataset = xr.open_mfdataset(temp_filepaths, parallel = True)
+    full_dataset.to_netcdf(OUTPUT_FILEPATH)
+
 
 # %%
 path2dataoutput = path2CLEO / "data/output"
 TEMPORARY_DIR = path2dataoutput / "temporary/" f"{identification_type}_{cloud_id}"
 TEMPORARY_DIR.mkdir(exist_ok=True, parents=True)
-OUTPUT_DIR = path2dataoutput / "processed/testfolder/" f"{identification_type}_{cloud_id}"
+OUTPUT_DIR = processeddirectory / f"{identification_type}_{cloud_id}"
 OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
+OUTPUT_FILEPATH = OUTPUT_DIR / "full_dataset.nc"
 
-# %%
-all_ids = np.arange(config['totnsupers'], dtype = int)
-len(all_ids)
-# all_ids_reshaped = all_ids.reshape(100)
-# use each gridcell on its own
-n = config['totnsupers'] / 256
-all_ids = all_ids.reshape(int(n), -1)
-# temporary file names based on seed
-
-seed = 2
-np.random.seed(seed)
-hashs = np.random.choice(int(1e6), size = all_ids.shape[0])
-str_func = np.vectorize(lambda x : TEMPORARY_DIR / f"{hex(x)}.nc")
-temp_filepaths = str_func(hashs)
-
-assert len(temp_filepaths) == all_ids.shape[0]
-
-
-print(f"Temporary files will be stored in {TEMPORARY_DIR}")
-
-# %%
-attributes = ["xi", "radius", "coord3", "sdgbxindex", "msol"]
-
-print(f"Processing {all_ids.shape[0]} files")
-
-for ids, temp_filepath in zip(all_ids, temp_filepaths):
-    print(f"Processing {ids.min()}-{ids.max()} to \t{temp_filepath}")
-    ids = list(ids)
-    list_dataarrays = []
-    for attr in attributes:
-        data = sdtracing.attribute_for_superdroplets_sample(
-            sddata,
-            attr,
-            ids=ids,
-        )
-        da = xr.DataArray(
-            data=data,
-            dims=["time", "sd_id"],
-            coords={"time": simple_ds.time, "sd_id": ids}
-        )
-        da.name = attr
-        list_dataarrays.append(da)
-
-    ds = xr.merge(list_dataarrays)
-    ds.to_netcdf(temp_filepath)
-
-# %% [markdown]
-# ## Combine datasets and store in folder
-
-# %%
-print(f"Combine datasets and store in folder {OUTPUT_DIR}")
-
-full_dataset = xr.open_mfdataset(temp_filepaths, parallel = True)
-full_dataset.to_netcdf(OUTPUT_DIR / "full_dataset.nc")
-
-
+if os.path.exists(OUTPUT_FILEPATH):
+    print(f"The file {OUTPUT_FILEPATH} exists. Skip processing.")
+else:
+    print(f"The file {OUTPUT_FILEPATH} does not exist. Create it.")
+    main()
