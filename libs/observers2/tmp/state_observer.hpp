8d40fb0852c6893b4950fbcdd1ce696853545b53
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-04 00:42:08 +0200

    new state observer file

---
+++ libs/observers2/tmp/state_observer.hpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
+ *
+ * ----- CLEO -----
+ * File: state_observer.hpp
+ * Project: tmp
+ * Created Date: Wednesday 24th January 2024
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 4th April 2024
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * Observer to write variables related to Gridboxes' state at the start of
+ * a constant interval timestep to arrays in a dataset
+ */
+
+#ifndef LIBS_OBSERVERS2_TMP_STATE_OBSERVER_HPP_
+#define LIBS_OBSERVERS2_TMP_STATE_OBSERVER_HPP_
+
+#endif  // LIBS_OBSERVERS2_TMP_STATE_OBSERVER_HPP_
