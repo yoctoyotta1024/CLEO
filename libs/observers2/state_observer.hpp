8ba6984cac75973c1c3bce6a5fcbb906144940ba
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-04 22:27:00 +0200

    thermo state observer complete

--- libs/observers2/state_observer.hpp
+++ libs/observers2/state_observer.hpp
@@ -49,6 +49,42 @@ struct PressFunc {
   }
 };
 
+// Operator is functor to perform copy of temperature in each gridbox to d_data in parallel.
+// Note conversion of temperature from double (8 bytes) to single precision (4 bytes float) in
+// output
+struct TempFunc {
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii, viewd_constgbx d_gbxs, const viewd_constsupers totsupers,
+                  Buffer<float>::mirrorviewd_buffer d_data) const {
+    auto temp = static_cast<float>(d_gbxs(ii).state.temp);
+    d_data(ii) = temp;
+  }
+};
+
+// Operator is functor to perform copy of vapour mass mixing ratio (qvap) in each gridbox to d_data
+// in parallel. Note conversion of qvap from double (8 bytes) to single precision (4 bytes float) in
+// output
+struct QvapFunc {
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii, viewd_constgbx d_gbxs, const viewd_constsupers totsupers,
+                  Buffer<float>::mirrorviewd_buffer d_data) const {
+    auto qvap = static_cast<float>(d_gbxs(ii).state.qvap);
+    d_data(ii) = qvap;
+  }
+};
+
+// Operator is functor to perform copy of liquid mass mixing ratio (qcond) in each gridbox to d_data
+// in parallel. Note conversion of qcond from double (8 bytes) to single precision (4 bytes
+// float) in output
+struct QcondFunc {
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const size_t ii, viewd_constgbx d_gbxs, const viewd_constsupers totsupers,
+                  Buffer<float>::mirrorviewd_buffer d_data) const {
+    auto qcond = static_cast<float>(d_gbxs(ii).state.qcond);
+    d_data(ii) = qcond;
+  }
+};
+
 /* returns CollectDataForDataset which writes a state variable from
 each gridbox to an array in a dataset in a given store for a given datatype and using a given
 function-like functor */
@@ -64,15 +100,32 @@ CollectDataForDataset<Store> auto CollectStateVariable(
   return GenericCollectData(ffunc, xzarr, ngbxs);
 }
 
+/* constructs CollectDataForDataset for a given Store which writes writes thermodynamic variables
+from the state of each gridbox using an instance of the GenericCollectData class */
+template <typename Store>
+inline CollectDataForDataset<Store> auto CollectThermo(const Dataset<Store> &dataset,
+                                                       const int maxchunk, const size_t ngbxs) {
+  const CollectDataForDataset<Store> auto press = CollectStateVariable<Store, float, PressFunc>(
+      dataset, PressFunc{}, "press", "hPa", "<f4", dlc::P0 / 100, maxchunk, ngbxs);
+
+  const CollectDataForDataset<Store> auto temp = CollectStateVariable<Store, float, TempFunc>(
+      dataset, TempFunc{}, "temp", "K", "<f4", dlc::TEMP0, maxchunk, ngbxs);
+
+  const CollectDataForDataset<Store> auto qvap = CollectStateVariable<Store, float, QvapFunc>(
+      dataset, QvapFunc{}, "qvap", "g/Kg", "<f4", 1000.0, maxchunk, ngbxs);
+
+  const CollectDataForDataset<Store> auto qcond = CollectStateVariable<Store, float, QcondFunc>(
+      dataset, QcondFunc{}, "qcond", "g/Kg", "<f4", 1000.0, maxchunk, ngbxs);
+
+  return press >> temp >> qvap >> qcond;
+}
+
 /* constructs observer which writes writes thermodynamic variables from the state of each gridbox
 with a constant timestep 'interval' using an instance of the WriteToDatasetObserver class */
 template <typename Store>
 inline Observer auto ThermoObserver(const unsigned int interval, const Dataset<Store> &dataset,
                                     const int maxchunk, const size_t ngbxs) {
-  const CollectDataForDataset<Store> auto press = CollectStateVariable<Store, float, PressFunc>(
-      dataset, PressFunc{}, "press", "hPa", "<f4", dlc::P0 / 100, maxchunk, ngbxs);
-
-  const auto collect_thermodata = press;  // TODO(CB) WIP
+  const auto collect_thermodata = CollectThermo(dataset, maxchunk, ngbxs);
   return WriteToDatasetObserver(interval, dataset, collect_thermodata);
 }
 
