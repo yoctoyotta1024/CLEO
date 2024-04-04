8b0322e6758214ef7d9eb63bdfd74ae03761b965
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-05 01:10:08 +0200

    comments

--- libs/observers2/massmoments_observer.hpp
+++ libs/observers2/massmoments_observer.hpp
@@ -40,12 +40,15 @@
 droplet mass distribution in each gridbox, i.e. 0th, 3rd and 6th moments of the droplet
 radius distribution for each gridbox. Calculation is done for all gridboxes in parallel.
 Kokkos::parallel_reduce([...]) is equivalent in serial to:
-for (size_t kk(0); kk < supers.extent(0); ++kk){[...]} */
+for (size_t kk(0); kk < supers.extent(0); ++kk){[...]}.
+Note conversion from 8 to 4byte precision for all mass moments: mom0 from size_t (architecture
+dependent usually long unsigned int = 8 bytes) to single precision (uint32_t = 4 bytes), and mom1
+and mom2 from double (8 bytes) to float (4 bytes) */
 struct MassMomentsFunc {
   KOKKOS_FUNCTION
   operator()(const TeamMember & team_member, const viewd_constgbx d_gbxs,
              Buffer<uint32_t>::mirrorviewd_buffer d_mom0, Buffer<float>::mirrorviewd_buffer d_mom1,
-             Buffer<float>::mirrorviewd_buffer d_mom2);  // TODO(CB)
+             Buffer<float>::mirrorviewd_buffer d_mom2);  // TODO(CB) implementation
 };
 
 /* Operator is functor to perform calculation of 0th, 1st and 2nd moments of the (real)
@@ -53,12 +56,15 @@ raindroplet mass distribution in each gridbox, i.e. 0th, 3rd and 6th moments of 
 radius distribution for each gridbox. A raindrop is droplet with a radius >= rlim = 40microns.
 Calculation is done for all gridboxes in parallel.
 Kokkos::parallel_reduce([...]) is equivalent in serial to:
-for (size_t kk(0); kk < supers.extent(0); ++kk){[...]} */
+for (size_t kk(0); kk < supers.extent(0); ++kk){[...]}.
+Note conversion from 8 to 4byte precision for all mass moments: mom0 from size_t (architecture
+dependent usually long unsigned int = 8 bytes) to single precision (uint32_t = 4 bytes), and mom1
+and mom2 from double (8 bytes) to float (4 bytes) */
 struct RaindropsMassMomentsFunc {
   KOKKOS_FUNCTION
   operator()(const TeamMember & team_member, const viewd_constgbx d_gbxs,
              Buffer<uint32_t>::mirrorviewd_buffer d_mom0, Buffer<float>::mirrorviewd_buffer d_mom1,
-             Buffer<float>::mirrorviewd_buffer d_mom2);  // TODO(CB)
+             Buffer<float>::mirrorviewd_buffer d_mom2);  // TODO(CB) implementation
 };
 
 /* struct satifying CollectDataForDataset for collecting the 0th, 1st and 2nd moments of the
