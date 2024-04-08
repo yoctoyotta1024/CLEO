6a24ea9073c694b0e165e9c49e5655d62a60e23b
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-09 00:10:30 +0200

    fix axes labels, all validations working with new zarr and observer libraries

--- examples/yac/fromfile/yac1_fromfile.py
+++ examples/yac/fromfile/yac1_fromfile.py
@@ -6,7 +6,7 @@ Created Date: Friday 17th November 2023
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Monday 8th April 2024
+Last Modified: Tuesday 9th April 2024
 Modified By: CB
 -----
 License: BSD 3-Clause "New" or "Revised" License
