8aa576ffa9029cec4cc174e68cb037220aa05fb8
Clara Levante <yoctoyotta1024@yahoo.com>
2024-04-08 15:13:04 +0200

    comments

--- examples/speedtest/speedtest.sh
+++ examples/speedtest/speedtest.sh
@@ -97,7 +97,7 @@ echo "KOKKOS_DEVICE_PARALLELISM: ${kokkosdevice}"
 echo "KOKKOS_HOST_PARALLELISM: ${kokkoshost}"
 echo "BUILD_DIR: ${path2build_gpus_cpus}"
 
-# build gpus_cpus executable
+# build CLEO
 cmake -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_CC_COMPILER=${CC} \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
@@ -128,7 +128,7 @@ echo "KOKKOS_DEVICE_PARALLELISM: ${kokkosdevice}"
 echo "KOKKOS_HOST_PARALLELISM: ${kokkoshost}"
 echo "BUILD_DIR: ${path2build_cpus}"
 
-# build cpu OpenMP executable
+# build CLEO
 cmake -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_CC_COMPILER=${CC} \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
@@ -159,7 +159,7 @@ echo "KOKKOS_DEVICE_PARALLELISM: ${kokkosdevice}"
 echo "KOKKOS_HOST_PARALLELISM: ${kokkoshost}"
 echo "BUILD_DIR: ${path2build_serial}"
 
-# build gpus_cpus executable
+# build CLEO
 cmake -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_CC_COMPILER=${CC} \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
