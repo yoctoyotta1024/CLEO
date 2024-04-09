ca57f07cd9e68de2f091cff9ad404503feafd004
clara.bayley <yoctoyotta1024@yahoo.com>
2024-04-09 17:40:44 +0200

    feat(eurec4a): main prog for new executable complete

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Monday 8th April 2024
+ * Last Modified: Tuesday 9th April 2024
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
