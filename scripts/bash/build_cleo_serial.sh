ddb5a04d454bebbcbf8c00f20825749cdb206689
clara.bayley <yoctoyotta1024@yahoo.com>
2024-04-11 00:30:52 +0200

    refactor(bash): consistent naming

--- scripts/bash/build_cleo_serial.sh
+++ scripts/bash/build_cleo_serial.sh
@@ -11,19 +11,20 @@
 #SBATCH --output=./build/bin/serialbuild_out.%j.out
 #SBATCH --error=./build/bin/serialbuild_err.%j.out
 
-### ---------------------------------------------------- ###
-### ------- You MUST edit these lines to set your ------ ###
-### --- default compiler(s) (and python environment) --- ###
-### ----  and paths for CLEO and build directories  ---- ###
-### ---------------------------------------------------- ###
+### ------------------------------------------------------------------------ ###
+### ------- You MUST edit these lines to set your default compiler(s) ------ ###
+### --------- and optionally your environment, path to CLEO and the -------- ###
+### ----------------------- desired build directory  ----------------------- ###
+### ------------------------------------------------------------------------ ###
 module load gcc/11.2.0-gcc-11.2.0
 spack load cmake@3.23.1%gcc
 source activate /work/mh1126/m300950/cleoenv
 gxx="/sw/spack-levante/gcc-11.2.0-bcn7mb/bin/g++"
 gcc="/sw/spack-levante/gcc-11.2.0-bcn7mb/bin/gcc"
+
 path2CLEO=$1    # get from command line argument
 path2build=$2   # get from command line argument
-### ---------------------------------------------------- ###
+### ------------------------------------------------------------------------ ###
 
 ### ---------------------------------------------------- ###
 ### ------- You can optionally edit the following ------ ###
@@ -50,7 +51,7 @@ kokkoshost=""
 kokkosdevice=""
 ### ---------------------------------------------------- ###
 
-### ------------ build and compile with cmake ---------- ###
+### ---------------- build CLEO with cmake ------------- ###
 echo "CXX_COMPILER=${CXX} CC_COMPILER=${CC}"
 echo "CLEO_DIR: ${path2CLEO}"
 echo "BUILD_DIR: ${path2build}"
@@ -59,7 +60,6 @@ echo "KOKKOS_DEVICE_PARALLELISM: ${kokkosdevice}"
 echo "KOKKOS_HOST_PARALLELISM: ${kokkoshost}"
 echo "CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}"
 
-# build then compile in parallel
 cmake -DCMAKE_CXX_COMPILER=${CXX} \
     -DCMAKE_CC_COMPILER=${CC} \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
