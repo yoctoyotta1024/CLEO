3c56d802aeeb7e0cffd409b2d04943856ff44946
clara.bayley <yoctoyotta1024@yahoo.com>
2024-04-17 23:46:56 +0200

    feat(pySD): function for text file replaced by yaml version for reading of config file

--- pySD/readconfigfile.py
+++ pySD/readconfigfile.py
@@ -18,35 +18,44 @@ https://opensource.org/licenses/BSD-3-Clause
 File Description:
 '''
 
+from ruamel.yaml import YAML
+
+def extract_floats(node, floats, notfloats):
+    '''Function to recursively searches YAML node
+    and add any value that is convertible to a float to a dictionary
+    with its key '''
+
+    if isinstance(node, dict):
+        for key, value in node.items():
+            if isinstance(value, (dict, list)):
+                extract_floats(value, floats, notfloats)
+            else:
+                try:
+                    floats[key] = float(value)
+                except:
+                    notfloats[key] = value
+    elif isinstance(node, list):
+        for item in node:
+            extract_floats(item, floats, notfloats)
+
+    return floats, notfloats
+
 def read_configparams_into_floats(filename):
-  """returns dictionary of value: float from
-  values assigned in a config .txt file.
-  Also returns dictionary of notfloats
-  for values that couldn't be converted. """
+    '''returns dictionary of {key, values} pairs from
+    keys from a config yaml file which can be assigned float values.
+    Also obtains dictionary of notfloats for values that couldn't be converted
+    and converts nspacedims to integer if found in floats.'''
 
-  floats = {}
-  notfloats = {}
-  with open(filename) as file:
-    rlines=[]
-    filelines = file.readlines()
-    for line in filelines:
-      if line[0] != "#" and line[0] != "/" and "=" in line:
-        goodline = remove_excess_line(line)
-        rlines.append(goodline)
+    yaml = YAML(typ='safe')
+    with open(filename, 'r') as file:
+        config = yaml.load(file)
 
-    for line in rlines:
-      ind = line.find("=")
-      name =  line[:ind]
-      value =  line[ind+1:]
+    floats, notfloats = {}, {}
+    floats, notfloats = extract_floats(config, floats, notfloats)
 
-      try:
-        floats[name] = float(value)
-      except ValueError:
-        notfloats[name] = value
+    try:
+        floats["nspacedims"] = int(floats["nspacedims"])              # no spatial coords to SDs
+    except:
+        pass
 
-  try:
-    floats["nspacedims"] = int(floats["nspacedims"])              # no spatial coords to SDs
-  except:
-    pass
-
-  return floats
+    return floats
