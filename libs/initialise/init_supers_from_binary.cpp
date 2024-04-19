0e0d8e99ae5ca9313d217933528dab8468048cff
clara.bayley <yoctoyotta1024@yahoo.com>
2024-04-19 13:48:36 +0200

    feat: add nan values to initdata

--- libs/initialise/init_supers_from_binary.cpp
+++ libs/initialise/init_supers_from_binary.cpp
@@ -23,7 +23,25 @@
 
 #include "initialise/init_supers_from_binary.hpp"
 
+template <typename T>
+inline std::vector<T> nan_vector(const size_t size) {
+  const auto nanValue = std::numeric_limits<T>::signaling_NaN();
+  return std::vector<T>(size, nanValue);
+}
+
 InitSupersData InitSupersFromBinary::fetch_invalid_superdrops_data(InitSupersData &initdata) const {
-  std::cout << "size: " << maxnsupers << ", " << initdata.radii.size() << "\n";
-  return initdata;
+  const auto size = maxnsupers - initdata.sdgbxindexes.size();
+
+  const auto sdgbxindexes = nan_vector<unsigned int>(size);
+  const auto coord3s = nan_vector<double>(size);
+  const auto coord1s = nan_vector<double>(size);
+  const auto coord2s = nan_vector<double>(size);
+  const auto radii = nan_vector<double>(size);
+  const auto msols = nan_vector<double>(size);
+  const auto xis = nan_vector<uint64_t>(size);
+
+  const auto nandata =
+      InitSupersData{initdata.solutes, sdgbxindexes, coord3s, coord1s, coord2s, radii, msols, xis};
+
+  return initdata + nandata;
 }
