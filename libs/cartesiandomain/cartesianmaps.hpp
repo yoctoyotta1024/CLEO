ab1ffaa04355cfd5e3ff44140d337d780bf5334a
clara.bayley <yoctoyotta1024@yahoo.com>
2024-05-01 00:34:56 +0200

    fix: Revert "chore: rename variable"
    This reverts commit cf185510037ffafb7516a18c4f6a4eb765700362.

--- libs/cartesiandomain/cartesianmaps.hpp
+++ libs/cartesiandomain/cartesianmaps.hpp
@@ -5,11 +5,30 @@
  * ----- CLEO -----
  * File: cartesianmaps.hpp
  * Project: cartesiandomain
- * Created Date: Friday 13th October 2023
+ * Created Date: Wednesday 1st May 2024
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
  * Last Modified: Wednesday 1st May 2024
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ */
+
+
+/* Copyright (c) 2023 MPI-M, Clara Bayley
+ *
+ * ----- CLEO -----
+ * File: cartesianmaps.hpp
+ * Project: cartesiandomain
+ * Created Date: Friday 13th October 2023
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 9th November 2023
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -41,16 +60,16 @@ map between gbxindexes and gridbox boundaries in a cartiesian domain.
 The keys of each map are the gridbox indexes. The corresponding value
 of each bounds map is that gridbox's {lower boundary, upper boundary}.
 to_[direction]_coord[X]nghbr (for direction = back, forward)
-map from a given gbxindex to the gbxindex of a neighbouring gridbox
+map from a given gbxidx to the gbxidx of a neighbouring gridbox
 in that direction */
 struct CartesianMaps {
  private:
-  /* maps from gbxindex to {lower, upper} coords of gridbox boundaries */
+  /* maps from gbxidx to {lower, upper} coords of gridbox boundaries */
   kokkos_pairmap to_coord3bounds;
   kokkos_pairmap to_coord1bounds;
   kokkos_pairmap to_coord2bounds;
 
-  /* maps from gbxindex to gbxindx of front / back neighbour */
+  /* maps from gbxidx to gbxindx of front / back neighbour */
   kokkos_uintmap to_back_coord3nghbr;
   kokkos_uintmap to_forward_coord3nghbr;
   kokkos_uintmap to_back_coord1nghbr;
@@ -223,31 +242,31 @@ struct CartesianMaps {
   }
 
   /* returns {lower bound, upper bound}  in coord3
-  (z) direction of gridbox with index 'gbxindex'
+  (z) direction of gridbox with index 'gbxidx'
   on device */
   KOKKOS_INLINE_FUNCTION
-  Kokkos::pair<double, double> coord3bounds(const unsigned int gbxindex) const {
-    const auto i(to_coord3bounds.find(gbxindex));  // index in map of key 'gbxindex'
+  Kokkos::pair<double, double> coord3bounds(const unsigned int gbxidx) const {
+    const auto i(to_coord3bounds.find(gbxidx));  // index in map of key 'gbxidx'
 
     return to_coord3bounds.value_at(i);  // value returned by map at index i
   }
 
   /* returns {lower bound, upper bound}  in coord1
-  (x) direction of gridbox with index 'gbxindex'
+  (x) direction of gridbox with index 'gbxidx'
   on device */
   KOKKOS_INLINE_FUNCTION
-  Kokkos::pair<double, double> coord1bounds(const unsigned int gbxindex) const {
-    const auto i(to_coord1bounds.find(gbxindex));  // index in map of key 'gbxindex'
+  Kokkos::pair<double, double> coord1bounds(const unsigned int gbxidx) const {
+    const auto i(to_coord1bounds.find(gbxidx));  // index in map of key 'gbxidx'
 
     return to_coord1bounds.value_at(i);  // value returned by map at index i
   }
 
   /* returns {lower bound, upper bound}  in coord2
-  (y) direction of gridbox with index 'gbxindex'
+  (y) direction of gridbox with index 'gbxidx'
   on device */
   KOKKOS_INLINE_FUNCTION
-  Kokkos::pair<double, double> coord2bounds(const unsigned int gbxindex) const {
-    const auto i(to_coord2bounds.find(gbxindex));  // index in map of key 'gbxindex'
+  Kokkos::pair<double, double> coord2bounds(const unsigned int gbxidx) const {
+    const auto i(to_coord2bounds.find(gbxidx));  // index in map of key 'gbxidx'
 
     return to_coord2bounds.value_at(i);  // value returned by map at index i
   }
@@ -256,7 +275,7 @@ struct CartesianMaps {
   gridbox in the backwards coord3, ie. downwards z, direction */
   KOKKOS_INLINE_FUNCTION
   unsigned int coord3backward(unsigned int gbxindex) const {
-    const auto i(to_back_coord3nghbr.find(gbxindex));  // index in map of key 'gbxindex'
+    const auto i(to_back_coord3nghbr.find(gbxindex));  // index in map of key 'gbxidx'
 
     return to_back_coord3nghbr.value_at(i);  // value returned by map at index i
   }
@@ -265,7 +284,7 @@ struct CartesianMaps {
   gridbox in the forwards coord3, ie. upwards z, direction */
   KOKKOS_INLINE_FUNCTION
   unsigned int coord3forward(unsigned int gbxindex) const {
-    const auto i(to_forward_coord3nghbr.find(gbxindex));  // index in map of key 'gbxindex'
+    const auto i(to_forward_coord3nghbr.find(gbxindex));  // index in map of key 'gbxidx'
 
     return to_forward_coord3nghbr.value_at(i);  // value returned by map at index i
   }
@@ -274,7 +293,7 @@ struct CartesianMaps {
   gridbox in the backwards coord1, ie. into page x, direction */
   KOKKOS_INLINE_FUNCTION
   unsigned int coord1backward(unsigned int gbxindex) const {
-    const auto i(to_back_coord1nghbr.find(gbxindex));  // index in map of key 'gbxindex'
+    const auto i(to_back_coord1nghbr.find(gbxindex));  // index in map of key 'gbxidx'
 
     return to_back_coord1nghbr.value_at(i);  // value returned by map at index i
   }
@@ -283,7 +302,7 @@ struct CartesianMaps {
   gridbox in the forwards coord1, ie. out of page x, direction */
   KOKKOS_INLINE_FUNCTION
   unsigned int coord1forward(unsigned int gbxindex) const {
-    const auto i(to_forward_coord1nghbr.find(gbxindex));  // index in map of key 'gbxindex'
+    const auto i(to_forward_coord1nghbr.find(gbxindex));  // index in map of key 'gbxidx'
 
     return to_forward_coord1nghbr.value_at(i);  // value returned by map at index i
   }
@@ -292,7 +311,7 @@ struct CartesianMaps {
   gridbox in the backwards coord2, ie. left y, direction */
   KOKKOS_INLINE_FUNCTION
   unsigned int coord2backward(unsigned int gbxindex) const {
-    const auto i(to_back_coord2nghbr.find(gbxindex));  // index in map of key 'gbxindex'
+    const auto i(to_back_coord2nghbr.find(gbxindex));  // index in map of key 'gbxidx'
 
     return to_back_coord2nghbr.value_at(i);  // value returned by map at index i
   }
@@ -301,7 +320,7 @@ struct CartesianMaps {
   gridbox in the forwards coord2, ie. right y, direction */
   KOKKOS_INLINE_FUNCTION
   unsigned int coord2forward(unsigned int gbxindex) const {
-    const auto i(to_forward_coord2nghbr.find(gbxindex));  // index in map of key 'gbxindex'
+    const auto i(to_forward_coord2nghbr.find(gbxindex));  // index in map of key 'gbxidx'
 
     return to_forward_coord2nghbr.value_at(i);  // value returned by map at index i
   }
