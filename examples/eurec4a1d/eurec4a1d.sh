3be79264dfea5260dceb8f7f329f87c9bded6f34
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-05-03 10:17:54 +0200

    feat(eurec4a1d): specify build, compile and run steps in the bash script itself. Remove calling the generic bash script.

--- examples/eurec4a1d/eurec4a1d.sh
+++ examples/eurec4a1d/eurec4a1d.sh
@@ -12,36 +12,63 @@
 #SBATCH --output=./logfiles/eurec4a1d_out.%j.out
 #SBATCH --error=./logfiles/eurec4a1d_err.%j.out
 
+### ---------------------------------------------------- ###
 ### ------------------ Input Parameters ---------------- ###
 ### ------ You MUST edit these lines to set your ------- ###
-### ---- build type, directories, the executable(s) ---- ###
-### -------- to compile, and your python script -------- ###
+### ----- environment, build type, directories, the ---- ###
+### --------- executable(s) to compile and your -------- ###
+### --------------  python script to run. -------------- ###
 ### ---------------------------------------------------- ###
 buildtype="cuda"
 path2CLEO=${HOME}/CLEO/
 path2build=${HOME}/CLEO/build_eurec4a1D/
 executables="eurec4a1D"
+pythonscript=${path2CLEO}examples/eurec4a1d/eurec4a1d.py
 
 configfile=${path2CLEO}examples/eurec4a1d/src/config/eurec4a1d_config.yaml
-pythonscript=${path2CLEO}examples/eurec4a1d/eurec4a1d.py
-script_args="${configfile}"
+rawdirectory=${path2CLEO}data/output/raw/no_aerosols_no_collision/
 
 path2sdmeurec4a=${HOME}/repositories/sdm-eurec4a/
 cloud_observation_configfile=${path2sdmeurec4a}data/model/input/new/clusters_18.yaml
-rawdirectory=${path2CLEO}data/output/raw/no_aerosols_no_collision/
 
 
 script_args="${HOME} ${configfile} ${cloud_observation_configfile} ${rawdirectory}"
 
 
+cleoenv=/work/mh1126/m300950/cleoenv
+python=${cleoenv}/bin/python3
+spack load cmake@3.23.1%gcc
+module load python3/2022.01-gcc-11.2.0
+source activate ${cleoenv}
 ### ---------------------------------------------------- ###
 ### ---------------------------------------------------- ###
 ### ---------------------------------------------------- ###
 
-### ---------- build, compile and run example ---------- ###
-${path2CLEO}/examples/run_example.sh \
-  ${buildtype} ${path2CLEO} ${path2build} \
-  "${executables}" ${pythonscript} "${script_args}"
+### -------------------- print inputs ------------------ ###
+echo "----- Running Example -----"
+echo "buildtype:  ${buildtype}"
+echo "path2CLEO: ${path2CLEO}"
+echo "path2build: ${path2build}"
+echo "executables: ${executables}"
+echo "pythonscript: ${pythonscript}"
+echo "script_args: ${script_args}"
+echo "---------------------------"
+### ---------------------------------------------------- ###
+
+### ---------------------- build CLEO ------------------ ###
+${path2CLEO}/scripts/bash/build_cleo.sh ${buildtype} ${path2CLEO} ${path2build}
+### ---------------------------------------------------- ###
+
+### --------- compile executable(s) from scratch ---------- ###
+cd ${path2build} && make clean
+
+${path2CLEO}/scripts/bash/compile_cleo.sh ${cleoenv} ${buildtype} ${path2build} "${executables}"
+### ---------------------------------------------------- ###
+
+### --------- run model through Python script ---------- ###
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+${python}  ${pythonscript} ${path2CLEO} ${path2build} ${script_args}
 ### ---------------------------------------------------- ###
 
 echo "--------------------------------------------"
