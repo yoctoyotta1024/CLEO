74ccef7dee104f3a3dad055bbe22d3467e08d48d
clara.bayley <yoctoyotta1024@yahoo.com>
2024-05-04 16:45:19 +0200

    refactor: encapsulate motion functions into struct within movement
    To avoid accidental capture of boudnary conditions by KOKKOS_CLASS_LAMBDA macro

--- libs/gridboxes/movesupersindomain.hpp
+++ libs/gridboxes/movesupersindomain.hpp
@@ -8,7 +8,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Friday 19th April 2024
+ * Last Modified: Saturday 4th May 2024
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -39,83 +39,88 @@ the domain by updating their spatial coordinates (according to
 some type of Motion) and then moving them between gridboxes
 after updating their gridbox indexes concordantly */
 template <GridboxMaps GbxMaps, Motion<GbxMaps> M, typename BoundaryConditions>
-struct MoveSupersInDomain {
-  M motion;
+class MoveSupersInDomain {
+ private:
   BoundaryConditions apply_domain_boundary_conditions;
+  struct EnactMotion {
+    M motion;
 
-  /* enact steps (1) and (2) movement of superdroplets for 1 gridbox:
-  (1) update their spatial coords according to type of motion. (device)
-  (1b) optional detect precipitation (device)
-  (2) update their sdgbxindex accordingly (device).
-  Kokkos::parallel_for([...]) is equivalent to:
-  for (size_t kk(0); kk < supers.extent(0); ++kk) {[...]}
-  when in serial */
-  KOKKOS_INLINE_FUNCTION
-  void move_supers_in_gbx(const TeamMember &team_member, const unsigned int gbxindex,
-                          const GbxMaps &gbxmaps, const State &state,
-                          const subviewd_supers supers) const {
-    const size_t nsupers(supers.extent(0));
-    Kokkos::parallel_for(Kokkos::TeamThreadRange(team_member, nsupers), [&, this](const size_t kk) {
-      /* step (1) */
-      motion.superdrop_coords(gbxindex, gbxmaps, state, supers(kk));
+    /* enact steps (1) and (2) movement of superdroplets for 1 gridbox:
+    (1) update their spatial coords according to type of motion. (device)
+    (1b) optional detect precipitation (device)
+    (2) update their sdgbxindex accordingly (device).
+    Kokkos::parallel_for([...]) is equivalent to:
+    for (size_t kk(0); kk < supers.extent(0); ++kk) {[...]}
+    when in serial */
+    KOKKOS_INLINE_FUNCTION
+    void move_supers_in_gbx(const TeamMember &team_member, const unsigned int gbxindex,
+                            const GbxMaps &gbxmaps, const State &state,
+                            const subviewd_supers supers) const {
+      const size_t nsupers(supers.extent(0));
+      Kokkos::parallel_for(Kokkos::TeamThreadRange(team_member, nsupers),
+                           [&, this](const size_t kk) {
+                             /* step (1) */
+                             motion.superdrop_coords(gbxindex, gbxmaps, state, supers(kk));
 
-      /* optional step (1b) */
-      // gbx.detectors -> detect_precipitation(area, drop); // TODO(CB) detectors
+                             /* optional step (1b) */
+                             // gbx.detectors -> detect_precipitation(area, drop); // TODO(CB)
+                             // detectors
 
-      /* step (2) */
-      motion.superdrop_gbx(gbxindex, gbxmaps, supers(kk));
-    });
-  }
+                             /* step (2) */
+                             motion.superdrop_gbx(gbxindex, gbxmaps, supers(kk));
+                           });
+    }
 
-  /* enact steps (1) and (2) movement of superdroplets
-  throughout domain (i.e. for all gridboxes):
-  (1) update their spatial coords according to type of motion. (device)
-  (1b) optional detect precipitation (device)
-  (2) update their sdgbxindex accordingly (device).
-  Kokkos::parallel_for([...]) is equivalent to:
-  for (size_t ii(0); ii < ngbxs; ++ii) {[...]}
-  when in serial */
-  void move_supers_in_gridboxes(const GbxMaps &gbxmaps, const viewd_gbx d_gbxs) const {
-    const size_t ngbxs(d_gbxs.extent(0));
+    /* enact steps (1) and (2) movement of superdroplets
+    throughout domain (i.e. for all gridboxes):
+    (1) update their spatial coords according to type of motion. (device)
+    (1b) optional detect precipitation (device)
+    (2) update their sdgbxindex accordingly (device).
+    Kokkos::parallel_for([...]) is equivalent to:
+    for (size_t ii(0); ii < ngbxs; ++ii) {[...]}
+    when in serial */
+    void move_supers_in_gridboxes(const GbxMaps &gbxmaps, const viewd_gbx d_gbxs) const {
+      const size_t ngbxs(d_gbxs.extent(0));
 
-    Kokkos::parallel_for(
-        "move_supers_in_gridboxes", TeamPolicy(ngbxs, Kokkos::AUTO()),
-        KOKKOS_CLASS_LAMBDA(const TeamMember &team_member) {
-          const int ii = team_member.league_rank();
+      Kokkos::parallel_for(
+          "move_supers_in_gridboxes", TeamPolicy(ngbxs, Kokkos::AUTO()),
+          KOKKOS_CLASS_LAMBDA(const TeamMember &team_member) {
+            const int ii = team_member.league_rank();
 
-          auto &gbx(d_gbxs(ii));
-          move_supers_in_gbx(team_member, gbx.get_gbxindex(), gbxmaps, gbx.state,
-                             gbx.supersingbx());
-        });
-  }
+            auto &gbx(d_gbxs(ii));
+            move_supers_in_gbx(team_member, gbx.get_gbxindex(), gbxmaps, gbx.state,
+                               gbx.supersingbx());
+          });
+    }
 
-  /* (re)sorting supers based on their gbxindexes and then updating the span for each gridbox
-  accordingly.
-  Kokkos::parallel_for([...]) (on host) is equivalent to:
-  for (size_t ii(0); ii < ngbxs; ++ii){[...]}
-  when in serial
-  _Note:_ totsupers is view of all superdrops (both in and out of bounds of domain).
-  */
-  void move_supers_between_gridboxes(const viewd_gbx d_gbxs, const viewd_supers totsupers) const {
-    sort_supers(totsupers);
+    /* (re)sorting supers based on their gbxindexes and then updating the span for each gridbox
+    accordingly.
+    Kokkos::parallel_for([...]) (on host) is equivalent to:
+    for (size_t ii(0); ii < ngbxs; ++ii){[...]}
+    when in serial
+    _Note:_ totsupers is view of all superdrops (both in and out of bounds of domain).
+    */
+    void move_supers_between_gridboxes(const viewd_gbx d_gbxs, const viewd_supers totsupers) const {
+      sort_supers(totsupers);
 
-    const size_t ngbxs(d_gbxs.extent(0));
-    Kokkos::parallel_for(
-        "move_supers_between_gridboxes", TeamPolicy(ngbxs, Kokkos::AUTO()),
-        KOKKOS_CLASS_LAMBDA(const TeamMember &team_member) {
-          const int ii = team_member.league_rank();
+      const size_t ngbxs(d_gbxs.extent(0));
+      Kokkos::parallel_for(
+          "move_supers_between_gridboxes", TeamPolicy(ngbxs, Kokkos::AUTO()),
+          KOKKOS_CLASS_LAMBDA(const TeamMember &team_member) {
+            const int ii = team_member.league_rank();
 
-          auto &gbx(d_gbxs(ii));
-          gbx.supersingbx.set_refs(team_member);
-        });
+            auto &gbx(d_gbxs(ii));
+            gbx.supersingbx.set_refs(team_member);
+          });
 
-    // /* optional (expensive!) test to raise error if
-    // superdrops' gbxindex doesn't match gridbox's gbxindex */
-    // for (size_t ii(0); ii < ngbxs; ++ii)
-    // {
-    //   d_gbxs(ii).supersingbx.iscorrect();
-    // }
-  }
+      // /* optional (expensive!) test to raise error if
+      // superdrops' gbxindex doesn't match gridbox's gbxindex */
+      // for (size_t ii(0); ii < ngbxs; ++ii)
+      // {
+      //   d_gbxs(ii).supersingbx.iscorrect();
+      // }
+    }
+  } enactmotion;
 
   /* enact movement of superdroplets throughout domain in three stages:
   (1) update their spatial coords according to type of motion. (device)
@@ -130,17 +135,18 @@ struct MoveSupersInDomain {
   void move_superdrops_in_domain(const unsigned int t_sdm, const GbxMaps &gbxmaps, viewd_gbx d_gbxs,
                                  const viewd_supers totsupers) const {
     /* steps (1 - 2) */
-    move_supers_in_gridboxes(gbxmaps, d_gbxs);
+    enactmotion.move_supers_in_gridboxes(gbxmaps, d_gbxs);
 
     /* step (3) */
-    move_supers_between_gridboxes(d_gbxs, totsupers);
+    enactmotion.move_supers_between_gridboxes(d_gbxs, totsupers);
 
     /* step (4) */
     apply_domain_boundary_conditions(gbxmaps, d_gbxs, totsupers);
   }
 
+ public:
   MoveSupersInDomain(const M mtn, const BoundaryConditions boundary_conditions)
-      : motion(mtn), apply_domain_boundary_conditions(boundary_conditions) {}
+      : apply_domain_boundary_conditions(boundary_conditions), enactmotion({mtn}) {}
 
   /* extra constructor useful to help when compiler cannot deduce type of GBxMaps */
   MoveSupersInDomain(const GbxMaps &gbxmaps, const M mtn,
@@ -150,7 +156,9 @@ struct MoveSupersInDomain {
   /* returns time when superdroplet motion is
   next due to occur given current time, t_sdm */
   KOKKOS_INLINE_FUNCTION
-  unsigned int next_step(const unsigned int t_sdm) const { return motion.next_step(t_sdm); }
+  unsigned int next_step(const unsigned int t_sdm) const {
+    return enactmotion.motion.next_step(t_sdm);
+  }
 
   /*
    * if current time, t_sdm, is time when superdrop motion should occur, enact movement of
@@ -161,7 +169,7 @@ struct MoveSupersInDomain {
    */
   void run_step(const unsigned int t_sdm, const GbxMaps &gbxmaps, viewd_gbx d_gbxs,
                 const viewd_supers totsupers) const {
-    if (motion.on_step(t_sdm)) {
+    if (enactmotion.motion.on_step(t_sdm)) {
       move_superdrops_in_domain(t_sdm, gbxmaps, d_gbxs, totsupers);
     }
   }
