e33870c606ab4373e3eb41e431639d6af92012a1
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-05-06 09:48:02 +0200

    refactor(eurec4a1d): use seperate scripts for experiment setups
    The original evolving setup (no boundary condition) and the new stationary setup with boundary conditions should be implemented seperatly to keep a better overview

--- examples/eurec4a1d/eurec4a1d.sh
+++ examples/eurec4a1d/eurec4a1d.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=eurec4a1d
-#SBATCH --partition=gpu
+#SBATCH --partition=compute
 #SBATCH --nodes=1
-#SBATCH --gpus=4
 #SBATCH --ntasks-per-node=128
 #SBATCH --mem=30G
 #SBATCH --time=00:10:00
@@ -19,29 +18,54 @@
 ### --------- executable(s) to compile and your -------- ###
 ### --------------  python script to run. -------------- ###
 ### ---------------------------------------------------- ###
-buildtype="cuda"
-path2CLEO=${HOME}/CLEO/
-path2build=${HOME}/CLEO/build_eurec4a1D/
-executables="eurec4a1D"
-pythonscript=${path2CLEO}examples/eurec4a1d/eurec4a1d.py
 
-configfile=${path2CLEO}examples/eurec4a1d/src/config/eurec4a1d_config.yaml
-rawdirectory=${path2CLEO}data/output/raw/no_aerosols_no_collision/
+echo "--------------------------------------------"
+echo "START RUN"
+date
+echo "git hash: $(git rev-parse HEAD)"
+echo "git branch: $(git symbolic-ref --short HEAD)"
+echo "============================================"
+
+buildtype="openmp"
+path2CLEO=${HOME}/CLEO/
+path2builds=${path2CLEO}builds/
+path2data=${path2CLEO}data/output/raw/
+path2eurec4a1d=${path2CLEO}examples/eurec4a1d/
+# Use the stationary or evolving version of the model
+
+### ---------- Setup for the EUREC4A1D model ---------- ###
+
+# --- stationary version, with super droplet creation at domain top by boundarz conditions
+path2build=${path2builds}build_eurec4a1D_stationary/
+pythonscript=${path2eurec4a1d}scripts/eurec4a1d_stationary.py
+configfile=${path2eurec4a1d}src/config/eurec4a1d_config_stationary.yaml
+rawdirectory=${path2data}stationary/
+
+# # --- evolving version, without super droplet creation at domain top
+# path2build=${path2builds}build_eurec4a1D_evolving/
+# pythonscript=${path2eurec4a1d}scripts/eurec4a1d_evolving.py
+# configfile=${path2eurec4a1d}src/config/eurec4a1d_config_evolving.yaml
+# rawdirectory=${path2data}evolving/
+
+
+executables="eurec4a1D"
+
 
 path2sdmeurec4a=${HOME}/repositories/sdm-eurec4a/
 cloud_observation_configfile=${path2sdmeurec4a}data/model/input/new/clusters_18.yaml
 
-
+# create the script arguments
 script_args="${HOME} ${configfile} ${cloud_observation_configfile} ${rawdirectory}"
+### ---------------------------------------------------- ###
 
 
+
+### ------------------ Load Modules -------------------- ###
 cleoenv=/work/mh1126/m300950/cleoenv
 python=${cleoenv}/bin/python3
 spack load cmake@3.23.1%gcc
 module load python3/2022.01-gcc-11.2.0
 source activate ${cleoenv}
-### ---------------------------------------------------- ###
-### ---------------------------------------------------- ###
 ### ---------------------------------------------------- ###
 
 ### -------------------- print inputs ------------------ ###
@@ -55,15 +79,15 @@ echo "script_args: ${script_args}"
 echo "---------------------------"
 ### ---------------------------------------------------- ###
 
-### ---------------------- build CLEO ------------------ ###
-${path2CLEO}/scripts/bash/build_cleo.sh ${buildtype} ${path2CLEO} ${path2build}
-### ---------------------------------------------------- ###
+# ### ---------------------- build CLEO ------------------ ###
+# ${path2CLEO}/scripts/bash/build_cleo.sh ${buildtype} ${path2CLEO} ${path2build}
+# ### ---------------------------------------------------- ###
 
-### --------- compile executable(s) from scratch ---------- ###
-cd ${path2build} && make clean
+# ### --------- compile executable(s) from scratch ---------- ###
+# cd ${path2build} && make clean
 
-${path2CLEO}/scripts/bash/compile_cleo.sh ${cleoenv} ${buildtype} ${path2build} "${executables}"
-### ---------------------------------------------------- ###
+# ${path2CLEO}/scripts/bash/compile_cleo.sh ${cleoenv} ${buildtype} ${path2build} "${executables}"
+# ### ---------------------------------------------------- ###
 
 ### --------- run model through Python script ---------- ###
 export OMP_PROC_BIND=spread
