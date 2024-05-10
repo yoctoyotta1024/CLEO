a18a73496c44ff19d759ccc9c95e097173c23eac
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-05-10 10:21:23 +0200

    feat: function to create a eulerian ragged array from a data array and a same shaped indexer array

--- awkward_example.py
+++ awkward_example.py
@@ -2,11 +2,16 @@
 import awkward as ak
 import numpy as np
 from tqdm import tqdm
+from pySD.sdmout_src import pysetuptxt, pyzarr
+from typing import Union
+
+
+IntegerArray = Union[np.ndarray, ak.highlevel.Array]
 
 # %%
 data = ak.Array(
     [
-        [10, 20, 30],
+        [10.2, 20, 30],
         [12],
         [40, 50],
         [90],
@@ -22,9 +27,9 @@ time_index = ak.Array(
 )
 gridbox = ak.Array(
     [
-        [10, 1, 10],
+        [5, 1, 2],
         [1],
-        [1, 2],
+        [3, 3],
         [3],
     ]
 )
@@ -40,12 +45,14 @@ counts = ak.Array(
 
 id = ak.Array(
     [
-        [10, 1, 2],
+        [3, 1, 2],
         [2],
-        [2, 3],
+        [1, 3],
         [7],
     ]
 )
+
+# %%
 
 
 def eulerian_from_count(data, counts, G):
@@ -67,8 +74,47 @@ def create_counts_np(data, counts):
     return counts
 
 
-def create_counts_fast(data, T):
+def create_counts_fast(data: IntegerArray) -> ak.highlevel.Array:
+    """
+    This function creates a 2D array with the counts of unique values in the input array.
+    It identifies the unique values in each subarray along axis 1.
+
+    Parameters
+    ----------
+    data : IntegerArray
+        The input array containing the data for which counts need to be calculated.
+        Only ``int`` types are allowed!
+        If an ``np.ndarray`` is used, the dimensions should be T x N
+        If an ``ak.highlevel.Array`` is used, the dimensions should be T x var
+
+    Returns
+    -------
+    ak.highlevel.Array
+        A 2D array with the counts of unique values in the input array.
+    """
+
+    # check for int type:
+
+    assert (
+        data.ndim == 2
+    ), f"data needs to be 2 dimensional but is {data.ndim} dimensional"
+
+    if isinstance(data, np.ndarray):
+        dtype = data.dtype
+    elif isinstance(data, ak.highlevel.Array):
+        dtype = data.type.content.content.primitive
+    else:
+        raise TypeError(
+            f"The provided type of 'data': '{type(data)}' is not supported!"
+        )
+
+    if not np.issubdtype(dtype, np.integer):
+        raise TypeError(
+            f"The array must contain 'int' like dtypes but has '{dtype}'. This is not supported"
+        )
+
     M = int(ak.max(data)) + 1
+    T = int(ak.num(data, axis=0))
     modified = data + np.arange(T) * M
     modified = ak.flatten(modified)
 
@@ -89,21 +135,70 @@ N = int(ak.max(id_unique, axis=0)) + 1
 
 
 gridbox_argsort = ak.argsort(gridbox)
-# gridbox_argsort = ak.argsort(id)
+gridbox_argsort = ak.argsort(id)
 id_sort = id[gridbox_argsort]
 gridbox_sort = gridbox[gridbox_argsort]
 data_sort = data[gridbox_argsort]
 id_sort = id[gridbox_argsort]
 
 # %%
-eulerian_from_count(data_sort, create_counts_fast(id_sort, T), N)
-
-# %%
 
 
-# unique = ak.unflatten(unique, num)
-# counts = ak.unflatten(counts, num)
+def euler_full(
+    data: ak.highlevel.Array, indexer: ak.highlevel.Array
+) -> ak.highlevel.Array:
+    """
+    Calculates the Eulerian from the given data and indexer.
+    The indexer array needs to have a ``int`` like dtype.
+    The output shape of the array will be given by the maximum value M in ``indexer``.
+    Output shape : T x M x var
 
+    Note
+    ----
+    This functions uses np.bincount on the lower levels.
+    Thus, at one point an np.ndarray of shape (T * M) will be created and needs to be stored in memory.
+    This limits the capability due to memory usage.
+
+    For a lagrangian tracking for non sparse outputs, it is prefered to use the lagrangian function.
+
+    Parameters
+    ----------
+    data (ak.highlevel.Array):
+        The input data array (T x var).
+    indexer (ak.highlevel.Array):
+        The indexer array (T x var) of dtype int.
+        With maximum value M.
+
+    Returns
+    -------
+    ak.highlevel.Array:
+        The calculated Eulerian array of shape (T x M x var)
+    """
+    # indexer_unique = np.unique(ak.flatten(indexer))
+    N = int(ak.max(indexer)) + 1
+
+    # sort arrays by their
+    argsort = ak.argsort(indexer, axis=1)
+    indexer_sort = indexer[argsort]
+    data_sort = data[argsort]
+
+    counts = create_counts_fast(indexer_sort)
+
+    # The function eulerian_from_count performes this bit of code:
+    # ----------
+    # # unflatten the data array using the counts array.
+    # res = ak.unflatten(data, ak.flatten(counts), axis=1)
+    # # flatten this array again
+    # res = ak.unflatten(ak.flatten(res), N)
+    # # then unflatten with the correct length of the number of gridboxes
+    # res = ak.unflatten(ak.flatten(res), N)
+    # return res
+    # ----------
+
+    return eulerian_from_count(data_sort, counts, N)
+
+
+euler_full(data, id)
 # %%
 
 
@@ -113,10 +208,6 @@ def cut_unique_values(array, data):
         return ak.unflatten(data, counts)
     else:
         return array
-
-
-# %%
-cut_unique_values(gridbox_sort[0], data_sort[0])
 
 
 # %%
@@ -167,9 +258,6 @@ def euler2(data_sort, gridbox_sort, T, G):
 
 # %%
 
-from pySD.sdmout_src import pysetuptxt, pyzarr
-import numpy as np
-
 setupfile = "/home/m/m301096/CLEO/data/output/raw/no_aerosols_collision_many_5012/clusters_301/eurec4a1d_setup.txt"
 dataset = "/home/m/m301096/CLEO/data/output/raw/no_aerosols_collision_many_5012/clusters_301/eurec4a1d_sol.zarr"
 # read in constants and intial setup from setup .txt file
@@ -178,6 +266,12 @@ consts = pysetuptxt.get_consts(setupfile, isprint=False)
 # Create a first simple dataset to have the coordinates for later netcdf creation
 sddata = pyzarr.get_supers(dataset, consts)
 
+
+data = sddata.radius
+id = sddata.sdId + int(1e5)
+gridbox = sddata.sdgbxindex
+
+time_index = sddata.time
 time_unique = np.unique(sddata.time)
 gridbox_unique = np.unique(ak.flatten(sddata.sdgbxindex))
 id_unique = np.unique(ak.flatten(sddata.sdId))
@@ -195,20 +289,48 @@ data_sort = sddata.radius[gridbox_argsort]
 # %%
 # counts_fast = create_counts_fast(id_sort, T, G)
 # eulerian_from_count(data_sort, create_counts_fast(gridbox_sort, T, G), G)
-counts_fast = create_counts_fast(id_sort, T)
-
+counts_fast = create_counts_fast(id_sort)
+res = euler_full()
 # counts_np = np.zeros((T, N), dtype=int)
 # counts_np = create_counts_np(id_sort, counts_np)
 
 # %%
 
-res = eulerian_from_count(data_sort, counts_fast, N)
-M = ak.max(ak.num(res, axis=-1))
-res_padded = ak.pad_none(res, M, axis=-1)
-ma = res_padded.to_numpy()
-ma_float = ma.astype(float)
+# res = eulerian_from_count(data_sort, counts_fast, N)
+# M = ak.max(ak.num(res, axis=-1))
+# res_padded = ak.pad_none(res, M, axis=-1)
+# ma = res_padded.to_numpy()
+# ma_float = ma.astype(float)
 
 
-a = np.ma.getdata(ma_float)
-a[np.ma.getmask(ma_float)] = np.nan
-a
+# a = np.ma.getdata(ma_float)
+# a[np.ma.getmask(ma_float)] = np.nan
+# a
+
+# %%
+
+
+def get_shape(a):
+    np_size = [int(ak.num(a, axis=0))]
+    for i in range(1, a.ndim):
+        np_size.append(ak.max(ak.num(res, axis=i)))
+    return np_size
+
+
+def calc_memory_array(elements):
+    memory_usage = elements * 8  # 8 bytes per float
+
+    if memory_usage < 1024 * 1024:
+        memory_usage_mb = memory_usage / (1024 * 1024)
+        return f"Memory usage: {memory_usage_mb:.2f} Mb"
+    elif memory_usage < 1024 * 1024 * 1024:
+        memory_usage_gb = memory_usage / (1024 * 1024 * 1024)
+        return f"Memory usage: {memory_usage_gb:.2f} Gb"
+    else:
+        memory_usage_tb = memory_usage / (1024 * 1024 * 1024 * 1024)
+        return f"Memory usage: {memory_usage_tb:.2f} Tb"
+
+
+print(calc_memory_array(np.prod(get_shape(res))))
+print(calc_memory_array(ak.count(res)))
+# %%
