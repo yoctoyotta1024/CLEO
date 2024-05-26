7c4298a1ba73134edcf9d636f6d3f31c253da125
clara.bayley <yoctoyotta1024@yahoo.com>
2024-05-26 20:29:18 +0200

    fix: typo in cmake flag for setting C compiler

--- scripts/bash/build_cleo_cuda_openmp.sh
+++ scripts/bash/build_cleo_cuda_openmp.sh
@@ -78,7 +78,7 @@ echo "KOKKOS_HOST_PARALLELISM: ${kokkoshost}"
 echo "CMAKE_CXX_FLAGS: ${CMAKE_CXX_FLAGS}"
 
 cmake -DCMAKE_CXX_COMPILER=${CXX} \
-    -DCMAKE_CC_COMPILER=${CC} \
+    -DCMAKE_C_COMPILER=${CC} \
     -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS}" \
     -S ${path2CLEO} -B ${path2build} \
     ${kokkosflags} ${kokkosdevice} ${kokkoshost}
