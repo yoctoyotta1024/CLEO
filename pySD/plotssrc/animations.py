c48eb9a59d1024b3bd1a0641169eab3b2deb4726
nilsnevertree <49751662+nilsnevertree@users.noreply.github.com>
2024-05-29 19:19:43 +0200

    style: Apply pre-commit to all non example file to follow ruff formating

--- pySD/plotssrc/animations.py
+++ pySD/plotssrc/animations.py
@@ -1,4 +1,4 @@
-'''
+"""
 ----- CLEO -----
 File: animations.py
 Project: plotssrc
@@ -17,7 +17,7 @@ Copyright (c) 2023 MPI-M, Clara Bayley
 File Description:
 functions to create 1D or 2D animations
 of some output from CLEO e.g. mass moments
-'''
+"""
 
 
 import numpy as np
@@ -27,137 +27,176 @@ from matplotlib import animation
 from matplotlib.cm import ScalarMappable
 
 
-def animate_me(fig, update_frame, frames, plot_init,
-               saveani=False, savename=None, fargs=(), fps=5):
+def animate_me(
+    fig, update_frame, frames, plot_init, saveani=False, savename=None, fargs=(), fps=5
+):
+    print("making animation")
+    ani = FuncAnimation(
+        fig, update_frame, frames=range(0, frames), init_func=plot_init, fargs=fargs
+    )
 
-  print("making animation")
-  ani = FuncAnimation(fig, update_frame, frames=range(0, frames),
-                      init_func=plot_init, fargs=fargs)
+    if saveani:
+        print("saving animation as " + savename + ".gif")
+        ani.save(
+            savename + ".gif",
+            writer=animation.PillowWriter(fps=fps, bitrate=5000, codec="h264"),
+            dpi=100,
+            savefig_kwargs={"transparent": True},
+        )
 
-  if saveani:
-    print("saving animation as "+savename+".gif")
-    ani.save(savename+".gif",
-            writer=animation.PillowWriter(fps=fps, bitrate=5000, codec='h264'),
-            dpi=100, savefig_kwargs={'transparent': True})
 
-def animate1dprofile(gbxs, mom, timemins, nframes,
-                     xlabel=None, xlims=[None, None], color="black",
-                     saveani=False, savename=None, fps=5):
+def animate1dprofile(
+    gbxs,
+    mom,
+    timemins,
+    nframes,
+    xlabel=None,
+    xlims=[None, None],
+    color="black",
+    saveani=False,
+    savename=None,
+    fps=5,
+):
+    fig, ax, plots, txt, zkm = prepare_1dprofile(
+        gbxs, mom, timemins, xlabel, color=color
+    )
 
-  fig, ax, plots, txt, zkm = prepare_1dprofile(gbxs, mom, timemins,
-                                               xlabel, color=color)
+    def init_1dprofile():
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
 
-  def init_1dprofile():
+        ylims = [0, ax.get_ylim()[1]]
+        ax.set_ylim(ylims)
+        yticks = np.arange(ylims[0], ylims[1], 0.5)
+        ax.set_yticks(yticks, yticks, fontsize=16)
 
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+        ax.set_xlim([xlims[0], xlims[1] * 1.1])
+        xticks = np.linspace(xlims[0], xlims[1], 3)
+        xticklabels = xticklabels = ["{:.1f}".format(x) for x in xticks]
+        ax.set_xticks(xticks, xticklabels, fontsize=16)
 
-    ylims = [0, ax.get_ylim()[1]]
-    ax.set_ylim(ylims)
-    yticks = np.arange(ylims[0], ylims[1], 0.5)
-    ax.set_yticks(yticks, yticks, fontsize=16)
+        ax.tick_params(length=10, width=1)
 
-    ax.set_xlim([xlims[0], xlims[1]*1.1])
-    xticks = np.linspace(xlims[0], xlims[1], 3)
-    xticklabels = xticklabels = ["{:.1f}".format(x) for x in xticks]
-    ax.set_xticks(xticks, xticklabels, fontsize=16)
+        fig.tight_layout()
 
-    ax.tick_params(length=10, width=1)
+        return (
+            plots,
+            txt,
+        )
 
-    fig.tight_layout()
+    animate_me(
+        fig,
+        update_1dprofileframe,
+        nframes,
+        init_1dprofile,
+        saveani=saveani,
+        savename=savename,
+        fargs=(plots, txt, zkm, timemins, mom),
+        fps=fps,
+    )
 
-    return plots, txt,
-
-  animate_me(fig, update_1dprofileframe, nframes, init_1dprofile,
-           saveani=saveani, savename=savename,
-           fargs=(plots, txt, zkm, timemins, mom), fps=fps)
 
 def prepare_1dprofile(gbxs, massmom, timemins, xlabel, color):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 8))
 
-  fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 8))
+    zkm = gbxs["zfull"] / 1000  # convert m to km
 
-  alpha = 0.3
-  zkm = gbxs["zfull"] / 1000 # convert m to km
+    f = 0
+    plots = ax.plot(massmom[f], zkm, color=color)[0]
 
-  f = 0
-  plots = ax.plot(massmom[f], zkm, color=color)[0]
+    timetext = "t = {:.0f}min".format(timemins[f])
+    txt = fig.text(0.7, 0.875, timetext, fontsize=16)
 
-  timetext = "t = {:.0f}min".format(timemins[f])
-  txt = fig.text(0.7, 0.875, timetext, fontsize=16)
-
-  ax.set_ylabel("z /km", fontsize=16)
-  ax.set_xlabel(xlabel, fontsize=16)
-
-  fig.tight_layout()
-
-  return fig, ax, plots, txt, zkm
-
-def update_1dprofileframe(f, plots, txt, zkm, timemins, massmom):
-
-  plots.set_data(massmom[f], zkm)
-
-  timetext = "t = {:.0f}min".format(timemins[f])
-  txt.set_text(timetext)
-
-  return plots, txt,
-
-def animate2dcmap(gbxs, mom2ani, timemins, nframes,
-                  cbarlabel=None, cmapnorm=None, cmap="viridis",
-                  saveani=False, savename=None, fps=5):
-
-  fig, ax, cbar, plot, txt = prepare_2dplot(gbxs, mom2ani, timemins,
-                                            cmap, cmapnorm)
-  cbar.set_label(cbarlabel, fontsize=16)
-
-  def init_2dcmap():
-
-    ax.set_aspect("equal")
-    ax.set_xlim([0, 1.5])
-    ax.set_ylim([0, 1.5])
-
-    ax.set_xlabel("x / km", fontsize=16)
-    ax.set_ylabel("z / km", fontsize=16)
-
-    ticks = [0, 0.75, 1.5]
-    ax.set_xticks(ticks, ticks, fontsize=16)
-    ax.set_yticks(ticks, ticks, fontsize=16)
-
-    ax.tick_params(length=10, width=1)
+    ax.set_ylabel("z /km", fontsize=16)
+    ax.set_xlabel(xlabel, fontsize=16)
 
     fig.tight_layout()
-    return plot
 
-  animate_me(fig, update_2dcmapframe, nframes,
-             init_2dcmap, saveani, savename,
-             fargs=(plot, txt, timemins, mom2ani), fps=fps)
+    return fig, ax, plots, txt, zkm
+
+
+def update_1dprofileframe(f, plots, txt, zkm, timemins, massmom):
+    plots.set_data(massmom[f], zkm)
+
+    timetext = "t = {:.0f}min".format(timemins[f])
+    txt.set_text(timetext)
+
+    return (
+        plots,
+        txt,
+    )
+
+
+def animate2dcmap(
+    gbxs,
+    mom2ani,
+    timemins,
+    nframes,
+    cbarlabel=None,
+    cmapnorm=None,
+    cmap="viridis",
+    saveani=False,
+    savename=None,
+    fps=5,
+):
+    fig, ax, cbar, plot, txt = prepare_2dplot(gbxs, mom2ani, timemins, cmap, cmapnorm)
+    cbar.set_label(cbarlabel, fontsize=16)
+
+    def init_2dcmap():
+        ax.set_aspect("equal")
+        ax.set_xlim([0, 1.5])
+        ax.set_ylim([0, 1.5])
+
+        ax.set_xlabel("x / km", fontsize=16)
+        ax.set_ylabel("z / km", fontsize=16)
+
+        ticks = [0, 0.75, 1.5]
+        ax.set_xticks(ticks, ticks, fontsize=16)
+        ax.set_yticks(ticks, ticks, fontsize=16)
+
+        ax.tick_params(length=10, width=1)
+
+        fig.tight_layout()
+        return plot
+
+    animate_me(
+        fig,
+        update_2dcmapframe,
+        nframes,
+        init_2dcmap,
+        saveani,
+        savename,
+        fargs=(plot, txt, timemins, mom2ani),
+        fps=fps,
+    )
+
 
 def prepare_2dplot(gbxs, massmom, timemins, cmap, cmapnorm):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 9))
 
-  fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 9))
+    f = 0
+    data2d = massmom[f, :, :]
+    plot = ax.pcolormesh(
+        gbxs["xxh"] / 1000, gbxs["zzh"] / 1000, data2d, cmap=cmap, norm=cmapnorm
+    )
+    plot.cmap.set_under("w")
+    timetext = "t = {:.0f}min".format(timemins[f])
+    txt = fig.text(0.765, 0.925, timetext, fontsize=16, ha="right")
 
-  f=0
-  data2d = massmom[f,:,:]
-  plot = ax.pcolormesh(gbxs["xxh"]/1000, gbxs["zzh"]/1000, data2d,
-                      cmap=cmap, norm=cmapnorm)
-  plot.cmap.set_under("w")
-  timetext = "t = {:.0f}min".format(timemins[f])
-  txt = fig.text(0.765, 0.925, timetext, fontsize=16, ha="right")
+    cbar = fig.colorbar(ScalarMappable(norm=cmapnorm, cmap=cmap), ax=ax, extend="both")
+    cbar.ax.tick_params(labelsize=16, which="both")
+    cbar.ax.tick_params(length=10, width=1, which="major")
+    cbar.ax.tick_params(length=10 / 3, width=1, which="minor")
 
-  cbar = fig.colorbar(ScalarMappable(norm=cmapnorm, cmap=cmap),
-                      ax=ax, extend="both")
-  cbar.ax.tick_params(labelsize=16, which="both")
-  cbar.ax.tick_params(length=10, width=1, which="major")
-  cbar.ax.tick_params(length=10/3, width=1, which="minor")
+    fig.tight_layout()
 
-  fig.tight_layout()
+    return fig, ax, cbar, plot, txt
 
-  return fig, ax, cbar, plot, txt
 
 def update_2dcmapframe(f, plot, txt, timemins, massmom):
+    plot.set_array(np.array(massmom[f, :, :]).ravel())
 
-  plot.set_array(np.array(massmom[f,:,:]).ravel())
+    timetext = "t = {:.0f}min".format(timemins[f])
+    txt.set_text(timetext)
 
-  timetext = "t = {:.0f}min".format(timemins[f])
-  txt.set_text(timetext)
-
-  return plot, txt
+    return plot, txt
--- pySD/plotssrc/pltmoms.py
+++ pySD/plotssrc/pltmoms.py
@@ -1,4 +1,4 @@
-'''
+"""
 ----- CLEO -----
 File: pltmoms.py
 Project: plotssrc
@@ -16,57 +16,55 @@ Copyright (c) 2023 MPI-M, Clara Bayley
 -----
 File Description:
 examples for ploting momments of the droplet distirbution
-'''
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plot_totnsupers(time, totnsupers, savename=""):
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 5), sharex=True)
 
-  fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8,5), sharex=True)
+    ax.plot(time.mins, totnsupers)
 
-  ax.plot(time.mins, totnsupers)
+    ax.set_ylabel("domain total number of superdroplets")
+    ax.set_xlabel("time /min")
 
-  ax.set_ylabel("domain total number of superdroplets")
-  ax.set_xlabel("time /min")
+    fig.tight_layout()
+    if savename != "":
+        fig.savefig(savename, dpi=400, bbox_inches="tight", facecolor="w", format="png")
+        print("Figure .png saved as: " + savename)
 
-  fig.tight_layout()
-  if savename != "":
-    fig.savefig(savename, dpi=400, bbox_inches="tight",
-                facecolor='w', format="png")
-    print("Figure .png saved as: "+savename)
+    plt.show()
 
-  plt.show()
 
 def plot_domainmassmoments(time, massmoms, savename=""):
+    def totmassmom(massmom):
+        """mass moment summed over entire domain"""
+        return np.sum(massmom, axis=(1, 2, 3))
 
-  def totmassmom(massmom):
-    '''mass moment summed over entire domain'''
-    return  np.sum(massmom, axis=(1,2,3))
+    fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(6, 8), sharex=True)
+    fig.suptitle("Total Mass Moments Over Domain")
 
-  fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(6,8), sharex=True)
-  fig.suptitle("Total Mass Moments Over Domain")
+    axs[0].plot(time.mins, totmassmom(massmoms.nsupers))
+    axs[1].plot(time.mins, totmassmom(massmoms.mom0))
+    axs[2].plot(time.mins, totmassmom(massmoms.mom1))
+    axs[3].plot(time.mins, totmassmom(massmoms.mom2))
+    meaneffmass = np.mean((massmoms.effmass), axis=(1, 2, 3))
+    axs[4].plot(time.mins, meaneffmass)
 
-  axs[0].plot(time.mins, totmassmom(massmoms.nsupers))
-  axs[1].plot(time.mins, totmassmom(massmoms.mom0))
-  axs[2].plot(time.mins, totmassmom(massmoms.mom1))
-  axs[3].plot(time.mins, totmassmom(massmoms.mom2))
-  meaneffmass = np.mean((massmoms.effmass), axis=(1,2,3))
-  axs[4].plot(time.mins, meaneffmass)
+    axs[0].set_ylabel("number of\nsuperdroplets")
+    axs[1].set_ylabel("$\u03BB^{m}_{0}$, number\nof droplets")
+    axs[2].set_ylabel("$\u03BB^{m}_{1}$, droplet\nmass /g")
+    axs[3].set_ylabel("$\u03BB^{m}_{2}$\n~reflectivity /g$^2$")
+    ylab4 = "mean effective\ndroplet mass,\n<$\u03BB^{m}_{2}$/$\u03BB^{m}_{1}>$ /g"
+    axs[4].set_ylabel(ylab4)
 
-  axs[0].set_ylabel("number of\nsuperdroplets")
-  axs[1].set_ylabel("$\u03BB^{m}_{0}$, number\nof droplets")
-  axs[2].set_ylabel("$\u03BB^{m}_{1}$, droplet\nmass /g")
-  axs[3].set_ylabel("$\u03BB^{m}_{2}$\n~reflectivity /g$^2$")
-  ylab4 = "mean effective\ndroplet mass,\n<$\u03BB^{m}_{2}$/$\u03BB^{m}_{1}>$ /g"
-  axs[4].set_ylabel(ylab4)
+    axs[-1].set_xlabel("time /min")
 
-  axs[-1].set_xlabel("time /min")
+    fig.tight_layout()
+    if savename != "":
+        fig.savefig(savename, dpi=400, bbox_inches="tight", facecolor="w", format="png")
+        print("Figure .png saved as: " + savename)
 
-  fig.tight_layout()
-  if savename != "":
-    fig.savefig(savename, dpi=400, bbox_inches="tight",
-                facecolor='w', format="png")
-    print("Figure .png saved as: "+savename)
-
-  plt.show()
+    plt.show()
