aea9fd925dd71f85beede067e3a8992847d41ed4
clara.bayley <yoctoyotta1024@yahoo.com>
2024-06-16 09:13:39 +0200

    feat: new files for breakup executables

---
+++ examples/boxmodelcollisions/testikstraub/src/main_testikstraubcolls.cpp
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
+ *
+ * ----- CLEO -----
+ * File: main_testikstraubcolls.cpp
+ * Project: src
+ * Created Date: Sunday 16th June 2024
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Sunday 16th June 2024
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * runs the CLEO super-droplet model (SDM) for 0-D box model with coalescence, rebound and
+ * breakup with glag decided based on section 4 of Testik et al. 2011 (figure 12) as well
+ * as coalescence efficiency from Straub et al. 2010.
+ * After make/compiling, execute for example via:
+ * ./src/testikstraubcolls ../src/config/config.yaml
+ */
+
+#include <Kokkos_Core.hpp>
+#include <cmath>
+#include <concepts>
+#include <iostream>
+#include <stdexcept>
+#include <string_view>
+
+#include "cartesiandomain/cartesianmaps.hpp"
+#include "cartesiandomain/cartesianmotion.hpp"
+#include "cartesiandomain/createcartesianmaps.hpp"
+#include "cartesiandomain/null_boundary_conditions.hpp"
+#include "coupldyn_null/nulldynamics.hpp"
+#include "coupldyn_null/nulldyncomms.hpp"
+#include "gridboxes/gridboxmaps.hpp"
+#include "initialise/config.hpp"
+#include "initialise/init_all_supers_from_binary.hpp"
+#include "initialise/initgbxsnull.hpp"
+#include "initialise/initialconditions.hpp"
+#include "initialise/timesteps.hpp"
+#include "observers/gbxindex_observer.hpp"
+#include "observers/observers.hpp"
+#include "observers/streamout_observer.hpp"
+#include "observers/superdrops_observer.hpp"
+#include "observers/time_observer.hpp"
+#include "runcleo/coupleddynamics.hpp"
+#include "runcleo/couplingcomms.hpp"
+#include "runcleo/runcleo.hpp"
+#include "runcleo/sdmmethods.hpp"
+// #include "superdrops/collisions/coalescence.hpp"
+// #include "superdrops/collisions/lowlistprob.hpp" WIP
+#include "superdrops/microphysicalprocess.hpp"
+#include "superdrops/motion.hpp"
+#include "zarr/dataset.hpp"
+#include "zarr/fsstore.hpp"
+
+inline InitialConditions auto create_initconds(const Config &config) {
+  const InitAllSupersFromBinary initsupers(config.get_initsupersfrombinary());
+  const InitGbxsNull initgbxs(config.get_ngbxs());
+
+  return InitConds(initsupers, initgbxs);
+}
+
+inline GridboxMaps auto create_gbxmaps(const Config &config) {
+  const auto gbxmaps = create_cartesian_maps(config.get_ngbxs(), config.get_nspacedims(),
+                                             config.get_grid_filename());
+  return gbxmaps;
+}
+
+inline auto create_movement(const CartesianMaps &gbxmaps) {
+  const Motion<CartesianMaps> auto motion = NullMotion{};
+  const auto boundary_conditions = NullBoundaryConditions{};
+
+  return MoveSupersInDomain(gbxmaps, motion, boundary_conditions);
+}
+
+inline MicrophysicalProcess auto create_microphysics(const Config &config,
+                                                     const Timesteps &tsteps) {
+  // WIP (see includes too)
+  return colls;
+}
+
+template <typename Store>
+inline Observer auto create_superdrops_observer(const unsigned int interval,
+                                                Dataset<Store> &dataset, const int maxchunk) {
+  CollectDataForDataset<Store> auto sdid = CollectSdId(dataset, maxchunk);
+  CollectDataForDataset<Store> auto xi = CollectXi(dataset, maxchunk);
+  CollectDataForDataset<Store> auto radius = CollectRadius(dataset, maxchunk);
+  CollectDataForDataset<Store> auto msol = CollectMsol(dataset, maxchunk);
+
+  const auto collect_sddata = msol >> radius >> xi >> sdid;
+  return SuperdropsObserver(interval, dataset, maxchunk, collect_sddata);
+}
+
+template <typename Store>
+inline Observer auto create_observer(const Config &config, const Timesteps &tsteps,
+                                     Dataset<Store> &dataset) {
+  const auto obsstep = tsteps.get_obsstep();
+  const auto maxchunk = config.get_maxchunk();
+
+  const Observer auto obs0 = StreamOutObserver(obsstep, &step2realtime);
+
+  const Observer auto obs1 = TimeObserver(obsstep, dataset, maxchunk, &step2dimlesstime);
+
+  const Observer auto obssd = create_superdrops_observer(obsstep, dataset, maxchunk);
+
+  return obssd >> obs1 >> obs0;
+}
+
+template <typename Store>
+inline auto create_sdm(const Config &config, const Timesteps &tsteps, Dataset<Store> &dataset) {
+  const auto couplstep = (unsigned int)tsteps.get_couplstep();
+  const GridboxMaps auto gbxmaps(create_gbxmaps(config));
+  const MicrophysicalProcess auto microphys(create_microphysics(config, tsteps));
+  const MoveSupersInDomain movesupers(create_movement(gbxmaps));
+  const Observer auto obs(create_observer(config, tsteps, dataset));
+
+  return SDMMethods(couplstep, gbxmaps, microphys, movesupers, obs);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    throw std::invalid_argument("configuration file(s) not specified");
+  }
+
+  Kokkos::Timer kokkostimer;
+
+  /* Read input parameters from configuration file(s) */
+  const std::filesystem::path config_filename(argv[1]);  // path to configuration file
+  const Config config(config_filename);
+  const Timesteps tsteps(config.get_timesteps());
+
+  /* Create Xarray dataset wit Zarr backend for writing output data to a store */
+  auto store = FSStore(config.get_zarrbasedir());
+  auto dataset = Dataset(store);
+
+  /* Create coupldyn solver and coupling between coupldyn and SDM */
+  const CoupledDynamics auto coupldyn = NullDynamics(tsteps.get_couplstep());
+  const CouplingComms<NullDynamics> auto comms = NullDynComms{};
+
+  /* Initial conditions for CLEO run */
+  const InitialConditions auto initconds = create_initconds(config);
+
+  /* Initialise Kokkos parallel environment */
+  Kokkos::initialize(argc, argv);
+  {
+    /* CLEO Super-Droplet Model (excluding coupled dynamics solver) */
+    const SDMMethods sdm(create_sdm(config, tsteps, dataset));
+
+    /* Run CLEO (SDM coupled to dynamics solver) */
+    const RunCLEO runcleo(sdm, coupldyn, comms);
+    runcleo(initconds, tsteps.get_t_end());
+  }
+  Kokkos::finalize();
+
+  const auto ttot = double{kokkostimer.seconds()};
+  std::cout << "-----\n Total Program Duration: " << ttot << "s \n-----\n";
+
+  return 0;
+}
