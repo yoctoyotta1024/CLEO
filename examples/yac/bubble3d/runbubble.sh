e3102fc96ca85e17c3fc63bd178ebe9776645a89
clara.bayley <yoctoyotta1024@yahoo.com>
2024-06-19 16:52:35 +0200

    refactor: work in progress getting python bindings and yac installation to work for bubble

--- examples/yac/bubble3d/runbubble.sh
+++ examples/yac/bubble3d/runbubble.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-### First add this to your ~/.spack/upstreams.yaml to get py-netcdf from commin's:
+### If you did this to your ~/.spack/upstreams.yaml to get py-netcdf from commin's:
+# then python=/sw/spack-levante/python-3.9.9-fwvsvi/bin/python # is the one loaded by py-netcdf4
 # upstreams:
   # community_spack:
   #   install_tree: /work/k20200/k202160/community-spack/install
   # system_installs:
   #  install_tree: /sw/spack-levante
 
-# Note: python=/sw/spack-levante/python-3.9.9-fwvsvi/bin/python # the one loaded by py-netcdf4
-
 cd /home/m/m300950/CLEO/build_yac
 
-# spack load py-netcdf4
-module load python3/2022.01-gcc-11.2.0
-source activate /work/mh1126/m300950/cleoenv
+module load python3/2022.01-gcc-11.2.0 # version of python must match the YAC python bindings
 module load openmpi/4.1.2-gcc-11.2.0 # same mpi as loaded for the build
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/sw/spack-levante/libfyaml-0.7.12-fvbhgo/lib
-export PYTHONPATH=/work/mh1126/m300950/yac/yac/python/:$PYTHONPATH
+export PYTHONPATH=/work/mh1126/m300950/yac/yac-v3.2.0/python/:$PYTHONPATH # path to python bindings
 
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 
-/work/mh1126/m300950/cleoenv/bin/mpiexec -n 1 /home/m/m300950/CLEO/build_yac/examples/yac/bubble3d/src/bubble3D \
+mpiexec -n 1 /home/m/m300950/CLEO/build_yac/examples/yac/bubble3d/src/bubble3D \
   /home/m/m300950/CLEO/examples/yac/bubble3d/src/config/bubble3d_config.yaml \
-  : -n 1 /work/mh1126/m300950/cleoenv/bin/python3 \
+  : -n 1 python \
   /home/m/m300950/CLEO/examples/yac/bubble3d/yac_icon_data_reader.py
