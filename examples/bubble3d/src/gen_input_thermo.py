f566239aaa6a00d4179d74383ae313b80155f38b
clara.bayley <yoctoyotta1024@yahoo.com>
2024-07-09 09:45:25 +0200

    chore: format header

--- examples/bubble3d/src/gen_input_thermo.py
+++ examples/bubble3d/src/gen_input_thermo.py
@@ -5,7 +5,7 @@ Copyright (c) 2024 MPI-M, Clara Bayley
 ----- CLEO -----
 File: gen_input_thermo.py
 Project: src
-Created Date: Tuesday 9th July 2024
+Created Date: Monday 25th March 2024
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
@@ -16,31 +16,9 @@ License: BSD 3-Clause "New" or "Revised" License
 https://opensource.org/licenses/BSD-3-Clause
 -----
 File Description:
-"""
-
-
-"""
-Copyright (c) 2024 MPI-M, Clara Bayley
-
-
------ CLEO -----
-File: gen_input_thermo.py
-Project: src
-Created Date: Monday 25th March 2024
-Author: Clara Bayley (CB)
-Additional Contributors:
------
-Last Modified: Wednesday 17th April 2024
-Modified By: CB
------
-License: BSD 3-Clause "New" or "Revised" License
-https://opensource.org/licenses/BSD-3-Clause
------
-File Description:
 Python functions used by bubble3d example to make thermo and wind fields
 for CLEO to run example with 3-D time-varying thermodynamics.
 """
-
 
 import sys
 import numpy as np
