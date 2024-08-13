d8180189cca27ccd27a19922eb21446a5e21a73b
clara.bayley <yoctoyotta1024@yahoo.com>
2024-08-13 17:15:36 +0200

    refactor: generalise temporary bubble test script

--- examples/bubble3d/run_bubble_tmp.sh
+++ examples/bubble3d/run_bubble_tmp.sh
@@ -12,48 +12,51 @@
   #  install_tree: /sw/spack-levante
 
 action=$1
+path2CLEO=${2:-${HOME}/CLEO}
+path2yac=${3:-/work/mh1126/m300950/yac}
+path2build=${4:-${HOME}/CLEO/build_bubble3d}
 
 if [ "${action}" == "build" ]
 then
-  mkdir /home/m/m300950/CLEO/build_bubble3d
-  mkdir /home/m/m300950/CLEO/build_bubble3d/bin
-  mkdir /home/m/m300950/CLEO/build_bubble3d/share
+  mkdir ${path2build}
+  mkdir ${path2build}/bin
+  mkdir ${path2build}/share
 
-  /home/m/m300950/CLEO/scripts/bash/build_cleo_openmp.sh \
-    /home/m/m300950/CLEO/ /home/m/m300950/CLEO/build_bubble3d true /work/mh1126/m300950/yac
+  ${path2CLEO}/scripts/bash/build_cleo_openmp.sh \
+    ${path2CLEO}/ ${path2build} true ${path2yac}
 
 elif [ "${action}" == "compile" ]
 then
-  cd /home/m/m300950/CLEO/build_bubble3d && make clean
-  /home/m/m300950/CLEO/scripts/bash/compile_cleo.sh \
-    /work/mh1126/m300950/cleoenv openmp /home/m/m300950/CLEO/build_bubble3d bubble3D
+  cd ${path2build} && make clean
+  ${path2CLEO}/scripts/bash/compile_cleo.sh \
+    /work/mh1126/m300950/cleoenv openmp ${path2build} bubble3D
 
 elif [ "${action}" == "inputfiles" ]
 then
   source activate /work/mh1126/m300950/cleoenv
-  /work/mh1126/m300950/cleoenv/bin/python /home/m/m300950/CLEO/examples/bubble3d/bubble3d_inputfiles.py \
-   /home/m/m300950/CLEO \
-   /home/m/m300950/CLEO/build_bubble3d \
-   /home/m/m300950/CLEO/examples/bubble3d/src/config/bubble3d_config.yaml \
-   /home/m/m300950/CLEO/build_bubble3d/share/bubble3d_dimlessGBxboundaries.dat \
-   /home/m/m300950/CLEO/build_bubble3d/share/bubble3d_dimlessSDsinit.dat 0
+  /work/mh1126/m300950/cleoenv/bin/python ${path2CLEO}/examples/bubble3d/bubble3d_inputfiles.py \
+   ${path2CLEO} \
+   ${path2build} \
+   ${path2CLEO}/examples/bubble3d/src/config/bubble3d_config.yaml \
+   ${path2build}/share/bubble3d_dimlessGBxboundaries.dat \
+   ${path2build}/share/bubble3d_dimlessSDsinit.dat 0
 
 elif [ "${action}" == "run" ]
 then
-  cd /home/m/m300950/CLEO/build_bubble3d
+  cd ${path2build}
 
   # module load python3/2022.01-gcc-11.2.0 # version of python must match the YAC python bindings
   module load openmpi/4.1.2-gcc-11.2.0 # same mpi as loaded for the build
   spack load py-netcdf4
 
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/sw/spack-levante/libfyaml-0.7.12-fvbhgo/lib
-  export PYTHONPATH=${PYTHONPATH}:/work/mh1126/m300950/yac/yac-v3.2.0/python # path to python bindings
+  export PYTHONPATH=${PYTHONPATH}:${path2yac}/yac-v3.2.0/python # path to python bindings
 
   export OMP_PROC_BIND=spread
   export OMP_PLACES=threads
 
-  mpiexec -n 1 /home/m/m300950/CLEO/build_bubble3d/examples/bubble3d/src/bubble3D \
-    /home/m/m300950/CLEO/examples/bubble3d/src/config/bubble3d_config.yaml \
+  mpiexec -n 1 ${path2build}/examples/bubble3d/src/bubble3D \
+    ${path2CLEO}/examples/bubble3d/src/config/bubble3d_config.yaml \
     : -n 1 python \
-    /home/m/m300950/CLEO/examples/bubble3d/yac_bubble_data_reader.py
+    ${path2CLEO}/examples/bubble3d/yac_bubble_data_reader.py
 fi
