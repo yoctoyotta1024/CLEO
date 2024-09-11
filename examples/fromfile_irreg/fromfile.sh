3b05380b107307c9f371da408fd8ff76359d39cc
clara.bayley <yoctoyotta1024@yahoo.com>
2024-09-11 22:27:16 +0200

    feat: new example for irregular grid version of fromfile example (for MPI devlopment)

---
+++ examples/fromfile_irreg/fromfile.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --job-name=fromfile
+#SBATCH --partition=compute
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --mem=30G
+#SBATCH --time=00:05:00
+#SBATCH --mail-user=clara.bayley@mpimet.mpg.de
+#SBATCH --mail-type=FAIL
+#SBATCH --account=mh1126
+#SBATCH --output=./fromfile_out.%j.out
+#SBATCH --error=./fromfile_err.%j.out
+
+### ---------------------------------------------------- ###
+### ------------------ Input Parameters ---------------- ###
+### ------ You MUST edit these lines to set your ------- ###
+### ---- build type, directories, the executable(s) ---- ###
+### -------- to compile, and your python script -------- ###
+### ---------------------------------------------------- ###
+buildtype="openmp"
+path2CLEO=${HOME}/CLEO/
+path2build=${HOME}/CLEO/build_fromfile/
+enableyac=false
+executables="fromfile"
+
+pythonscript=${path2CLEO}/examples/fromfile/fromfile.py
+configfile=${path2CLEO}/examples/fromfile/src/config/fromfile_config.yaml
+script_args="${configfile}"
+### ---------------------------------------------------- ###
+### ---------------------------------------------------- ###
+### ---------------------------------------------------- ###
+
+### ---------- build, compile and run example ---------- ###
+${path2CLEO}/examples/run_example.sh \
+  ${buildtype} ${path2CLEO} ${path2build} ${enableyac} \
+  "${executables}" ${pythonscript} "${script_args}"
+### ---------------------------------------------------- ###
