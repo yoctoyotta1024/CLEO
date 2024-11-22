aab95ef25bdf5d7712b092ad7e3e5bd89014e770
clara.bayley <yoctoyotta1024@yahoo.com>
2024-11-22 10:24:34 +0100

    fix: update levante mamba env path

--- scripts/inputfiles.sh
+++ scripts/inputfiles.sh
@@ -16,7 +16,7 @@
 ### ----  and paths for CLEO and build directories  ---- ###
 path2CLEO=${2:-${HOME}/CLEO}
 path2scripts=${path2CLEO}/scripts
-condaenv=/work/bm1183/m300950/mambaenvs/cleoenv
+condaenv=/work/bm1183/m300950/bin/envs/cleoenv
 python=${condaenv}/bin/python
 ### ---------------------------------------------------- ###
 
