d0599536da5d54970e07ec4c069da8338a122609
clara.bayley <yoctoyotta1024@yahoo.com>
2024-12-06 09:29:54 +0100

    refactor: update install yac bash script to make python bindings correctly

--- scripts/bash/install_yac.sh
+++ scripts/bash/install_yac.sh
@@ -16,20 +16,27 @@
 ### gcc 11.2.0 compiler with openmpi 4.1.2 on Levante
 ### ------------------------------------------------------- ###
 
+set -ex
+
 root4YAC=$1 # absolute path for YAC and YAXT installations
 
-yaxt_source=https://swprojects.dkrz.de/redmine/attachments/download/534/yaxt-0.11.1.tar.gz
-yaxt_version=yaxt-0.11.1 # must match yaxt_source
+yaxt_tag=0.11.1
+yaxt_version=yaxt-${yaxt_tag}
+yaxt_release_tag=release-${yaxt_tag}
+yaxt_source=https://gitlab.dkrz.de/dkrz-sw/yaxt/-/archive/$yaxt_release_tag/$yaxt_version.tar.gz
 
-yac_source=https://gitlab.dkrz.de/dkrz-sw/yac/-/archive/v3.2.0/yac-v3.2.0.tar.gz
-yac_version=yac-v3.2.0 # must match yac_source
+yac_tag=v3.5.2
+yac_version=yac_$yac_tag
+yac_source=https://gitlab.dkrz.de/dkrz-sw/yac/-/archive/$yac_tag/$yac_version.tar.gz
 
 gcc=gcc/11.2.0-gcc-11.2.0
 openmpi=openmpi/4.1.2-gcc-11.2.0
 netcdf=netcdf-c/4.8.1-openmpi-4.1.2-gcc-11.2.0 # must match gcc and openmpi
 netcdf_root=/sw/spack-levante/netcdf-c-4.8.1-6qheqr # must match with `module show ${netcdf}`
 fyaml_root=/sw/spack-levante/libfyaml-0.7.12-fvbhgo # must match with `spack location -i libfyaml``
+python=python@3.9.9%gcc@=11.2.0/fwv
 pycython=py-cython@0.29.33%gcc@=11.2.0/j7b4fa
+pympi4py=py-mpi4py@3.1.2%gcc@=11.2.0/hdi5yl6
 
 CC=/sw/spack-levante/openmpi-4.1.2-mnmady/bin/mpicc # must match gcc
 FC=/sw/spack-levante/openmpi-4.1.2-mnmady/bin/mpif90 # must match gcc
@@ -38,16 +45,22 @@ if [ "${root4YAC}" == "" ]
 then
   echo "Bad input, please specify absolute path for where you want to install YAC"
 else
+  mkdir ${root4YAC}
   module load ${gcc} ${openmpi} ${netcdf}
+  ### ----------------- load Python ------------------------ ###
+  spack load ${python}
   spack load ${pycython}
+  spack load ${pympi4py}
+  ### ------------------------------------------------------ ###
 
   ### --------------------- install YAXT ------------------- ###
-  cd ${root4YAC} && pwd
-  curl -s -L ${yaxt_source} | tar xvz
-  cd ${yaxt_version}
-  ./configure \
+  mkdir ${root4YAC}/${yaxt_version}
+  cd ${root4YAC}/${yaxt_version} &&  pwd
+  curl -s -L ${yaxt_source} | tar xvz --strip-components=1
+  mkdir build && cd build
+  ../configure \
     CC=${CC} FC=${FC} \
-    CFLAGS="-O0 -g -Wall -fPIC" \
+    CFLAGS="-O0 -g -Wall" \
     FCFLAGS="-O0 -g -Wall -cpp -fimplicit-none" \
     --without-regard-for-quality \
     --without-example-programs \
@@ -61,25 +74,27 @@ else
 
   ## --------------------- install YAC -------------------- ###
   # python bindings made in yac_version directory (note this is not yac directory!)
-  cd ${root4YAC} && pwd
-  curl -s -L ${yac_source} | tar xvz
-  cd ${yac_version}
-  ./configure \
+  mkdir ${root4YAC}/${yac_version}
+  cd ${root4YAC}/${yac_version} && pwd
+  curl -s -L ${yac_source} | tar xvz --strip-components=1
+  mkdir build && cd build
+  ../configure \
     CC=${CC} FC=${FC} \
-    CFLAGS="-O0 -g -Wall -fPIC" \
+    CFLAGS="-O0 -g -Wall" \
     FCFLAGS="-O0 -g -Wall -cpp -fimplicit-none" \
     LDFLAGS="-lm" \
     --disable-mpi-checks \
     --with-yaxt-root=${root4YAC}/yaxt \
     --with-netcdf-root=${netcdf_root} \
     --with-fyaml-root=${fyaml_root} \
-    --enable-rpaths \
     --enable-python-bindings \
+    --enable-rpaths \
+    --with-pic \
     --prefix=${root4YAC}/yac
   make -j 8
-  make install || true
+  make install
 
-  mv ${root4YAC}/${yac_version}/python ${root4YAC}/yac/python
+  mv ${root4YAC}/${yac_version}/build/python ${root4YAC}/yac/
   cd ${root4YAC} && rm -rf ${yac_version}
   ### ------------------------------------------------------ ###
 fi
