d33e7ffb2de0087db3e034919a49b06eedc3d2a9
clara.bayley <yoctoyotta1024@yahoo.com>
2025-01-07 09:45:31 +0100

    refactor: add alternative bubble plot

--- examples/bubble3d/bubble3d_plotting.py
+++ examples/bubble3d/bubble3d_plotting.py
@@ -221,3 +221,119 @@ fig, axs = plot_2d_supers(xxh_km, zzh_km, wind_var, t2plts, sample, cmap, vlims)
 savename = path2build / "bin" / "bubble_motion.png"
 fig.savefig(savename, dpi=400, bbox_inches="tight", facecolor="w", format="png")
 print("Figure .png saved as: " + str(savename))
+
+
+# %%
+maxid = np.amax(sddata.sdId[0])
+nsample = 500
+sample_attrs = ["coord3", "coord1", "radius"]
+sample = sdtracing.attrs_for_superdroplets_sample(
+    sddata, sample_attrs, ndrops2sample=nsample, minid=0, maxid=maxid
+)
+
+# %%
+wind_var = winds["wvel"]
+label = "vertical velocity /m/s"
+cmap = "PRGn"
+vlims = [-10.0, 10.0]
+
+
+# %%
+def plot_2d_supers_contours(xxh_km, zzh_km, wind_var, t2plts, sample, cmap, vlims):
+    nplots = len(t2plts)
+    fig, axes = plt.subplots(
+        nrows=1,
+        ncols=nplots + 4,
+        figsize=(18, 6),
+        constrained_layout=True,
+        width_ratios=[24] * nplots + [0.5, 1, 0.5, 1],
+    )
+    axs = axes[:-4]
+    caxs = [axes[-3], axes[-1]]
+    axes[-4].axis("off")
+    axes[-2].axis("off")
+    fontsize = 18
+
+    ## wind field contour plot
+    cmap = plt.get_cmap(cmap, 20)
+    norm = mcolors.Normalize(vmin=vlims[0], vmax=vlims[1])
+
+    ## superdroplets scatter plot
+    size = sample["radius"] * 5
+    coord3_km = sample["coord3"] / 1000
+    coord1_km = sample["coord1"] / 1000 - 50
+
+    tidx = np.argmin(abs(time.mins - t2plts[-1]))
+    c = np.repeat([time.mins[: tidx + 1]], coord3_km.shape[1], axis=0).T
+    sd_norm = mcolors.Normalize(vmin=time.mins[0], vmax=time.mins[tidx + 1])
+    sd_cmap = "plasma"
+
+    for m in range(0, nplots):
+        tidx = np.argmin(abs(time.mins - t2plts[m]))
+        axs[m].contourf(xxh_km, zzh_km, wind_var[tidx, 0, :, :], cmap=cmap, norm=norm)
+
+        sd_x = coord1_km[: tidx + 1, :]
+        sd_y = coord3_km[: tidx + 1, :]
+        axs[m].scatter(
+            sd_x,
+            sd_y,
+            s=size[: tidx + 1, :],
+            c=c[: tidx + 1, :],
+            cmap=sd_cmap,
+            norm=sd_norm,
+        )
+        axs[m].set_title(
+            "{:.0f} mins".format(time.mins[tidx]), fontsize=fontsize, y=1.025
+        )
+        axs[m].set_xlim([-17, 13])
+        axs[m].set_ylim([0, 2.5])
+        axs[m].spines["top"].set_visible(False)
+        axs[m].spines["right"].set_visible(False)
+
+    for ax in axs:
+        ax.set_yticks([0, 1.25, 2.5])
+        ax.set_yticklabels([0, "", 2.5], fontsize=fontsize)
+        ax.set_xticks([-17, 0, 13])
+        ax.set_xticklabels([-15, "", 15], fontsize=fontsize)
+    for ax in axs[1:]:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+
+    axs[0].set_xlabel("x /km", fontsize=fontsize, labelpad=-12)
+    axs[0].set_ylabel("z /km", fontsize=fontsize, labelpad=-8)
+    fig.suptitle(
+        "Superdroplet motion in up/downdrafts modelled by CLEO-YAC-ICON",
+        fontsize=fontsize + 2,
+        y=1.075,
+    )
+    cbar = fig.colorbar(
+        ScalarMappable(norm=norm, cmap=cmap), cax=caxs[0], orientation="vertical"
+    )
+    cbar.set_label(label=label, fontsize=fontsize, labelpad=-8)
+    ticks = [-10, 0, 10]
+    cbar.set_ticks(ticks=ticks)
+    cbar.set_ticklabels(ticklabels=ticks, fontsize=fontsize)
+
+    sd_cbar = fig.colorbar(
+        ScalarMappable(norm=sd_norm, cmap=sd_cmap),
+        cax=caxs[1],
+        orientation="vertical",
+    )
+    sd_cbar.set_label(
+        label="time of superdroplet location /mins", fontsize=fontsize, labelpad=-8
+    )
+    sd_ticks = [0, 20, 40, 60, 80]
+    sd_ticklabels = [0, "", "", "", 80]
+    sd_cbar.set_ticks(ticks=sd_ticks)
+    sd_cbar.set_ticklabels(ticklabels=sd_ticklabels, fontsize=fontsize)
+
+    return fig, axs
+
+
+t2plts = [20, 50, 80]  # mins
+fig, axs = plot_2d_supers_contours(
+    xxh_km, zzh_km, wind_var, t2plts, sample, cmap, vlims
+)
+savename = path2build / "bin" / "bubble_motion_v2.png"
+fig.savefig(savename, dpi=400, bbox_inches="tight", facecolor="w", format="png")
+print("Figure .png saved as: " + str(savename))
