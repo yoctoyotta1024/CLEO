ad01b1b36091d2a3d59dcadfdfa26575018c072a
clara.bayley <yoctoyotta1024@yahoo.com>
2025-03-06 16:05:17 +0100

    refactor: split shuffle implementations into .cpp file
    docs: update info on shuffling

---
+++ libs/superdrops/collisions/shuffle.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024 MPI-M, Clara Bayley
+ *
+ * ----- CLEO -----
+ * File: shuffle.cpp
+ * Project: collisions
+ * Created Date: Thursday 6th March 2025
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 6th March 2025
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * Implementaiton file for functions for Kokkos compatibile thread-safe
+ * versions of C++ Fisher-Yates serial shuffling algorithm.
+ */
+
+#include "./shuffle.hpp"
+
+/**
+ * @brief Randomly shuffles the order of super-droplet objects in a view
+ * using Fisher-Yates algorithm
+ *
+ * Thread-Safe Kokkos compatible version of Fisher-Yates shuffling algorithm for
+ * super-droplets. Shuffling is done in serial (slow!) on single thread per team
+ *
+ * Uses only one member of a Kokkos team to randomly shuffle the order of super-droplet objects
+ * in the 'supers' view using Kokkos compatible rewrite of C++ standard library Fisher-Yates
+ * shuffling algorithm. Afterwards synchronizes the team and returns the view of
+ * shuffled super-droplets. Uses Kokkos thread safe random number generator from pool.
+ *
+ * @param team_member The Kokkos team member.
+ * @param supers The view of superdroplets to shuffle.
+ * @param genpool The random number generator pool.
+ * @return The shuffled view of superdroplets.
+ */
+KOKKOS_FUNCTION viewd_supers shuffle_supers(const TeamMember& team_member,
+                                            const viewd_supers supers,
+                                            const GenRandomPool genpool) {
+  namespace KE = Kokkos::Experimental;
+
+  Kokkos::single(Kokkos::PerTeam(team_member), [=]() {
+    const auto first = KE::begin(supers);
+    const auto dist = KE::distance(first, KE::end(supers) - 1);  // distance to last elemnt from 1st
+    URBG<ExecSpace> urbg{genpool.get_state()};
+    fisher_yates_shuffle(urbg, first, dist);
+    genpool.free_state(urbg.gen);
+  });
+  team_member.team_barrier();  // synchronise threads
+
+  return supers;
+}
--- libs/superdrops/collisions/shuffle.hpp
+++ libs/superdrops/collisions/shuffle.hpp
@@ -15,11 +15,8 @@
  * https://opensource.org/licenses/BSD-3-Clause
  * -----
  * File Description:
- * Functions for Kokkos compatibile thread-safe versions of C++ Fisher-Yates serial shuffling
- * algorithm, and of MergeShuffle parallelsised shuffling algorithm. MergeShuffle comes from
- * "A Very Fast, Parallel Random Permutation Algorithm", Axel Bacher, Olivier Bodini,
- * Alexandros Hollender, and Jérémie Lumbroso, August 14, 2015. See also their code
- * repository: https://github.com/axel-bacher/mergeshuffle)
+ * Implementaiton file for functions for Kokkos compatibile thread-safe
+ * versions of C++ Fisher-Yates serial shuffling algorithm.
  */
 
 #ifndef LIBS_SUPERDROPS_COLLISIONS_SHUFFLE_HPP_
@@ -33,18 +30,36 @@
 #include "./urbg.hpp"
 
 /**
+ * @brief Randomly shuffles the order of super-droplet objects in a view
+ * using Fisher-Yates algorithm
+ *
+ * Thread-Safe Kokkos compatible version of Fisher-Yates shuffling algorithm for
+ * super-droplets. Shuffling is done in serial (slow!) on single thread per team
+ *
+ * Uses only one member of a Kokkos team to randomly shuffle the order of super-droplet objects
+ * in the 'supers' view using Kokkos compatible rewrite of C++ standard library Fisher-Yates
+ * shuffling algorithm. Afterwards synchronizes the team and returns the view of
+ * shuffled super-droplets. Uses Kokkos thread safe random number generator from pool.
+ *
+ * @param team_member The Kokkos team member.
+ * @param supers The view of superdroplets to shuffle.
+ * @param genpool The random number generator pool.
+ * @return The shuffled view of superdroplets.
+ */
+KOKKOS_FUNCTION viewd_supers shuffle_supers(const TeamMember& team_member,
+                                            const viewd_supers supers, const GenRandomPool genpool);
+/**
  * @brief Swaps the values of two super-droplets.
  *
  * Equivalent to C++98 std::swap but works on device as well as host (gpu compatible).
  *
- * _Note:_ Involves a copy construction and two assignment operations, which may not be efficient
- * if Superdrop class stores large quantities of data.
+ * _Note:_ Involves a copy construction and two assignment operations, which may not be
+ * efficient if Superdrop class stores large quantities of data.
  *
  * @param a The first super-droplet.
  * @param b The second super-droplet.
  */
-KOKKOS_INLINE_FUNCTION
-void device_swap(Superdrop& a, Superdrop& b) {
+KOKKOS_INLINE_FUNCTION void device_swap(Superdrop& a, Superdrop& b) {
   Superdrop c(a);
   a = b;
   b = c;
@@ -53,55 +68,26 @@ void device_swap(Superdrop& a, Superdrop& b) {
 /**
  * @brief Shuffles the order of super-droplets in a view.
  *
- * Randomly shuffles the order of super-droplets using the
- * URBG (Uniform Random Bit Generator) struct (on device).
+ * Randomly shuffles the order of super-droplets using the URBG
+ * (Uniform Random Bit Generator) struct (on device). Supers included in
+ * shuffle are from iterators in range [first, first+dist] (inclusive), e.g.
+ * if first points to the 5th superdroplet and dist=2, then the 5th, 6th and
+ * 7th superdroplets will be shuffled amongst each other.
  *
  * @tparam DeviceType The Kokkos device type.
  * @param supers The view of super-droplets to shuffle.
  * @param urbg The random number generator.
+ * @param first iterator/pointer to first element in supers to shuffle
+ * @param dist number of elements (including first) to shuffle
  * @return The shuffled view of super-droplets.
  */
 template <class DeviceType>
-KOKKOS_INLINE_FUNCTION viewd_supers shuffle_supers(const viewd_supers supers,
-                                                   URBG<DeviceType> urbg) {
-  namespace KE = Kokkos::Experimental;
-
-  const auto first = KE::begin(supers);
-  const auto dist = KE::distance(first, KE::end(supers) - 1);  // distance to last element from 1st
-
+KOKKOS_INLINE_FUNCTION void fisher_yates_shuffle(URBG<DeviceType> urbg, const auto first,
+                                                 const auto dist) {
   for (auto iter(dist); iter > 0; --iter) {
     const auto randiter = urbg(0, iter + 1);  // random uint64_t equidistributed between [0, i]
     device_swap(*(first + iter), *(first + randiter));
   }
-
-  return supers;
-}
-
-/**
- * @brief Randomly shuffles the order of super-droplet objects in a view using a single thread in
- * a Kokkos team (i.e. a single team member).
- *
- * Uses only one member of a Kokkos team to randomly shuffle the order of super-droplet objects
- * in the 'supers' view. Then synchronizes the team and returns the view of shuffled super-droplets.
- * Uses Kokkos thread safe random number generator from pool.
- *
- * @param team_member The Kokkos team member.
- * @param supers The view of superdroplets to shuffle.
- * @param genpool The random number generator pool.
- * @return The shuffled view of superdroplets.
- */
-KOKKOS_INLINE_FUNCTION viewd_supers shuffle_supers(const TeamMember& team_member,
-                                                   const viewd_supers supers,
-                                                   const GenRandomPool genpool) {
-  Kokkos::single(Kokkos::PerTeam(team_member), [=]() {
-    URBG<ExecSpace> urbg{genpool.get_state()};
-    shuffle_supers(supers, urbg);
-    genpool.free_state(urbg.gen);
-  });
-
-  team_member.team_barrier();  // synchronise threads
-
-  return supers;
 }
 
 #endif  // LIBS_SUPERDROPS_COLLISIONS_SHUFFLE_HPP_
