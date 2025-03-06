ac3ac15da2c0d220cccfba9562499257afdfb833
clara.bayley <yoctoyotta1024@yahoo.com>
2025-03-06 18:13:20 +0100

    refactor: creating merge shuffle parallel algorithm

--- libs/superdrops/collisions/shuffle.cpp
+++ libs/superdrops/collisions/shuffle.cpp
@@ -15,35 +15,94 @@
  * https://opensource.org/licenses/BSD-3-Clause
  * -----
  * File Description:
- * Implementaiton file for functions for Kokkos compatibile thread-safe versions of C++
- * Fisher-Yates serial shuffling algorithm, and of MergeShuffle parallelsised shuffling
- * algorithm. MergeShuffle comes from "A Very Fast, Parallel Random Permutation Algorithm",
- * Axel Bacher, Olivier Bodini, Alexandros Hollender, and Jérémie Lumbroso, August 14, 2015.
- * See also their code repository: https://github.com/axel-bacher/mergeshuffle)
+ * Implementaiton file for functions for Kokkos compatibile thread-safe versions
+ * of C++ Fisher-Yates serial shuffling algorithm, and of MergeShuffle
+ * parallelsised shuffling algorithm. MergeShuffle comes from "A Very Fast,
+ * Parallel Random Permutation Algorithm", Axel Bacher, Olivier Bodini,
+ * Alexandros Hollender, and Jérémie Lumbroso, August 14, 2015. See also their
+ * code repository: https://github.com/axel-bacher/mergeshuffle)
  */
 
 #include "./shuffle.hpp"
 
+/*
+ * C++ and Kokkos compatible version of ```shuffle(unsigned int *t, unsigned int
+ * n)``` from "A Very Fast, Parallel Random Permutation Algorithm", Axel Bacher,
+ * Olivier Bodini, Alexandros Hollender, and Jérémie Lumbroso, August 14, 2015.
+ * see: https://github.com/axel-bacher/mergeshuffle/blob/master/merge_omp.c
+ */
+KOKKOS_FUNCTION viewd_supers merge_shuffle_supers(const TeamMember &team_member,
+                                                  const viewd_supers supers,
+                                                  const GenRandomPool genpool) {
+  namespace KE = Kokkos::Experimental;
+
+  Kokkos::single(Kokkos::PerTeam(team_member), [=]() {
+    constexpr unsigned int cutoff = 1024;  // below, c is largest integer < log_2(nn / cutoff)
+    /**< determines smallest number of superdroplets for which merge sort
+     * resorts to fisher-yates */
+
+    const size_t nn = supers.extent(0);  // total number of superdroplets to shuffle
+    unsigned int c = 0;                  // length of blocks for fisher-yates = (n/2^c) +/- 1
+    while ((nn >> c) > cutoff) c++;      // c is largest number such that (n/2^c > cutoff)
+    unsigned int q = 1 << c;             // number of blocks, q = 2^c
+
+    const auto fisher_yates = FisherYatesShuffle{};
+    for (unsigned int i = 0; i < q; i++) {
+      const size_t j = (nn * i) >> c;        // (nn+i)/2^c
+      const size_t k = (nn * (i + 1)) >> c;  // (nn+nn*i)/2^c
+
+      const auto first = KE::begin(supers);
+      const auto block_first = first + j;  // distance from 1st to j'th element
+      const auto block_dist =
+          KE::distance(block_first, first + k - 1);  // distance from 1st to k'th
+
+      URBG<ExecSpace> urbg{genpool.get_state()};
+      fisher_yates.shuffle_supers(urbg, supers, block_first, block_dist);
+      genpool.free_state(urbg.gen);
+    }
+
+    for (unsigned int p = 1; p < q; p += p) {
+      for (unsigned int _i = 0; _i * (2 * p) < q; _i++) {
+        const auto i = _i * 2 * p;
+        const size_t j = (nn * i) >> c;
+        const size_t k = (nn * (i + p)) >> c;
+        const size_t l = (nn * (i + 2 * p)) >> c;
+        URBG<ExecSpace> urbg{genpool.get_state()};
+        merge_blocks(urbg, supers, j, k, l);
+        genpool.free_state(urbg.gen);
+      }
+    }
+  });
+  team_member.team_barrier();  // synchronise threads
+
+  return supers;
+}
+
 /**
- * @brief Randomly shuffles the order of super-droplet objects in a view using a single thread in
- * a Kokkos team (i.e. a single team member).
+ * @brief Randomly shuffles the order of super-droplet objects in a view using a
+ * single thread in a Kokkos team (i.e. a single team member).
  *
- * Uses only one member of a Kokkos team to randomly shuffle the order of super-droplet objects
- * in the 'supers' view using Kokkos compatible rewrite of C++ standard library Fisher-Yates
- * shuffling algorithm. Afterwards synchronizes the team and returns the view of
- * shuffled super-droplets. Uses Kokkos thread safe random number generator from pool.
+ * Uses only one member of a Kokkos team to randomly shuffle the order of
+ * super-droplet objects in the 'supers' view using Kokkos compatible rewrite of
+ * C++ standard library Fisher-Yates shuffling algorithm. Afterwards
+ * synchronizes the team and returns the view of shuffled super-droplets. Uses
+ * Kokkos thread safe random number generator from pool.
  *
  * @param team_member The Kokkos team member.
  * @param supers The view of superdroplets to shuffle.
  * @param genpool The random number generator pool.
  * @return The shuffled view of superdroplets.
  */
-KOKKOS_FUNCTION viewd_supers FisherYatesShuffle::operator()(const TeamMember& team_member,
+KOKKOS_FUNCTION viewd_supers FisherYatesShuffle::operator()(const TeamMember &team_member,
                                                             const viewd_supers supers,
                                                             const GenRandomPool genpool) const {
+  namespace KE = Kokkos::Experimental;
+
   Kokkos::single(Kokkos::PerTeam(team_member), [=, *this]() {
+    const auto first = KE::begin(supers);
+    const auto dist = KE::distance(first, KE::end(supers) - 1);  // distance to last elemnt from 1st
     URBG<ExecSpace> urbg{genpool.get_state()};
-    shuffle_supers(supers, urbg);
+    shuffle_supers(urbg, supers, first, dist);
     genpool.free_state(urbg.gen);
   });
   team_member.team_barrier();  // synchronise threads
