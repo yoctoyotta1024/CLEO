668794b5f2d20cd3f9985321c15f8f45273542f2
clara.bayley <yoctoyotta1024@yahoo.com>
2025-05-28 13:45:01 +0200

    refactor: updated to communicator including YAC dependency in CMake

--- libs/configuration/communicator.cpp
+++ libs/configuration/communicator.cpp
@@ -22,11 +22,39 @@
 
 #include "configuration/communicator.hpp"
 
+extern "C" {
+#include "yac.h"
+}
+
+int init_communicator::yac_comp_id = -1;
+MPI_Comm init_communicator::comm = NULL;
+
 init_communicator::init_communicator(const Config &config) {
-  yac_present = std::isnan(config.get_yac_dynamics().lower_longitude);
-  if (yac_present) {
+  if (!(std::isnan(config.get_yac_dynamics().lower_longitude))) {
     std::cout << "yac is present";
+    // -- YAC initialization and calendar definitions ---
+    yac_cinit();
+    // --- Component definition ---
+    std::string component_name = "cleo";
+    yac_cdef_comp(component_name.c_str(), &init_communicator::yac_comp_id);
+    yac_cget_comp_comm(init_communicator::yac_comp_id, &comm);
   } else {
-    std::cout << "yac is not present";
+    std::cout << "yac is not present" << yac_present;
+    MPI_Init(NULL, NULL);
+    comm = MPI_COMM_WORLD;
+    int comm_size;
+    MPI_Comm_size(comm, &comm_size);
+    if (comm_size > 1) {
+      std::cout << "ERROR: The current example is not prepared"
+                << " to be run with more than one MPI process" << std::endl;
+      MPI_Abort(comm, 1);
+    }
   }
+};
+
+MPI_Comm init_communicator::get_communicator() {
+  if (init_communicator::comm == MPI_COMM_NULL) {
+    // call MPI_Abort and print msg
+  }
+  return comm;
 };
