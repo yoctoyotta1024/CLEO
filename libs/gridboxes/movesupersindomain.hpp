5fe52d2a6737d0ddd33b2053f282e151ebab3c55
clara.bayley <yoctoyotta1024@yahoo.com>
2025-05-28 13:15:57 +0200

    refactor: use MPI communicator instead of direct MPI calls continued
    is continuation of commit: 0bcc8c5093a07219ab789ae8a6e7aee0d03ccbf0

--- libs/gridboxes/movesupersindomain.hpp
+++ libs/gridboxes/movesupersindomain.hpp
@@ -225,11 +225,6 @@ class MoveSupersInDomain {
   */
   SupersInDomain move_superdrops_in_domain(const unsigned int t_sdm, const GbxMaps &gbxmaps,
                                            viewd_gbx d_gbxs, SupersInDomain &allsupers) const {
-    int my_rank;
-    MPI_Comm comm;
-    comm = init_communicator::get_communicator();
-    MPI_Comm_rank(comm, &my_rank);
-
     /* steps (1 - 2) */
     move_supers_in_gridboxes(gbxmaps, d_gbxs, allsupers.domain_supers());
 
