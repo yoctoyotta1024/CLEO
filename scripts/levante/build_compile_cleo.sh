21428902ea59b72a83cef8d9e33f4cb601be2fe9
clara.bayley <yoctoyotta1024@yahoo.com>
2025-06-05 11:51:19 +0200

    perf: split long lines over multiple

--- scripts/levante/build_compile_cleo.sh
+++ scripts/levante/build_compile_cleo.sh
@@ -42,7 +42,8 @@ then
   exit 1
 fi
 source ${path2CLEO}/scripts/levante/bash/src/check_inputs.sh
-check_args_not_empty "${buildtype}" "${compilername}" "${enabledebug}" "${path2CLEO}" "${path2build}" "${enableyac}"
+check_args_not_empty "${buildtype}" "${path2CLEO}" "${path2build}"
+check_args_not_empty "${compilername}" "${enabledebug}" "${enableyac}"
 ### ---------------------------------------------------- ###
 
 ### ----------------- export inputs -------------------- ###
