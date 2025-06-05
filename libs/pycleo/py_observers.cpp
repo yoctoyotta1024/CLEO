62b19227d3d4d528ca10d5867a2076e7b5236752
clara.bayley <yoctoyotta1024@yahoo.com>
2025-06-05 18:07:29 +0200

    feat: create first bindings for observers

---
+++ libs/pycleo/py_observers.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025 MPI-M, Clara Bayley
+ *
+ *
+ * ----- CLEO -----
+ * File: py_observers.cpp
+ * Project: pycleo
+ * Created Date: Thursday 5th June 2025
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Thursday 5th June 2025
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * Functionality for creating python bindings to various different CLEO's Observers instantiations
+ */
+
+#include "./py_observers.hpp"
+
+void pyObservers_null(py::module &m) {
+  py::class_<pyca::obs_null>(m, "NullObserver")
+      .def(py::init());
+}
---
+++ libs/pycleo/py_observers.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2025 MPI-M, Clara Bayley
+ *
+ *
+ * ----- CLEO -----
+ * File: py_observers.hpp
+ * Project: pycleo
+ * Created Date: Thursday 5th June 2025
+ * Author: Clara Bayley (CB)
+ * Additional Contributors:
+ * -----
+ * Last Modified: Friday 6th June 2025
+ * Modified By: CB
+ * -----
+ * License: BSD 3-Clause "New" or "Revised" License
+ * https://opensource.org/licenses/BSD-3-Clause
+ * -----
+ * File Description:
+ * Python bindings to various different CLEO's Observers instantiations
+ */
+
+#ifndef LIBS_PYCLEO_PY_OBSERVERS_HPP_
+#define LIBS_PYCLEO_PY_OBSERVERS_HPP_
+
+#include <pybind11/pybind11.h>
+
+#include "./pycleo_aliases.hpp"
+#include "observers/observers.hpp"
+
+namespace py = pybind11;
+namespace pyca = pycleo_aliases;
+
+void pyObservers_null(py::module &m);
+
+#endif  // LIBS_PYCLEO_PY_OBSERVERS_HPP_
