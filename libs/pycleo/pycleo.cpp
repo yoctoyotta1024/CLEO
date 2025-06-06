7f9301e722a1b483c6e0d56a8e2a5e452f583208
clara.bayley <yoctoyotta1024@yahoo.com>
2025-06-06 11:50:12 +0200

    refactor: kokkos init and finalise via pycleo

--- libs/pycleo/pycleo.cpp
+++ libs/pycleo/pycleo.cpp
@@ -9,7 +9,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Thursday 5th June 2025
+ * Last Modified: Friday 6th June 2025
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -24,4 +24,19 @@
 int test_python_bindings(const int i, const int j) {
   std::cout << "Hello World\n";
   return i + j;
+}
+
+/* assumes MPI was initialised aleady, e.g. in python with ```from mpi4py import MPI``` */
+void pycleo_initialize() {
+  int comm_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
+  if (comm_size > 1) {
+    std::cout << "ERROR: The current example is not prepared"
+              << " to be run with more than one MPI process" << std::endl;
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  // Kokkos::initialize(config.get_kokkos_initialization_settings()); #WIP
+  Kokkos::initialize();
+  Kokkos::print_configuration(std::cout);
 }
