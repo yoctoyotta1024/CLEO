51b8ca5233644eefd6f4c4aa0e5d1f4c908f16f8
clara.bayley <yoctoyotta1024@yahoo.com>
2025-06-19 20:01:36 +0200

    chore: make comparable to old branch

--- libs/zarr/collective_dataset.hpp
+++ libs/zarr/collective_dataset.hpp
@@ -9,7 +9,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors: Wilton Jaciel Loch
  * -----
- * Last Modified: Thursday 19th June 2025
+ * Last Modified: Wednesday 28th May 2025
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
