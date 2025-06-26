34ac7af6f26a25d11b35c09b8a47b58545cfdff3
clara.bayley <yoctoyotta1024@yahoo.com>
2025-06-26 12:01:36 +0200

    fix: prevent multiple kokkos init/finalize in python bindings with at exit call

--- libs/pycleo/pycleo.hpp
+++ libs/pycleo/pycleo.hpp
@@ -9,7 +9,7 @@
  * Author: Clara Bayley (CB)
  * Additional Contributors:
  * -----
- * Last Modified: Tuesday 24th June 2025
+ * Last Modified: Thursday 26th June 2025
  * Modified By: CB
  * -----
  * License: BSD 3-Clause "New" or "Revised" License
@@ -25,6 +25,7 @@
 #include <mpi.h>
 #include <pybind11/pybind11.h>
 
+#include <cstdlib>
 #include <iostream>
 
 #include "./py_cartesiandomain.hpp"
