601db8338386377196e9dab747c47310c9012284
clara.bayley <yoctoyotta1024@yahoo.com>
2025-07-21 09:29:08 +0200

    refactor: more figure plotting params into input file args

--- examples/fromfile_irreg/fromfile_irreg.py
+++ examples/fromfile_irreg/fromfile_irreg.py
@@ -9,7 +9,7 @@ Created Date: Wednesday 11th September 2024
 Author: Clara Bayley (CB)
 Additional Contributors:
 -----
-Last Modified: Wednesday 4th June 2025
+Last Modified: Monday 21st July 2025
 Modified By: CB
 -----
 License: BSD 3-Clause "New" or "Revised" License
@@ -40,6 +40,8 @@ sys.path.append(
 from src import plot_output_thermo
 from plotssrc import pltsds, pltmoms
 from pySD.sdmout_src import pyzarr, pysetuptxt, pygbxsdat
+
+isfigures = [True, True]  # booleans for [making, saving] initialisation figures
 
 ### ---------------------------------------------------------------- ###
 ### ----------------------- INPUT PARAMETERS ----------------------- ###
@@ -80,10 +82,12 @@ shutil.rmtree(all_thermofiles, ignore_errors=True)
 fromfile_irreg_inputfiles.main(
     path2CLEO,
     path2build,
+    savefigpath,
     config_filename,
     grid_filename,
     initsupers_filename,
     thermofiles,
+    isfigures=isfigures,
 )
 ### ---------------------------------------------------------------- ###
 ### ---------------------------------------------------------------- ###
