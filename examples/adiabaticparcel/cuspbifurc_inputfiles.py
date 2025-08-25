cdf52ca5ad4e05ec76575d8cb74ee7f68076b49e
clara.bayley <yoctoyotta1024@yahoo.com>
2025-08-25 17:12:02 +0200

    refactor: clean up cuspbifurc example

---
+++ examples/adiabaticparcel/cuspbifurc_inputfiles.py
@@ -0,0 +1,166 @@
+"""
+Copyright (c) 2025 MPI-M, Clara Bayley
+
+
+----- CLEO -----
+File: cuspbifurc_inputfiles.py
+Project: adiabaticparcel
+Created Date: Monday 25th August 2025
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Monday 25th August 2025
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+File Description:
+Script generates input files for adiabatic parcel example which produces plot
+similar to Figure 5 of "On the CCN (de)activation nonlinearities"
+S. Arabas and S. Shima 2017 to show example of cusp birfucation for
+0D adiabatic parcel expansion and contraction.
+"""
+
+
+# %%
+### ------------------------- FUNCTION DEFINITIONS ------------------------- ###
+def parse_arguments():
+    import argparse
+    from pathlib import Path
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path2CLEO", type=Path, help="Absolute path to CLEO directory (for PySD)"
+    )
+    parser.add_argument(
+        "path2build", type=Path, help="Absolute path to build directory"
+    )
+    parser.add_argument(
+        "config_filename", type=Path, help="Absolute path to configuration YAML file"
+    )
+    parser.add_argument(
+        "--savefigpath",
+        type=Path,
+        default=None,
+        help="Directory to save initialiation figures in (is save_figures is True)",
+    )
+    parser.add_argument(
+        "--show_figures",
+        action="store_true",  # default is False
+        help="Show initialiation figures",
+    )
+    parser.add_argument(
+        "--save_figures",
+        action="store_true",  # default is False
+        help="Save initialiation figures in savefigpath",
+    )
+    return parser.parse_args()
+
+
+# %%
+### -------------------------------- MAIN ---------------------------------- ###
+def main(
+    path2CLEO,
+    path2build,
+    config_filename,
+    savefigpath=None,
+    show_figures=False,
+    save_figures=False,
+):
+    import sys
+    import numpy as np
+    from pathlib import Path
+    from ruamel.yaml import YAML
+
+    sys.path.append(str(path2CLEO))  # for imports from pySD package
+    from pySD import geninitconds
+    from pySD.initsuperdropsbinary_src import rgens, dryrgens, probdists, attrsgen
+
+    if path2CLEO == path2build:
+        raise ValueError("build directory cannot be CLEO")
+
+    ### --- Load the config YAML file --- ###
+    yaml = YAML()
+    with open(config_filename, "r") as file:
+        config = yaml.load(file)
+
+    ### ------------------------ INPUT PARAMETERS -------------------------- ###
+    ### --- required CLEO cleoconstants.hpp file --- ###
+    constants_filename = Path(config["inputfiles"]["constants_filename"])
+
+    ### --- plots of initial conditions --- ###
+    isfigures = [
+        show_figures,
+        save_figures,
+    ]  # booleans for [showing, saving] initialisation figures
+    SDgbxs2plt = "all"  # gbxindex of initial SDs to plot if any(isfigures) (nb. "all" can be very slow)
+
+    # settings for 0D Model (number of SD and grid coordinates)
+    nsupers = {0: 1}
+    zgrid = np.asarray([0, 100])
+    xgrid = np.asarray([0, 100])
+    ygrid = np.asarray([0, 100])
+
+    # settings for monodisperse droplet radii
+    # numconc = total no. concentration of droplets [m^-3]
+    numconc = 0.5e9
+    # monor = dry radius of all droplets [m]
+    monor = 0.025e-6
+
+    # monodisperse droplet radii probability distribution
+    radiigen = rgens.MonoAttrGen(monor)
+    dryradiigen = dryrgens.ScaledRadiiGen(1.0)
+    xiprobdist = probdists.DiracDelta(monor)
+
+    # do not generate SD coords
+    coord3gen = None
+    coord1gen = None
+    coord2gen = None
+
+    ### --------------------- BINARY FILES GENERATION ---------------------- ###
+    ### ----- write gridbox boundaries binary ----- ###
+    grid_filename = Path(config["inputfiles"]["grid_filename"])
+    geninitconds.generate_gridbox_boundaries(
+        grid_filename,
+        zgrid,
+        xgrid,
+        ygrid,
+        constants_filename,
+        isprintinfo=True,
+        isfigures=isfigures,
+        savefigpath=savefigpath,
+    )
+
+    ### ----- write initial superdroplets binary ----- ###
+    initsupers_filename = Path(config["initsupers"]["initsupers_filename"])
+    initattrsgen = attrsgen.AttrsGenerator(
+        radiigen, dryradiigen, xiprobdist, coord3gen, coord1gen, coord2gen
+    )
+    geninitconds.generate_initial_superdroplet_conditions(
+        initattrsgen,
+        initsupers_filename,
+        config_filename,
+        constants_filename,
+        grid_filename,
+        nsupers,
+        numconc,
+        isprintinfo=True,
+        isfigures=isfigures,
+        savefigpath=savefigpath,
+        gbxs2plt=SDgbxs2plt,
+    )
+
+
+# %%
+### --------------------------- RUN PROGRAM -------------------------------- ###
+if __name__ == "__main__":
+    args = parse_arguments()
+    main(
+        args.path2CLEO,
+        args.path2build,
+        args.config_filename,
+        savefigpath=args.savefigpath,
+        show_figures=args.show_figures,
+        save_figures=args.save_figures,
+    )
