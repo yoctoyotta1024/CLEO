d29eda1e1685a93763404a3f8c4cf542bac6c478
clara.bayley <yoctoyotta1024@yahoo.com>
2025-08-29 14:59:34 +0200

    feat: minimum pyproject.toml to build examplesplotting locally

--- examples/exampleplotting/plotcleo/__init__.py
+++ examples/exampleplotting/plotcleo/__init__.py
@@ -13,3 +13,9 @@ Copyright (c) 2023 MPI-M, Clara Bayley
 -----
 File Description:
 """
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"  # Fallback for development mode
