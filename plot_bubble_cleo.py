dbe3ecbf64931039ba6a62334bc5b9a1a34108f6
clara.bayley <yoctoyotta1024@yahoo.com>
2025-10-01 11:57:19 +0200

    new files to help run and plot icon bubble

---
+++ plot_bubble_cleo.py
@@ -0,0 +1,16 @@
+"""
+Copyright (c) 2025 MPI-M, Clara Bayley
+
+
+----- CLEO -----
+File: plot_bubble.py
+Project: CLEO
+Created Date: Thursday 1st January 1970
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+File Description:
+"""
