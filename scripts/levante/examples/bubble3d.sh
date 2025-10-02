c6c50dec7fe7ef051231e276abb8239b9b6c0df5
clara.bayley <yoctoyotta1024@yahoo.com>
2025-10-02 18:16:16 +0200

    only use bubble script to create GBxs and SDs input files

--- scripts/levante/examples/bubble3d.sh
+++ scripts/levante/examples/bubble3d.sh
@@ -18,7 +18,7 @@
 ### ---- build type, directories, the executable(s) ---- ###
 ### -------- to compile, and your python script -------- ###
 ### ---------------------------------------------------- ###
-do_build="true"
+do_build="false"
 buildtype="openmp"
 compilername="gcc"
 path2CLEO=${HOME}/CLEO/
@@ -29,8 +29,7 @@ executables="bubble3d"
 
 pythonscript=${path2CLEO}/examples/bubble3d/bubble3d.py
 src_config_filename=${path2CLEO}/examples/bubble3d/src/config/bubble3d_config.yaml
-script_args="${src_config_filename} \
-  --do_inputfiles --do_run_executable --do_plot_results"
+script_args="${src_config_filename} --do_inputfiles"
 ### ---------------------------------------------------- ###
 ### ---------------------------------------------------- ###
 ### ---------------------------------------------------- ###
@@ -41,6 +40,9 @@ then
   echo "-> please use compilername=gcc"
   exit 1
 fi
+
+mkdir ${path2build}
+mkdir ${path2build}/share
 
 ### ---------- build, compile and run example ---------- ###
 ${path2CLEO}/scripts/levante/examples/build_compile_run_plot.sh ${do_build} \
