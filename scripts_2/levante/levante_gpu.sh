90efd5c9a30a467fa4d5514c8bac5077a2bf8b4c
Harshada Balasubramanian <harshada.bala@gmail.com>
2026-05-27 17:35:37 +0200

    chore: update sbatch scripts

--- scripts_2/levante/levante_gpu.sh
+++ scripts_2/levante/levante_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=build_compile_run_cleo
+#SBATCH --job-name=cleo_gpu
 #SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --gpus=4
@@ -10,13 +10,12 @@
 #SBATCH --mail-user=
 #SBATCH --mail-type=FAIL
 #SBATCH --account=
-#SBATCH --output=./build_compile_run_cleo_out.%j.out
-#SBATCH --error=./build_compile_run_cleo_err.%j.out
+#SBATCH --output=./cleo_gpu.%j.out
+#SBATCH --error=./cleo_gpu.%j.out
 
 set -e
 source /etc/profile
 module purge
 spack unload --all
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-${SCRIPT_DIR}/build_compile_run_plot_cleo.sh "$@"
+${SLURM_SUBMIT_DIR}/scripts_2/levante/build_compile_run_plot_cleo.sh constthermo2d cuda
