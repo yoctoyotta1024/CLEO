0db63b32b17293a09513d484dc76e3845b9eada2
clara.bayley <yoctoyotta1024@yahoo.com>
2026-07-30 17:18:13 +0200

    fix: creation of b4 array must come before call to get_local_bounding_gridbox_index

--- libs/cartesiandomain/movement/cartesian_transport_across_domain.hpp
+++ libs/cartesiandomain/movement/cartesian_transport_across_domain.hpp
@@ -213,14 +213,17 @@ viewd_supers sendrecv_supers(const GbxMaps &gbxmaps, const viewd_gbx d_gbxs,
     // Get the local gridbox index which contains the superdroplet
     auto drop_coords = std::array<double, 3>{totsupers(i).get_coord3(), totsupers(i).get_coord1(),
                                              totsupers(i).get_coord2()};
+#ifndef NDEBUG
+    // see use of b4 in NDEBUG after call to get_local_bounding_gridbox_index
+    const auto b4 = std::array<double, 3>{drop_coords[0], drop_coords[1], drop_coords[2]};
+#endif
     const auto gbxindex =
         (unsigned int)gbxmaps.get_domain_decomposition().get_local_bounding_gridbox_index(
             drop_coords);  // TODO(ALL): access through gbxmaps (note error in conversions?)
 
+#ifndef NDEBUG
     // Since the coordinates have already been corrected in the sending
     // process here just the gridbox index update is necessary
-#ifndef NDEBUG
-    const auto b4 = std::array<double, 3>{drop_coords[0], drop_coords[1], drop_coords[2]};
     if ((drop_coords[0] != b4[0]) || (drop_coords[1] != b4[1]) || (drop_coords[2] != b4[2])) {
       throw std::runtime_error(
           "drop coordinates should have already been corrected and so shoudn't have changed here");
