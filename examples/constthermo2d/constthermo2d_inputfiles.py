ea58b55b90079e12d5c5f22095cc4ae59bbc35bb
clara.bayley <yoctoyotta1024@yahoo.com>
2026-08-18 15:49:16 +0200

    refactor(constthermo2d): remove hardcoded values from inputfiles

--- examples/constthermo2d/constthermo2d_inputfiles.py
+++ examples/constthermo2d/constthermo2d_inputfiles.py
@@ -111,6 +111,7 @@ def main(
     yaml = YAML()
     with open(config_filename, "r") as file:
         config = yaml.load(file)
+    pyconfig = config["python_inputfiles"]
 
     ### ------------------------ INPUT PARAMETERS -------------------------- ###
     ### --- required CLEO cleoconstants.hpp file --- ###
@@ -126,37 +127,39 @@ def main(
     ]  # gbxindex of initial SDs to plot if any(isfigures) (nb. "all" can be very slow)
 
     ### --- settings for 2-D gridbox boundaries --- ###
-    zgrid = [0, 1500, 75]  # evenly spaced zhalf coords [zmin, zmax, zdelta] [m]
-    xgrid = [0, 1500, 75]  # evenly spaced xhalf coords [m]
-    ygrid = np.array([0, 20])  # array of yhalf coords [m]
+    # evenly spaced x and z spatial coords [min, max, delta] [m]
+    zgrid_spacing = float(pyconfig["zgrid_max"] / pyconfig["zgrid_ngbxs"])
+    xgrid_spacing = float(pyconfig["xgrid_max"] / pyconfig["xgrid_ngbxs"])
+    zgrid = [0, pyconfig["zgrid_max"], zgrid_spacing]  # evenly spaced zhalf coords [m]
+    xgrid = [0, pyconfig["xgrid_max"], xgrid_spacing]  # evenly spaced xhalf coords [m]
+    ygrid = np.array([0, pyconfig["ygrid_max"]])  # array of yhalf coords [y0, y1] [m]
 
     ### --- settings for initial superdroplets --- ###
     # settings for initial superdroplet coordinates
-    zlim = 500  # max z coord of superdroplets
-    npergbx = 8  # number of superdroplets per gridbox
+    zlim = pyconfig["sd_zlim"]
+    npergbx = pyconfig["nsupers_pergbx"]
 
     # [min, max] range of initial superdroplet radii (and implicitly solute masses)
-    rspan = [3e-9, 3e-6]  # [m]
+    rspan = list(pyconfig["rspan"])
 
-    # settings for initial superdroplet multiplicies
-    # (from bimodal Lognormal distribution)
-    geomeans = [0.02e-6, 0.15e-6]
-    geosigs = [1.4, 1.6]
-    scalefacs = [6e6, 4e6]
-    numconc = np.sum(scalefacs)
+    # settings for initial superdroplet multiplicies (from bimodal Lognormal distribution)
+    geomeans = list(pyconfig["geomeans"])
+    geosigs = list(pyconfig["geosigs"])
+    scalefacs = list(pyconfig["scalefacs"])
+    numconc = pyconfig["numconc"]
 
     ### --- settings for 2D Thermodynamics --- ###
-    PRESSz0 = 101315  # [Pa]
-    THETA = 288.15  # [K]
-    qcond = 0.0  # [Kg/Kg]
-    WMAX = 0.6  # [m/s]
-    VVEL = None  # [m/s]
-    Zlength = 1500  # [m]
-    Xlength = 1500  # [m]
-    qvapmethod = "sratio"
-    Zbase = 750  # [m]
-    sratios = [0.99, 1.0025]  # s_ratio [below, above] Zbase
+    PRESSz0 = pyconfig["thermo_PRESSz0"]
+    THETA = pyconfig["thermo_THETA"]
+    WMAX = pyconfig["thermo_WMAX"]
+    Zlength = pyconfig["thermo_Zlength"]
+    Xlength = pyconfig["thermo_Xlength"]
+    qvapmethod = pyconfig["thermo_qvapmethod"]
+    Zbase = pyconfig["thermo_Zbase"]
+    sratios = list(pyconfig["thermo_sratios"])
     moistlayer = False
+    qcond = 0.0  # background liquid water mass mixing ratio [Kg/Kg]
+    VVEL = None  # [m/s]
 
     ### --------------------- BINARY FILES GENERATION ---------------------- ###
     ### ----- write gridbox boundaries binary ----- ###
